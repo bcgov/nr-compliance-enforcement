b377b8bc73b5304981c636283ecf4614d79d9f5f
afwilcox <alecwilcox@gmail.com>
2023-04-14 14:51:23 -0700

    Update App.tsx
    This is a test change to see how the Pull Requests react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload!
+          Edit <code>src/App.tsx</code> and save to reload!  This is Alec's Change.
         </p>
         <a
           className="App-link"
