d2327e8976460ec197c66a069f2cc94c0bf1b072
Mike <100624415+marqueone-ps@users.noreply.github.com>
2023-04-17 11:22:25 -0700

    Frontend update (#6)
    Co-authored-by: Mike Sears <mike.2.sears@gov.bc.ca>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import reportWebVitals from "./reportWebVitals";
+import App from "./app/App";
+import { store } from "./app/store/store";
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
