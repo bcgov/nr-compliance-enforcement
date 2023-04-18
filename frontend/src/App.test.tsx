f5cb91f863ef3c02faa3bedb0d99f5722a7661a0
barrfalk <bfalk@salussystems.com>
2023-04-18 09:38:16 -0700

    Removed vite

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from "../app/store/store"
-import App from '../app/App';
+import { store } from "./app/store/store"
+import App from './app/App';
 
 test('renders learn react link', () => {
   render(
