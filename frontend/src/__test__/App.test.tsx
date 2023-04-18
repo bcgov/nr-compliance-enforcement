e60a25f37d914470c45480137715f057dd80ab41
barrfalk <bfalk@salussystems.com>
2023-04-17 21:53:28 -0700

    test-library updates

---
+++ frontend/src/__test__/App.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from "../app/store/store"
+import App from '../app/App';
+
+test('renders learn react link', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const testText = screen.getByText('Add Async');
+  expect(testText).toBeInTheDocument();
+});
