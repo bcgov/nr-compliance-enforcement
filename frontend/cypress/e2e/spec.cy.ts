37a31cf9694c395f4755d1910f0fe2e2cc6bad1b
barrfalk <bfalk@salussystems.com>
2023-04-19 20:15:10 -0700

    Update spec.cy.ts

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -4,13 +4,13 @@ describe('template spec', () => {
 
     cy.contains('Add Amount').click()
 
-    cy.get('#counter').should('contain', '2')
+    cy.get('#counter').should('contain', '-2')
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.Counter_asyncButton__hwwx\\+').click();
     /* ==== End Cypress Studio ==== */
 
-    cy.get('#counter').should('contain', '4')
+    cy.get('#counter').should('contain', '3')
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get(':nth-child(2) > :nth-child(4)').click();
