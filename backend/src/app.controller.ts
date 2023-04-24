55f8b8153295f6d337ea30d280a1caf28e04071d
barrfalk <bfalk@salussystems.com>
2023-04-23 18:27:20 -0700

    Backend Auth - Public Decorator
    Protect all backend controllers by default.  Introduced a public decorator to enable controllers that don't require auth.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,7 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 @ApiTags('Health Check')
@@ -11,6 +12,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
--- backend/src/auth/jwtauth.guard.ts
+++ backend/src/auth/jwtauth.guard.ts
@@ -5,14 +5,26 @@ import {
   UnauthorizedException,
   Logger,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport'; @Injectable()
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from './decorators/public.decorator';
+
+@Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
 
   private readonly logger = new Logger(JwtAuthGuard.name);
 
-  constructor() {
+  constructor(private reflector: Reflector) {
     super();
   } canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
     return super.canActivate(context);
   } handleRequest(err, user, info) {
     if (err || !user) {
