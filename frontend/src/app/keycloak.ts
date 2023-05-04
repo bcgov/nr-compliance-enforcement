5fdaedec6d12d883b7a8a72f934f62aaf0846227
barrfalk <bfalk@salussystems.com>
2023-05-04 13:57:26 -0700

    environment variables updates
    Cleaning the environment variables component

--- frontend/src/app/keycloak.ts
+++ frontend/src/app/keycloak.ts
@@ -1,13 +1,12 @@
 import Keycloak from 'keycloak-js';
-import config from "../config.json";
+import config from "../config.js";
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const _kc = new Keycloak({
   url: config.KEYCLOAK_URL,
-  realm: config.KEYCLOAK_REALM,
-  clientId: config.KEYCLOAK_CLIENT_ID,
+  realm: `${config.KEYCLOAK_REALM}`,
+  clientId: `${config.KEYCLOAK_CLIENT_ID}`,
 });
 
-console.log(`Keycloak URL ${config.KEYCLOAK_URL}`)
 export default _kc;
