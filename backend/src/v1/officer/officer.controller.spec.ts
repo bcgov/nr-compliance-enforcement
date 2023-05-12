6888a7b8181391165e633d7d80f0b10849fdc0f3
barrfalk <bfalk@salussystems.com>
2023-05-12 14:00:08 -0700

    Added jest tests for geo_org_unit_structure

--- backend/src/v1/officer/officer.controller.spec.ts
+++ backend/src/v1/officer/officer.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OfficerController } from './officer.controller';
 import { OfficerService } from './officer.service';
+import { Officer } from './entities/officer.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('OfficerController', () => {
   let controller: OfficerController;
@@ -8,7 +10,13 @@ describe('OfficerController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OfficerController],
-      providers: [OfficerService],
+      providers: [OfficerService,
+        {
+          provide: getRepositoryToken(Officer),
+          useValue: {
+
+          },
+      },],
     }).compile();
 
     controller = module.get<OfficerController>(OfficerController);
