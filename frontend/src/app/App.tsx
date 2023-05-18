852e8a87c4d5f874864448791dc4ce2908ceab0f
Mike <100624415+marqueone-ps@users.noreply.github.com>
2023-05-18 11:28:09 -0700

    COMPENF-31-ux-sass-config (#31)
    Co-authored-by: Mike Sears <mike.2.sears@gov.bc.ca>

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,25 +1,27 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
-import RenderOnRole from '../components/RenderOnRole';
-import Roles from './constants/roles';
+import RenderOnRole from "../components/RenderOnRole";
+import Roles from "./constants/roles";
+// import { SpaceReference as Reference } from "./components/reference";
 
 function App() {
   return (
-      <React.StrictMode>
-        <Provider store={store}>
-          <BrowserRouter>
-            <div className="container">
-              <RenderOnRole roles={[Roles.COS_OFFICER]}>
-                <Counter/>
-              </RenderOnRole>
-            </div>
-          </BrowserRouter>        
-        </Provider>
-      </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="container">
+            <RenderOnRole roles={[Roles.COS_OFFICER]}>
+              <Counter/>
+              {/* <Reference /> */}
+            </RenderOnRole>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,12 @@ import App from "./app/App";
 import { createRoot } from "react-dom/client";
 import UserService from "./app/service/UserServices";
 
-const container = document.getElementById('root')!;
+import "./assets/sass/app.scss"
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-const renderApp = () =>root.render(
-        <App />
-);
+const renderApp = () => root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
