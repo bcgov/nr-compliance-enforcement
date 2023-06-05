6b56965b45fa81bd25f93a82b74cfecd54ba1675
afwilcox <alecwilcox@gmail.com>
2023-06-05 14:02:01 -0700

    Initial Commit.   Should work but sub optimal.

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,12 +1,32 @@
 describe('template spec', () => {
 
   beforeEach(function() {
-    cy.kcLogout().kcLogin();
+      cy.kcLogout().kcLogin();
   });
 
-  it('passes', () => {
+  it('loads the header', () => {
 
       cy.visit("/");
 
-    })
+      // This is required to suppress cross domain errors when running locally.  Not an issue on OpenShift.
+      if (Cypress.config('baseUrl') == 'http://localhost:3000')
+      {
+        cy.origin('http://localhost:3000', () => {
+          cy.on('uncaught:exception', (e) => {
+              // we expected this error, so let's ignore it
+              // and let the test continue
+              return false
+          })
+          cy.contains('Human Wildlife Conflicts');
+
+          cy.contains('Enforcement');
+        })
+      } else
+      {
+        cy.contains('Human Wildlife Conflicts');
+        
+        cy.contains('Enforcement');
+      }
 })
+})
+
