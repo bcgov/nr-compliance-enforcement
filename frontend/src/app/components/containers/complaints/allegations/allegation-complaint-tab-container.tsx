839ad347765cdf511cf64edbde03523613f28804
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2023-06-09 10:17:55 -0700

    Refactoring work and bug fixes

--- frontend/src/app/components/containers/complaints/allegations/allegation-complaint-tab-container.tsx
+++ frontend/src/app/components/containers/complaints/allegations/allegation-complaint-tab-container.tsx
@@ -12,10 +12,10 @@ type Props = {
 export const AllegationComplaintTabContainer: FC<Props>  = ({ handleChange, handleSort, sort }) => {
     return <><div className="comp-div-tab"><ul className="nav nav-tabs comp-tab">
         <li className="nav-item comp-tab-inactive">
-            <a className="nav-link" href="#" onClick={() => handleChange(ComplaintType.HWCR_COMPLAINT)} >Human Wildlife Conflicts</a>
+            <button className="nav-link" onClick={() => handleChange(ComplaintType.HWCR_COMPLAINT)} >Human Wildlife Conflicts</button>
         </li>
         <li className="nav-item comp-tab-active">
-            <a className="nav-link active">Enforcement</a>
+            <button className="nav-link active">Enforcement</button>
         </li>
         </ul>
     </div>
--- frontend/src/app/components/containers/complaints/allegations/allegation-complaint-table.tsx
+++ frontend/src/app/components/containers/complaints/allegations/allegation-complaint-table.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect } from "react";
 import { format } from 'date-fns';
-import { Row, Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 import { getAllegationComplaints, allegationComplaints } from "../../../../store/reducers/allegation-complaint"
 
@@ -23,59 +23,45 @@ export const AllegationComplaintTable: FC<Props>  = ({ sortColumn, sortOrder }) 
         <Table id="comp-table" className="comp-table">
             <tbody>
                 {allegationComplaintsJson.map((val, key, {length}) => {
-                    const complaint_identifier = val.complaint_identifier.complaint_identifier;
-                    const incident_reported_datetime = val.complaint_identifier.incident_reported_datetime != null ? format(Date.parse(val.complaint_identifier.incident_reported_datetime), 'yyyy/MM/dd kk:mm:ss') : "";
-                    const violation_code = val.violation_code != null ? val.violation_code.long_description : "";
-                    const in_progress_class = (String)(val.in_progress_ind) === 'true' ? "btn btn-primary comp-in-progress-btn" : "btn btn-primary comp-in-progress-btn btn-hidden";
-                    const in_progress_ind = (String)(val.in_progress_ind) === 'true' ? "In Progress" : "";
-                    const geo_organization_unit_code = val.complaint_identifier.geo_organization_unit_code ? val.complaint_identifier.geo_organization_unit_code.short_description : "";
-                    const location_summary = val.complaint_identifier.location_summary_text;
-                    const statusClass =  val.complaint_identifier.complaint_status_code.long_description === 'Closed' ? 'btn btn-primary comp-status-closed-btn' : 'btn btn-primary comp-status-open-btn';
+                    const complaintIdentifier = val.complaint_identifier.complaint_identifier;
+                    const incidentReportedDatetime = val.complaint_identifier.incident_reported_datetime != null ? format(Date.parse(val.complaint_identifier.incident_reported_datetime), 'yyyy/MM/dd kk:mm:ss') : "";
+                    const violationCode = val.violation_code != null ? val.violation_code.long_description : "";
+                    const inProgressButtonClass = (String)(val.in_progress_ind) === 'true' ? "btn btn-primary comp-in-progress-btn" : "btn btn-primary comp-in-progress-btn btn-hidden";
+                    const inProgressInd = (String)(val.in_progress_ind) === 'true' ? "In Progress" : "";
+                    const geoOrganizationUnitCode = val.complaint_identifier.geo_organization_unit_code ? val.complaint_identifier.geo_organization_unit_code.short_description : "";
+                    const locationSummary = val.complaint_identifier.location_summary_text;
+                    const statusButtonClass =  val.complaint_identifier.complaint_status_code.long_description === 'Closed' ? 'btn btn-primary comp-status-closed-btn' : 'btn btn-primary comp-status-open-btn';
                     const status = val.complaint_identifier.complaint_status_code.long_description;
-                    const update_date = val.complaint_identifier.update_timestamp != null ? format(Date.parse(val.complaint_identifier.update_timestamp), 'yyyy/MM/dd kk:mm:ss') : "";
+                    const updateDate = val.complaint_identifier.update_timestamp != null ? format(Date.parse(val.complaint_identifier.update_timestamp), 'yyyy/MM/dd kk:mm:ss') : "";
                     //alternate behaviour for last row for alternate borders and radius'
-                    if(length - 1 === key)
-                    {
+                    const complaintIdentifierClass = (length - 1 === key) ? "comp-small-cell comp-cell comp-cell-bottom comp-cell-left comp-bottom-left" : "comp-small-cell comp-cell comp-cell-left";
+                    const incidentReportedDatetimeClass = (length - 1 === key) ? "comp-small-cell comp-cell-bottom comp-cell" : "comp-small-cell comp-cell";
+                    const violationCodeClass = (length - 1 === key) ? "comp-violation-cell comp-cell comp-cell-bottom" : "comp-violation-cell comp-cell";
+                    const inProgressIndClass = (length - 1 === key) ? "comp-in-progress-cell comp-cell comp-cell-bottom" : "comp-in-progress-cell comp-cell";
+                    const geoOrganizationUnitCodeClass = (length - 1 === key) ? "comp-area-cell comp-cell comp-cell-bottom" : "comp-area-cell comp-cell";
+                    const locationSummaryClass = (length - 1 === key) ? "comp-location-cell comp-cell comp-cell-bottom" : "comp-location-cell comp-cell";
+                    const officerAssignedClass = (length - 1 === key) ? "comp-medium-cell comp-cell comp-cell-bottom" : "comp-medium-cell comp-cell";
+                    const statusClass = (length - 1 === key) ? "comp-status-cell comp-cell comp-cell-bottom" : "comp-status-cell comp-cell";
+                    const updateDateClass = (length - 1 === key) ? "comp-last-updated-cell comp-cell comp-cell-bottom comp-bottom-right" : "comp-last-updated-cell comp-cell";
                         return (
-                            <Row key={key}>
-                                <td className="comp-small-cell comp-cell comp-cell-bottom comp-cell-left comp-bottom-left">{complaint_identifier}</td>
-                                <td className="comp-small-cell comp-cell-bottom comp-cell">{incident_reported_datetime}</td>
-                                <td className="comp-violation-cell comp-cell comp-cell-bottom">{violation_code}</td>
-                                <td className="comp-in-progress-cell comp-cell comp-cell-bottom">
-                                    <button type="button" className={in_progress_class}>{in_progress_ind}</button>
+                            <tr id={key.toString()}>
+                                <td className={complaintIdentifierClass}>{complaintIdentifier}</td>
+                                <td className={incidentReportedDatetimeClass}>{incidentReportedDatetime}</td>
+                                <td className={violationCodeClass}>{violationCode}</td>
+                                <td className={inProgressIndClass}>
+                                    <button type="button" className={inProgressButtonClass}>{inProgressInd}</button>
                                 </td>
-                                <td className="comp-area-cell comp-cell comp-cell-bottom">{geo_organization_unit_code}</td>
-                                <td className="comp-location-cell comp-cell comp-cell-bottom">{location_summary}</td>
-                                <td className="comp-medium-cell comp-cell comp-cell-bottom">
+                                <td className={geoOrganizationUnitCodeClass}>{geoOrganizationUnitCode}</td>
+                                <td className={locationSummaryClass}>{locationSummary}</td>
+                                <td className={officerAssignedClass}>
                                 </td>
-                                <td className="comp-status-cell comp-cell comp-cell-bottom">
-                                    <button type="button" className={statusClass}>{status}</button>
+                                <td className={statusClass}>
+                                    <button type="button" className={statusButtonClass}>{status}</button>
                                 </td>
-                                <td className="comp-last-updated-cell comp-cell comp-cell-bottom comp-bottom-right">{update_date}</td>
-                            </Row>
+                                <td className={updateDateClass}>{updateDate}</td>
+                            </tr>
                         )
-                    }
-                    else
-                    {
-                        return (
-                            <Row key={key}>
-                                <td className="comp-small-cell comp-cell comp-cell-left">{complaint_identifier}</td>
-                                <td className="comp-small-cell comp-cell">{incident_reported_datetime}</td>
-                                <td className="comp-violation-cell comp-cell">{violation_code}</td>
-                                <td className="comp-in-progress-cell comp-cell">
-                                    <button type="button" className={in_progress_class}>{in_progress_ind}</button>
-                                </td>
-                                <td className="comp-area-cell comp-cell">{geo_organization_unit_code}</td>
-                                <td className="comp-location-cell comp-cell">{location_summary}</td>
-                                <td className="comp-medium-cell comp-cell">
-                                </td>
-                                <td className="comp-status-cell comp-cell">
-                                <button type="button" className={statusClass}>{status}</button>
-                                </td>
-                                <td className="comp-last-updated-cell comp-cell">{update_date}</td>
-                            </Row>
-                        )
-                    }
+
                     })}
             </tbody>
         </Table>
