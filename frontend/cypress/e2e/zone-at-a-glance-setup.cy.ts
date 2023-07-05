e26ec5cb11d00be8f2ae1ecb1e8c304ad1ce7c9b
Mike <msears@parcsystems.ca>
2023-07-05 15:09:31 -0700

    added cypress tests

---
+++ frontend/cypress/e2e/zone-at-a-glance-setup.cy.ts
@@ -0,0 +1,60 @@
+describe("COMPENF-137 Zone at a Glance - Page Set Up", () => {
+  beforeEach(function () {
+    cy.viewport("macbook-16");
+    cy.kcLogout().kcLogin();
+  });
+
+  it("it should have more than one link", () => {
+    //-- navigate to application root
+    cy.visit("/");
+
+    //-- there should be more than one link in the side bar
+    cy.get(".comp-nav-item-list")
+      .find("a")
+      .then(({ length }) => {
+        expect(length, "rows N").to.be.gt(1);
+      });
+  });
+
+  it("it has link to zone at a glance", () => {
+    //-- navigate to application root
+    cy.visit("/");
+    
+    //-- sidebar should have link to zone at a glance
+    cy.get(".comp-nav-item-list")
+      .find("a")
+      .then((items) => {
+        const list = Array.from(items, (item) => {
+          return item.getAttribute("href");
+        });
+        expect(list).to.include("/zone/at-a-glance");
+      });
+  });
+
+  it("it can navigate to zone at a glance", () => {
+    //-- navigate to application root
+    cy.visit("/");
+
+    //-- navigate to the zone at a glance
+    cy.get("#zone-at-a-glance-link").click();
+    cy.wait(500);
+    //-- make sure we're on the zone at a glance page
+    cy.get('.comp-main-content').contains("Zone At a Glance")
+
+    //-- navigate back to complaints
+    cy.get('#complaints-link').click();
+    cy.wait(500);
+    cy.get("#root > div > div.comp-main-content > div.comp-sub-header").contains("Complaints")
+  });
+
+
+  // it("it can load zone at a glance", () => {
+  //   //-- navigate to application root
+  //   cy.visit("/");
+
+  //   //-- click on HWCR tab
+  //   cy.get("#hwcr-tab").click({ force: true });
+  //   cy.wait(5000);
+
+  // });
+});
--- frontend/src/app/types/app/menu-item.ts
+++ frontend/src/app/types/app/menu-item.ts
@@ -1,4 +1,5 @@
 export default interface MenuItem { 
+    id?: string,
     name: string,
     icon: string,
     route?: string
