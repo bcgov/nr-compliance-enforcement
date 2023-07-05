36f0dde9ed311e84bcf52e87e518caf6fad2c96c
Mike <msears@parcsystems.ca>
2023-07-05 15:26:23 -0700

    updated zone at a glance to include a proper header

--- frontend/src/app/components/containers/zone-at-a-glance/zone-at-a-glance.tsx
+++ frontend/src/app/components/containers/zone-at-a-glance/zone-at-a-glance.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 
-
-export const ZoneAtAGlance: FC = () =>  { 
-    return <>Zone At a Glance</>
-}
+export const ZoneAtAGlance: FC = () => {
+  return (
+    <>
+      <div className="comp-sub-header">Zone At a Glance</div>
+    </>
+  );
+};
