c76dab4596f6177b94032af4c14ee092ee616c20
Mike <msears@parcsystems.ca>
2023-07-06 15:54:16 -0700

    started implementation
    created banner map to dynamically select correct zone banner, added initial cypress test

--- frontend/src/app/components/containers/zone-at-a-glance/zone-at-a-glance.tsx
+++ frontend/src/app/components/containers/zone-at-a-glance/zone-at-a-glance.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
+import { useAppSelector } from "../../../hooks/hooks";
+import { profileZone } from "../../../store/reducers/app";
+import { getBannerByZone } from "./banner-map";
 
 export const ZoneAtAGlance: FC = () => {
+  const currentZone = useAppSelector<string>(profileZone);
+  let image = getBannerByZone(currentZone);
+
   return (
     <>
       <div className="comp-sub-header">Zone At a Glance</div>
+      <img src={image} />
     </>
   );
 };
