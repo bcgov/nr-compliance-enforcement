b546edd1767222f01f3039acec5c07b56e1c13b7
barrfalk <bfalk@salussystems.com>
2023-07-07 11:09:41 -0700

    Delete custom-styles.ts

--- frontend/src/app/common/custom-styles.ts
+++
@@ -1,9 +0,0 @@
-import { StylesConfig } from "react-select";
-
-export const customStyles: StylesConfig = {
-    control: (provided: Record<string, unknown>, state: any) => ({
-      ...provided,
-      border: state.isFocused ? "1px solid #90b6F8" : "1px solid #a1a1a1",
-      boxShadow: state.isFocused ? "0px 0px 0px 4px rgba(0,120,255,0.25)" : "none",
-    })
-  };
