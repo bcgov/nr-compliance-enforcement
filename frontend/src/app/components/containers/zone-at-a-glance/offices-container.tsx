e9dc1010b42b973cfe0a831ec4f6a78184730a6e
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2023-07-18 12:10:44 -0700

    Fixing code smells

--- frontend/src/app/components/containers/zone-at-a-glance/offices-container.tsx
+++ frontend/src/app/components/containers/zone-at-a-glance/offices-container.tsx
@@ -9,21 +9,16 @@ type Props = {
 }
 
 export const OfficesContainer: FC<Props> = ({hwcrOpenComplaintsOfficeStats, allegationOpenComplaintsOfficeStats}) => {
-  if(hwcrOpenComplaintsOfficeStats !== undefined && hwcrOpenComplaintsOfficeStats.length !== 0 && allegationOpenComplaintsOfficeStats !== undefined && allegationOpenComplaintsOfficeStats.length !== 0)
-  {
+
   return (
     <>
         { 
             hwcrOpenComplaintsOfficeStats.map((item, index) => {
               //fear the hackiness -- this should probably be refactored -- the orderby on the backend should keep these in line, but allegationOpenComplaintsOfficeStats[index] makes me feel dirty
-              return (<OfficeContainer hwcrOpenComplaintsOfficeStat={item} allegationOpenComplaintsOfficeStat={allegationOpenComplaintsOfficeStats[index]} />);
+              return (<OfficeContainer key={item.name} hwcrOpenComplaintsOfficeStat={item} allegationOpenComplaintsOfficeStat={allegationOpenComplaintsOfficeStats[index]} />);
             }) 
         }
     </>
   );
-      }
-      else
-      {
-        return <></>
-      }
+
 };
