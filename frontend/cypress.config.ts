97f726a2f30d0d1a33c5c11d1510bd0e2b76c922
afwilcox <alecwilcox@gmail.com>
2023-07-31 12:30:01 -0700

    Cypress: Remove waits, extend default timeout

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 export default defineConfig({
+  defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
