6f0d75fb9fa5555c1e11373f5e1ad5f897eb1ca2
Mike <msears@parcsystems.ca>
2023-08-03 14:13:35 -0700

    updated complaints list
    updated complaints list component to clear complaints when switching tabs to prevent data flashing

--- frontend/src/app/components/containers/complaints/complaint-container.tsx
+++ frontend/src/app/components/containers/complaints/complaint-container.tsx
@@ -5,13 +5,19 @@ import ComplaintType from "../../../constants/complaint-types";
 import Option from "../../../types/app/option";
 import { getComplaintTypeFromUrl } from "../../../common/methods";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
-import { getTokenProfile, profileZone, profileZoneDescription } from "../../../store/reducers/app";
+import {
+  getTokenProfile,
+  profileZone,
+  profileZoneDescription,
+} from "../../../store/reducers/app";
+import { setComplaints } from "../../../store/reducers/complaints";
+import COMPLAINT_TYPES from "../../../types/app/complaint-types";
 
 type Props = {
   initialState: number;
 };
 
-export const ComplaintContainer: FC<Props>  = ({ initialState }) => {
+export const ComplaintContainer: FC<Props> = ({ initialState }) => {
   const dispatch = useAppDispatch();
   const defaultZone = useAppSelector(profileZone);
   const defaultZoneLabel = useAppSelector(profileZoneDescription);
@@ -20,83 +26,134 @@ export const ComplaintContainer: FC<Props>  = ({ initialState }) => {
     if (!defaultZone || defaultZone === "") {
       dispatch(getTokenProfile());
     } else {
-      setZoneCodeFilter({value: defaultZone, label: defaultZoneLabel});
+      setZoneCodeFilter({ value: defaultZone, label: defaultZoneLabel });
     }
   }, [dispatch, defaultZone, defaultZoneLabel]);
 
-    const [sort, setSort] = useState(["incident_reported_datetime", "DESC"]);
-    const [regionCodeFilter, setRegionCodeFilter] = useState<Option | null>(null);
-    const [zoneCodeFilter, setZoneCodeFilter] = useState<Option | null>({value: defaultZone, label: defaultZoneLabel});
-    const [areaCodeFilter, setAreaCodeFilter] = useState<Option | null>(null);
-    const [officerFilter, setOfficerFilter] = useState<Option | null>(null);
-    const [natureOfComplaintFilter, setNatureOfComplaintFilter] = useState<Option | null>(null);
-    const [speicesCodeFilter, setSpeicesCodeFilter] = useState<Option | null>(null);
-    const [violationFilter, setViolationFilter] = useState<Option | null>(null);
-    const [startDateFilter, setStartDateFilter] = useState<Date>();
-    const [endDateFilter, setEndDateFilter] = useState<Date>();
-    const [complaintStatusFilter, setComplaintStatusFilter] = useState<Option | null>({value: 'OPEN', label: 'Open'});
-    const _test = getComplaintTypeFromUrl();
-    const [complaintType, setComplaintType] = useState<number>(
-      _test !== -1 ? _test : initialState
-    );    
+  const [sort, setSort] = useState(["incident_reported_datetime", "DESC"]);
+  const [regionCodeFilter, setRegionCodeFilter] = useState<Option | null>(null);
+  const [zoneCodeFilter, setZoneCodeFilter] = useState<Option | null>({
+    value: defaultZone,
+    label: defaultZoneLabel,
+  });
+  const [areaCodeFilter, setAreaCodeFilter] = useState<Option | null>(null);
+  const [officerFilter, setOfficerFilter] = useState<Option | null>(null);
+  const [natureOfComplaintFilter, setNatureOfComplaintFilter] =
+    useState<Option | null>(null);
+  const [speicesCodeFilter, setSpeicesCodeFilter] = useState<Option | null>(
+    null
+  );
+  const [violationFilter, setViolationFilter] = useState<Option | null>(null);
+  const [startDateFilter, setStartDateFilter] = useState<Date>();
+  const [endDateFilter, setEndDateFilter] = useState<Date>();
+  const [complaintStatusFilter, setComplaintStatusFilter] =
+    useState<Option | null>({ value: "OPEN", label: "Open" });
+  const _test = getComplaintTypeFromUrl();
+  const [complaintType, setComplaintType] = useState<number>(
+    _test !== -1 ? _test : initialState
+  );
 
-    function handleChange(newState: number)
-    {
-        setComplaintType(newState);
-        setSort(["incident_reported_datetime", "DESC"]);
-        setComplaintStatusFilter({value: 'OPEN', label: 'Open'});
-        setRegionCodeFilter(null);
-        setZoneCodeFilter({value: defaultZone, label: defaultZoneLabel});
-        setAreaCodeFilter(null);
-        setOfficerFilter(null);
-        setNatureOfComplaintFilter(null);
-        setSpeicesCodeFilter(null);
-        setViolationFilter(null);
-        setStartDateFilter(undefined);
-        setEndDateFilter(undefined);
+  function handleChange(newState: number) {
+    setComplaintType(newState);
+    setSort(["incident_reported_datetime", "DESC"]);
+    setComplaintStatusFilter({ value: "OPEN", label: "Open" });
+    setRegionCodeFilter(null);
+    setZoneCodeFilter({ value: defaultZone, label: defaultZoneLabel });
+    setAreaCodeFilter(null);
+    setOfficerFilter(null);
+    setNatureOfComplaintFilter(null);
+    setSpeicesCodeFilter(null);
+    setViolationFilter(null);
+    setStartDateFilter(undefined);
+    setEndDateFilter(undefined);
+
+    if (newState === ComplaintType.HWCR_COMPLAINT) {
+      console.log("clear hwcr")
+      dispatch(setComplaints({ type: COMPLAINT_TYPES.ERS, data: [] }));
+    } else {
+      console.log("clear ers")
+      dispatch(setComplaints({ type: COMPLAINT_TYPES.HWCR, data: [] }));
     }
-    function handleSort(newSortColumn: string) {
-      if (newSortColumn === sort[0]) {
-        if (sort[1] === "DESC") {
-          setSort([newSortColumn, "ASC"]);
-        } else {
-          setSort([newSortColumn, "DESC"]);
-        }
+  }
+  function handleSort(newSortColumn: string) {
+    if (newSortColumn === sort[0]) {
+      if (sort[1] === "DESC") {
+        setSort([newSortColumn, "ASC"]);
       } else {
-        if (
-          newSortColumn === "incident_reported_datetime" ||
-          newSortColumn === "update_timestamp"
-        ) {
-          setSort([newSortColumn, "DESC"]);
-        } else {
-          setSort([newSortColumn, "ASC"]);
-        }
+        setSort([newSortColumn, "DESC"]);
+      }
+    } else {
+      if (
+        newSortColumn === "incident_reported_datetime" ||
+        newSortColumn === "update_timestamp"
+      ) {
+        setSort([newSortColumn, "DESC"]);
+      } else {
+        setSort([newSortColumn, "ASC"]);
       }
     }
-    if(complaintType === ComplaintType.HWCR_COMPLAINT)
-    {
-        return <>
-            <div className="comp-sub-header">Complaints</div>
-            <div>
-                <HwcrComplaintTabContainer handleSort={handleSort} handleChange={handleChange} sort={sort} regionCodeFilter={regionCodeFilter} setRegionCodeFilter={setRegionCodeFilter} zoneCodeFilter={zoneCodeFilter} setZoneCodeFilter={setZoneCodeFilter} areaCodeFilter={areaCodeFilter} setAreaCodeFilter={setAreaCodeFilter} officerFilter={officerFilter} setOfficerFilter={setOfficerFilter} natureOfComplaintFilter={natureOfComplaintFilter} setNatureOfComplaintFilter={setNatureOfComplaintFilter}
-                    speciesCodeFilter={speicesCodeFilter} setSpeicesCodeFilter={setSpeicesCodeFilter} startDateFilter={startDateFilter} setStartDateFilter={setStartDateFilter} endDateFilter={endDateFilter} 
-                    setEndDateFilter={setEndDateFilter} complaintStatusFilter={complaintStatusFilter} setComplaintStatusFilter={setComplaintStatusFilter}/>
-            </div>
-        </>;
-    }
-    else if(complaintType === ComplaintType.ALLEGATION_COMPLAINT)
-    {
-        return <>
-            <div className="comp-sub-header">Complaints</div>
-            <div>
-                <AllegationComplaintTabContainer handleSort={handleSort} handleChange={handleChange} sort={sort} regionCodeFilter={regionCodeFilter} setRegionCodeFilter={setRegionCodeFilter} zoneCodeFilter={zoneCodeFilter} setZoneCodeFilter={setZoneCodeFilter} areaCodeFilter={areaCodeFilter} setAreaCodeFilter={setAreaCodeFilter} officerFilter={officerFilter} setOfficerFilter={setOfficerFilter}
-                    violationFilter={violationFilter} setViolationFilter={setViolationFilter} startDateFilter={startDateFilter} setStartDateFilter={setStartDateFilter} endDateFilter={endDateFilter} 
-                    setEndDateFilter={setEndDateFilter} complaintStatusFilter={complaintStatusFilter} setComplaintStatusFilter={setComplaintStatusFilter}/>
-            </div>
-        </>;
-    }
-    else
-    {
-        return <></>;
-    }
+  }
+  if (complaintType === ComplaintType.HWCR_COMPLAINT) {
+    return (
+      <>
+        <div className="comp-sub-header">Complaints</div>
+        <div>
+          <HwcrComplaintTabContainer
+            handleSort={handleSort}
+            handleChange={handleChange}
+            sort={sort}
+            regionCodeFilter={regionCodeFilter}
+            setRegionCodeFilter={setRegionCodeFilter}
+            zoneCodeFilter={zoneCodeFilter}
+            setZoneCodeFilter={setZoneCodeFilter}
+            areaCodeFilter={areaCodeFilter}
+            setAreaCodeFilter={setAreaCodeFilter}
+            officerFilter={officerFilter}
+            setOfficerFilter={setOfficerFilter}
+            natureOfComplaintFilter={natureOfComplaintFilter}
+            setNatureOfComplaintFilter={setNatureOfComplaintFilter}
+            speciesCodeFilter={speicesCodeFilter}
+            setSpeicesCodeFilter={setSpeicesCodeFilter}
+            startDateFilter={startDateFilter}
+            setStartDateFilter={setStartDateFilter}
+            endDateFilter={endDateFilter}
+            setEndDateFilter={setEndDateFilter}
+            complaintStatusFilter={complaintStatusFilter}
+            setComplaintStatusFilter={setComplaintStatusFilter}
+          />
+        </div>
+      </>
+    );
+  } else if (complaintType === ComplaintType.ALLEGATION_COMPLAINT) {
+    return (
+      <>
+        <div className="comp-sub-header">Complaints</div>
+        <div>
+          <AllegationComplaintTabContainer
+            handleSort={handleSort}
+            handleChange={handleChange}
+            sort={sort}
+            regionCodeFilter={regionCodeFilter}
+            setRegionCodeFilter={setRegionCodeFilter}
+            zoneCodeFilter={zoneCodeFilter}
+            setZoneCodeFilter={setZoneCodeFilter}
+            areaCodeFilter={areaCodeFilter}
+            setAreaCodeFilter={setAreaCodeFilter}
+            officerFilter={officerFilter}
+            setOfficerFilter={setOfficerFilter}
+            violationFilter={violationFilter}
+            setViolationFilter={setViolationFilter}
+            startDateFilter={startDateFilter}
+            setStartDateFilter={setStartDateFilter}
+            endDateFilter={endDateFilter}
+            setEndDateFilter={setEndDateFilter}
+            complaintStatusFilter={complaintStatusFilter}
+            setComplaintStatusFilter={setComplaintStatusFilter}
+          />
+        </div>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
