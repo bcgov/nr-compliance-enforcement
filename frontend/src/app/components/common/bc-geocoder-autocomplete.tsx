97ff81cf738b492019ce4e227fa4bbf65b4adaa5
barrfalk <bfalk@salussystems.com>
2023-08-20 09:23:01 -0700

    code smells

--- frontend/src/app/components/common/bc-geocoder-autocomplete.tsx
+++ frontend/src/app/components/common/bc-geocoder-autocomplete.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect, useState } from "react";
-import axios from "axios";
 import CreatableSelect from 'react-select/creatable';
 import { getGeocodedFeatures } from "../../hooks/geocoder";
 
@@ -22,52 +21,46 @@ export const BCGeocoderAutocomplete: FC<Props> = ({
   id,
   maxResults,
 }) => {
-  const [selectedAddress, setSelectedAddress] = useState<AddressOption | null>();
   const [addressOptions, setAddressOptions] = useState<AddressOption[]>([]);
-  const [inputValue, setInputValue] = useState<string>(`${value}`);
-
-  const fetchAddresses = async (inputValue: string) => {
-    const features = await getGeocodedFeatures(inputValue, maxResults);
-    try {
-      
-      if (features.features.length > 0) {
-      const options = features.features.map((feature: any) => ({
-        value: feature.properties.fullAddress,
-        label: feature.properties.fullAddress,
-      }));
-      if (options) {
-        setAddressOptions(options);
-      }
-    } else {
-      console.log("Feature length 0");
-    }
-    } catch (error) {
-      console.error("Error fetching addresses:", error);
-    }
-  };
+  const [inputValue, setInputValue] = useState<string>(`${value ?? ''}`);
 
   const handleInputChange = (inputValue: string) => {
     setInputValue(inputValue);
   };
 
-  const handleSelect = (selectedOption: AddressOption | null) => {
-    setSelectedAddress(selectedOption);
-  };
-
   useEffect(() => {
+    const fetchAddresses = async (inputValue: string) => {
+      const features = await getGeocodedFeatures(inputValue, maxResults);
+      try {
+        
+        if (features.features.length > 0) {
+        const options = features.features.map((feature: any) => ({
+          value: feature.properties.fullAddress,
+          label: feature.properties.fullAddress,
+        }));
+        if (options) {
+          setAddressOptions(options);
+        }
+      } else {
+        console.log("Feature length 0");
+      }
+      } catch (error) {
+        console.error("Error fetching addresses:", error);
+      }
+    };
     fetchAddresses(inputValue);
-  }, [inputValue]);
+  }, [inputValue, maxResults]);
 
   return (
     <CreatableSelect
       options={addressOptions}
       onInputChange={handleInputChange}
-      onChange={handleSelect}
       inputValue={inputValue}
       isClearable
       classNamePrefix="comp-select"
       placeholder="Search for an address"
       id={id}
+      formatCreateLabel={() => undefined}
       components={{
         DropdownIndicator: () => null,
         IndicatorSeparator: () => null,
--- frontend/src/app/hooks/geocoder.ts
+++ frontend/src/app/hooks/geocoder.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Feature } from '../types/maps/bcGeocoderType';
 
 export const getGeocodedFeatures = async (inputValue: string, maxResults: number) => {
-    const apiKey = "YOUR_GOOGLE_API_KEY";
     const apiUrl = `https://geocoder.api.gov.bc.ca/addresses.json?addressString=${inputValue}&locationDescriptor=any&maxResults=${maxResults}&interpolation=adaptive&echo=true&brief=false&autoComplete=true&setBack=0&outputSRS=4326&minScore=2&provinceCode=BC`;
 
     const response = await axios.get(apiUrl);
