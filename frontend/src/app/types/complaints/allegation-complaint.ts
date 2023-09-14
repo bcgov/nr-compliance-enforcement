59413f5232681695102d415831c9ee04642c1b33
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2023-09-14 12:35:22 -0700

    start of ERS changes

--- frontend/src/app/types/complaints/allegation-complaint.ts
+++ frontend/src/app/types/complaints/allegation-complaint.ts
@@ -1,3 +1,4 @@
+import { AgencyCode } from "../code-tables/agency-code";
 import { ComplaintStatusCode } from "../code-tables/complaint-status-code";
 import { ViolationCode } from "../code-tables/violation-code";
 import { LocationGeometryPoint } from "./complaint";
@@ -10,16 +11,26 @@ export interface AllegationComplaint {
       zone_code: string;
       office_location_name: string;
       area_name: string;
+      area_code: string;
     },
     location_geometry_point: LocationGeometryPoint;
     incident_datetime: string;
     incident_reported_datetime: string;
     location_summary_text: string;
+    location_detailed_text: string;
+    detail_text: string;
     create_user_id: string;
     create_timestamp: string;
     update_user_id: string;
     update_timestamp: string;
     complaint_status_code: ComplaintStatusCode;
+    caller_name: string;
+    caller_address: string;
+    caller_email: string;
+    caller_phone_1: string;
+    caller_phone_2: string;
+    caller_phone_3: string;
+    referred_by_agency_code: AgencyCode;
     person_complaint_xref: [{person_guid: {first_name: string, last_name: string},active_ind: boolean}]},
   violation_code: ViolationCode;
   in_progress_ind: string | boolean;
