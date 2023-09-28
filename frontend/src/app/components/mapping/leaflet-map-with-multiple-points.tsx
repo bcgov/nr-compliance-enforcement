a33b92e9f0bb002f4a0629f4e068d14cd9e26c61
barrfalk <bfalk@salussystems.com>
2023-09-27 22:06:27 -0700

    code smell

--- frontend/src/app/components/mapping/leaflet-map-with-multiple-points.tsx
+++ frontend/src/app/components/mapping/leaflet-map-with-multiple-points.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import "leaflet/dist/leaflet.css";
