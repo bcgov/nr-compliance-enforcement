346ddf36e39f48b3341fbff8388146abdb9a1224
afwilcox <alecwilcox@gmail.com>
2023-10-18 15:44:21 -0700

    Update Species Capitilization and Sort Order (#166)

---
+++ backend/db/migrations/R__Code-table-description-updates.sql
@@ -0,0 +1,5 @@
+-- species
+UPDATE species_code set short_description = 'Grizzly Bear', long_description = 'Grizzly Bear' WHERE species_code = 'GRZBEAR';
+UPDATE species_code set short_description = 'Hog/Pig/Boar (Feral)', long_description = 'Hog/Pig/Boar (Feral)' WHERE species_code = 'FERALHOG';
+UPDATE species_code set short_description = 'Mountain Goat', long_description = 'Mountain Goat' WHERE species_code = 'MTNGOAT';
+UPDATE species_code set short_description = 'River Otter', long_description = 'River Otter' WHERE species_code = 'RVROTTER';
