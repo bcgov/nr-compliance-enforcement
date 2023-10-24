68771467ce95bfaf2d5141355745915cbc4f4bb7
Mike Sears <msears@parcsystems.ca>
2023-10-24 15:12:49 -0700

    code smell cleanup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,7 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  // await app.listen(parseInt(process.env.PORT, 10) || 3001);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
