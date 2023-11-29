6aacae1a17777e074fe26967727a9fb3658fa5cd
barrfalk <bfalk@salussystems.com>
2023-11-29 11:55:13 -0800

    Update complaint-attachments.cy.ts

--- frontend/cypress/e2e/complaint-attachments.cy.ts
+++ frontend/cypress/e2e/complaint-attachments.cy.ts
@@ -37,7 +37,10 @@ describe("Complaint Attachments", () => {
       .and('not.be.visible');
 
       cy.get(".coms-carousel-actions").first().invoke('attr', 'style', 'display: block');
-      cy.get(".download-icon").should("exist");
+      
+      // cypress can't verify things that happen in other tabs, so don't open attachments in another tab
+      cy.get(".download-icon").first().invoke('removeAttr', 'target');
+      cy.get(".download-icon").first().click({force:true});
 
     });
 
