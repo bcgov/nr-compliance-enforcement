fbbf10c210d6e76623db261b1c0c4d9c9b5522a9
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2023-12-08 08:26:38 -0800

    Update complaint-details-edit.tsx

--- frontend/src/app/components/containers/complaints/details/complaint-details-edit.tsx
+++ frontend/src/app/components/containers/complaints/details/complaint-details-edit.tsx
@@ -80,6 +80,16 @@ export const ComplaintDetailsEdit: FC = () => {
     HwcrComplaint | AllegationComplaint | null | undefined
   >(complaint);
 
+  /*
+  useEffect(() => {
+    //-- when the component unmounts clear the complaint from redux
+    return () => {
+      console.log("updateComplaintzzzzzzzzzzzzzzzzzzzzzzz: " + JSON.stringify(updateComplaint));
+      dispatch(setComplaint(updateComplaint));
+      dispatch(setGeocodedComplaintCoordinates(updateComplaint?.complaint_identifier.location_geometry_point));
+    };
+  }, [dispatch, updateComplaint, setUpdateComplaint]);*/
+
   useEffect(() => {
     //-- when the component unmounts clear the complaint from redux
     return () => {
@@ -263,6 +273,7 @@ export const ComplaintDetailsEdit: FC = () => {
     area,
     region,
     zone,
+    zone_code,
     office,
     attractants,
     violationInProgress,
@@ -299,7 +310,7 @@ export const ComplaintDetailsEdit: FC = () => {
     selectComplaintSuspectWitnessDetails
   ) as ComplaintSuspectWitness;
 
-  const officersInZoneList = useAppSelector(selectOfficersByAgency(ownedByAgencyCode?.agency_code));
+  const officersInAgencyList = useAppSelector(selectOfficersByAgency(ownedByAgencyCode?.agency_code));
 
   useEffect(() => {
     const incidentDateTimeObject = incidentDateTime
@@ -316,8 +327,8 @@ export const ComplaintDetailsEdit: FC = () => {
   // Transform the fetched data into the DropdownOption type
 
   let assignableOfficers: Option[] =
-    officersInZoneList !== null
-      ? officersInZoneList.map((officer: Officer) => ({
+  officersInAgencyList !== null
+      ? officersInAgencyList.map((officer: Officer) => ({
           value: officer.person_guid.person_guid,
           label: `${officer.person_guid.first_name} ${officer.person_guid.last_name}`,
         }))
@@ -359,7 +370,7 @@ export const ComplaintDetailsEdit: FC = () => {
   );
   const selectedAreaCode = areaCodes.find((option) => option.label === area);
   const selectedAssignedOfficer = assignableOfficers?.find(
-    (option) => option.value === personGuid
+    (option) => option.value === updateComplaint?.complaint_identifier.person_complaint_xref[0]?.person_guid.person_guid ?? personGuid
   );
   const selectedAgencyCode = referredByAgencyCodes.find(
     (option) =>
@@ -566,8 +577,8 @@ export const ComplaintDetailsEdit: FC = () => {
         const { person_guid: officer } = selectedOfficer as any;
 
         if (from(source).any() && from(source).elementAt(0)) {
-          source[0].active_ind = true;
-          const assigned = { ...source[0], person_guid: officer };
+          const assigned = { ...source[0], person_guid: officer, active_ind: true };
+          console.log("assigned1 :" + JSON.stringify(assigned));
           source = [assigned];
         } else {
           const assigned = {
@@ -578,6 +589,7 @@ export const ComplaintDetailsEdit: FC = () => {
             active_ind: true,
             person_complaint_xref_code: "ASSIGNEE",
           };
+          console.log("assigned2 :" + JSON.stringify(assigned));
           source = [assigned];
         }
 
@@ -590,6 +602,7 @@ export const ComplaintDetailsEdit: FC = () => {
 
       } else if (from(source).any() && from(source).elementAt(0)) {
         const assigned = { ...source[0], active_ind: false };
+        console.log("assigned3 :" + JSON.stringify(assigned));
         source = [assigned];
 
         const updatedParent = {
@@ -599,7 +612,10 @@ export const ComplaintDetailsEdit: FC = () => {
 
         update.complaint_identifier = updatedParent;
       }
+      console.log("updateComplaintvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv: " + JSON.stringify(update));
+      console.log("lol");
       setUpdateComplaint(update);
+      console.log("lol2");
     }
   };
 
