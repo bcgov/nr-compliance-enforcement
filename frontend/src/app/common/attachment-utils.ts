a032b82efd8a44256de0733cad623d74d865ad04
Barrett Falk <bfalk@salussystems.com>
2023-12-11 09:09:42 -0800

    feature/CE-32-Add-Attachments-When-Creating-New-Complaint (#223)
    Co-authored-by: afwilcox <alecwilcox@gmail.com>

---
+++ frontend/src/app/common/attachment-utils.ts
@@ -0,0 +1,65 @@
+import {
+  deleteAttachments,
+  getAttachments,
+  saveAttachments,
+} from "../store/reducers/attachments";
+import { COMSObject } from "../types/coms/object";
+
+// used to update the state of attachments that are to be added to a complaint
+export const handleAddAttachments = (
+  setAttachmentsToAdd: React.Dispatch<React.SetStateAction<File[] | null>>,
+  selectedFiles: File[]
+) => {
+  setAttachmentsToAdd((prevFiles) =>
+    prevFiles ? [...prevFiles, ...selectedFiles] : selectedFiles
+  );
+};
+
+// used to update the state of attachments that are to be deleted from a complaint
+export const handleDeleteAttachments = (
+  attachmentsToAdd: File[] | null,
+  setAttachmentsToAdd: React.Dispatch<React.SetStateAction<File[] | null>>,
+  setAttachmentsToDelete: React.Dispatch<
+    React.SetStateAction<COMSObject[] | null>
+  >,
+  fileToDelete: COMSObject
+) => {
+  if (!fileToDelete.pendingUpload) {
+    // a user is wanting to delete a previously uploaded attachment
+    setAttachmentsToDelete((prevFiles) =>
+      prevFiles ? [...prevFiles, fileToDelete] : [fileToDelete]
+    );
+  } else if (attachmentsToAdd) {
+    // a user has added an attachment and deleted it, before the complaint was saved.  Let's make sure this file isn't uploaded, so remove it from the "attachmentsToAdd" state
+    setAttachmentsToAdd((prevAttachments) =>
+      prevAttachments
+        ? prevAttachments.filter((file) => decodeURIComponent(file.name) !== decodeURIComponent(fileToDelete.name))
+        : null
+    );
+  }
+};
+
+// Given a list of attachments to add/delete, call COMS to add/delete those attachments
+export async function handlePersistAttachments(
+  dispatch: any,
+  attachmentsToAdd: File[] | null,
+  attachmentsToDelete: COMSObject[] | null,
+  complaintIdentifier: string,
+  setAttachmentsToAdd: any,
+  setAttachmentsToDelete: any
+) {
+  if (attachmentsToDelete) {
+    await dispatch(deleteAttachments(attachmentsToDelete));
+  }
+
+  if (attachmentsToAdd) {
+    await dispatch(saveAttachments(attachmentsToAdd, complaintIdentifier));
+  }
+
+  // refresh store
+  await dispatch(getAttachments(complaintIdentifier));
+
+  // Clear the attachments since they've been added or saved.
+  setAttachmentsToAdd(null);
+  setAttachmentsToDelete(null);
+}
