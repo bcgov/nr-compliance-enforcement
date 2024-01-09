a28b8151423073cdfb860ae1a10ab50a3172f39c
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2024-01-09 15:44:07 -0800

    UI updates

---
+++ frontend/src/app/components/common/outcome-select.tsx
@@ -0,0 +1,72 @@
+import { FC } from "react";
+import Select, { StylesConfig } from "react-select";
+import Option from "../../types/app/option";
+
+type Props = {
+  id: string;
+  className?: string;
+  classNames?: {};
+  options: Array<Option>;
+  enableValidation: boolean;
+  errorMessage?: string;
+  classNamePrefix?: string;
+  placeholder: string;
+  defaultOption?: Option;
+  value?: Option | null;
+  onChange: (selectedOption: Option | null) => void;
+};
+
+export const OutcomeSelect: FC<Props> = ({
+  id,
+  className,
+  classNames,
+  options,
+  defaultOption,
+  placeholder,
+  enableValidation,
+  value,
+  onChange,
+  classNamePrefix,
+  errorMessage,
+}) => {
+  let styles: StylesConfig = {};
+
+  let items = [...options];
+
+  // If "none" is an option, lighten the colour a bit so that it doesn't appear the same as the other selectable options
+    styles = {
+      ...styles,
+      option: (provided, state) => ({
+        ...provided,
+        color:
+          state.label === "None" || state.label === "Unassigned"
+            ? "#a1a1a1"
+            : "black",
+      }),
+    };
+
+  //-- pass through the onChange event
+  const handleChange = (s: any) => {
+    onChange(s);
+  };
+
+  return (
+    <>
+      <Select
+        id={id}
+        className={
+          errorMessage ? `${className} error-select-border ` : className
+        }
+        classNames={classNames}
+        styles={styles}
+        placeholder={placeholder}
+        options={items}
+        value={value}
+        onChange={handleChange}
+        classNamePrefix={classNamePrefix}
+        defaultValue={defaultOption}
+      />
+      {enableValidation && <div className="error-message">{errorMessage}</div>}
+    </>
+  );
+};
--- frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
@@ -1,13 +1,112 @@
 import { FC } from "react";
+import { OutcomeSelect } from "../../../common/outcome-select";
+import Option from "../../../../types/app/option";
+import DatePicker from "react-datepicker";
+import { Button } from "react-bootstrap";
+import { Officer } from "../../../../types/person/person";
+import { useAppSelector } from "../../../../hooks/hooks";
+import { selectOfficersByAgency } from "../../../../store/reducers/officer";
+import { selectComplaintCallerInformation } from "../../../../store/reducers/complaints";
 
-export const HWCRComplaintAssessment: FC = () => {  
+
+export const HWCRComplaintAssessment: FC = () => {
+    const actionItems: Option[] = [
+        { value: "", label: "None"},
+        { value: "test1", label: "Test Action 1" },
+        { value: "test2", label: "Test Action 2" },
+    ];
+    const {
+        ownedByAgencyCode,
+      } = useAppSelector(selectComplaintCallerInformation);
+    const officersInAgencyList = useAppSelector(selectOfficersByAgency(ownedByAgencyCode?.agency_code));
+    const assignableOfficers: Option[] = officersInAgencyList !== null
+      ? officersInAgencyList.map((officer: Officer) => ({
+          value: officer.person_guid.person_guid,
+          label: `${officer.person_guid.first_name} ${officer.person_guid.last_name}`,
+        }))
+        : [];
     return (
         <div className="comp-outcome-report-block">
             <h6>Complaint assessment</h6>
             <div className="comp-outcome-report-complaint-assessment">
-
+                <div className="comp-outcome-report-container comp-outcome-report-top-spacing">
+                    <div className="comp-outcome-report-label-column">
+                        Assessment
+                    </div>
+                    <div className="comp-outcome-report-edit-column">
+                        <div className="form-check">
+                            <input className="form-check-input" id="review-required" type="checkbox" />
+                            <label className="form-check-label" htmlFor="review-required">Assessed public safety risk</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" id="review-required" type="checkbox" />
+                            <label className="form-check-label" htmlFor="review-required">Assessed health as per animal welfare guidelines</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" id="review-required" type="checkbox" />
+                            <label className="form-check-label" htmlFor="review-required">Assessed known conflict history</label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" id="review-required" type="checkbox" />
+                            <label className="form-check-label" htmlFor="review-required">Confirmed idenfication of offending animals</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="comp-outcome-report-container comp-outcome-report-inner-spacing">
+                    <div className="comp-outcome-report-label-half-column">
+                        Action required?
+                    </div>
+                    <div className="comp-outcome-report-edit-column">
+                        <OutcomeSelect id="action-required" options={actionItems} enableValidation={false} placeholder="Select" onChange={(e) => (e)} />
+                    </div>
+                    <div className="comp-outcome-report-label-half-column">
+                        Justification
+                    </div>
+                    <div className="comp-outcome-report-edit-column">
+                        <OutcomeSelect id="justification" options={actionItems} enableValidation={false} placeholder="Select" onChange={(e) => (e)} />
+                    </div>
+                </div>
+                <div className="comp-outcome-report-container comp-outcome-report-inner-spacing">
+                    <div className="comp-outcome-report-label-half-column">
+                        Officer
+                    </div>
+                    <div className="comp-outcome-report-edit-column">
+                        <OutcomeSelect id="outcome-officer" options={assignableOfficers} enableValidation={false} placeholder="Select" onChange={(e) => (e)} />
+                    </div>
+                    <div className="comp-outcome-report-label-half-column">
+                        Date
+                    </div>
+                    <div className="comp-outcome-report-edit-column">
+                        <DatePicker
+                            id="complaint-incident-time"
+                            showIcon
+                            onChange={(e) => (e)}
+                            dateFormat="yyyy-MM-dd"
+                            wrapperClassName="comp-details-edit-calendar-input"
+                        />
+                    </div>
+                </div>
+                <div className="comp-outcome-report-container comp-outcome-report-bottom-spacing">
+                        <div className="comp-outcome-report-actions">
+                <Button
+                  id="outcome-cancel-button"
+                  title="Cancel Outcome"
+                  className="comp-outcome-cancel"
+                  onClick={(e) => (e)}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  id="outcome-save-button"
+                  title="Save Outcome"
+                  className="comp-outcome-save"
+                  onClick={(e) => (e)}
+                >
+                  Save
+                </Button>
+                        </div>
+                    </div>
             </div>
         </div>
     );
 };
-  
