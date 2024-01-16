2ca039890a01676e6d1ec9b31992063d46bde20c
Mike Sears <msears@parcsystems.ca>
2024-01-16 15:42:46 -0800

    first pass, allegation_complaint cleanup

--- backend/src/v1/allegation_complaint/allegation_complaint.controller.ts
+++ backend/src/v1/allegation_complaint/allegation_complaint.controller.ts
@@ -1,19 +1,9 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Delete,
-  UseGuards,
-  Query,
-  Logger,
-} from "@nestjs/common";
+import { Controller, Get, Param, UseGuards, Logger } from "@nestjs/common";
 import { AllegationComplaintService } from "./allegation_complaint.service";
 import { JwtRoleGuard } from "../../auth/jwtrole.guard";
 import { ApiTags } from "@nestjs/swagger";
 import { Roles } from "../../auth/decorators/roles.decorator";
 import { Role } from "../../enum/role.enum";
-import { UUID } from "crypto";
-import { SearchPayload } from "../complaint/models/search-payload";
 
 @UseGuards(JwtRoleGuard)
 @ApiTags("allegation-complaint")
@@ -22,51 +12,9 @@ import { SearchPayload } from "../complaint/models/search-payload";
   version: "1",
 })
 export class AllegationComplaintController {
-  constructor(
-    private readonly allegationComplaintService: AllegationComplaintService
-  ) {}
+  constructor(private readonly allegationComplaintService: AllegationComplaintService) {}
 
   private readonly logger = new Logger(AllegationComplaintController.name);
-
-  @Get()
-  @Roles(Role.COS_OFFICER)
-  findAll(
-    @Query("sortColumn") sortColumn: string,
-    @Query("sortOrder") sortOrder: string
-  ) {
-    return this.allegationComplaintService.findAll(sortColumn, sortOrder);
-  }
-
-  @Get("search")
-  @Roles(Role.COS_OFFICER)
-  search(
-    @Query() model: SearchPayload ) {
-    return this.allegationComplaintService.search(model);
-  }
-
-  @Get("map/search")
-  @Roles(Role.COS_OFFICER)
-  searchMap(@Query() model) {
-    return this.allegationComplaintService.searchMap(model);
-  }
-
-  @Get(":id")
-  @Roles(Role.COS_OFFICER)
-  findOne(@Param("id") id: UUID) {
-    return this.allegationComplaintService.findOne(id);
-  }
-
-  @Delete(":id")
-  @Roles(Role.COS_OFFICER)
-  remove(@Param("id") id: UUID) {
-    return this.allegationComplaintService.remove(id);
-  }
-
-  @Get("/by-complaint-identifier/:id")
-  @Roles(Role.COS_OFFICER)
-  byId(@Param("id") id: string) {
-    return this.allegationComplaintService.findByComplaintIdentifier(id);
-  }
 
   @Get("/stats/by-zone/:zone")
   @Roles(Role.COS_OFFICER)
--- backend/src/v1/allegation_complaint/allegation_complaint.module.ts
+++ backend/src/v1/allegation_complaint/allegation_complaint.module.ts
@@ -3,64 +3,22 @@ import { AllegationComplaintService } from "./allegation_complaint.service";
 import { AllegationComplaintController } from "./allegation_complaint.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AllegationComplaint } from "./entities/allegation_complaint.entity";
-import { ComplaintService } from "../complaint/complaint.service";
-import { Complaint } from "../complaint/entities/complaint.entity";
-import { ViolationCodeService } from "../violation_code/violation_code.service";
-import { AgencyCodeService } from "../agency_code/agency_code.service";
-import { ComplaintStatusCode } from "../complaint_status_code/entities/complaint_status_code.entity";
-import { ComplaintStatusCodeService } from "../complaint_status_code/complaint_status_code.service";
-import { GeoOrganizationUnitCodeService } from "../geo_organization_unit_code/geo_organization_unit_code.service";
-import { ViolationCode } from "../violation_code/entities/violation_code.entity";
-import { AgencyCode } from "../agency_code/entities/agency_code.entity";
-import { GeoOrganizationUnitCode } from "../geo_organization_unit_code/entities/geo_organization_unit_code.entity";
-import { CosGeoOrgUnitService } from "../cos_geo_org_unit/cos_geo_org_unit.service";
 import { CosGeoOrgUnit } from "../cos_geo_org_unit/entities/cos_geo_org_unit.entity";
-import { OfficeService } from "../office/office.service";
-import { OfficerService } from "../officer/officer.service";
 import { Office } from "../office/entities/office.entity";
 import { Officer } from "../officer/entities/officer.entity";
-import { PersonService } from "../person/person.service";
-import { Person } from "../person/entities/person.entity";
-import { PersonComplaintXrefService } from "../person_complaint_xref/person_complaint_xref.service";
-import { PersonComplaintXref } from "../person_complaint_xref/entities/person_complaint_xref.entity";
-import { HwcrComplaint } from "../hwcr_complaint/entities/hwcr_complaint.entity";
 import { CodeTableModule } from "../code-table/code-table.module";
 import { AttractantHwcrXrefModule } from "../attractant_hwcr_xref/attractant_hwcr_xref.module";
-import { ReportedByCode } from "../reported_by_code/entities/reported_by_code.entity";
-import { ReportedByCodeService } from "../reported_by_code/reported_by_code.service";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AllegationComplaint]),
-    TypeOrmModule.forFeature([Complaint]),
-    TypeOrmModule.forFeature([ViolationCode]),
-    TypeOrmModule.forFeature([AgencyCode]),
-    TypeOrmModule.forFeature([ComplaintStatusCode]),
-    TypeOrmModule.forFeature([GeoOrganizationUnitCode]),
     TypeOrmModule.forFeature([CosGeoOrgUnit]),
     TypeOrmModule.forFeature([Office]),
     TypeOrmModule.forFeature([Officer]),
-    TypeOrmModule.forFeature([Person]),
-    TypeOrmModule.forFeature([PersonComplaintXref]),
-    TypeOrmModule.forFeature([HwcrComplaint]),
-    TypeOrmModule.forFeature([ReportedByCode]),
     CodeTableModule,
-    AttractantHwcrXrefModule
+    AttractantHwcrXrefModule,
   ],
   controllers: [AllegationComplaintController],
-  providers: [
-    AllegationComplaintService,
-    ComplaintService,
-    ViolationCodeService,
-    AgencyCodeService,
-    ComplaintStatusCodeService,
-    GeoOrganizationUnitCodeService,
-    CosGeoOrgUnitService,
-    OfficeService,
-    OfficerService,
-    PersonService,
-    PersonComplaintXrefService,
-    ReportedByCodeService,
-  ],
+  providers: [AllegationComplaintService],
 })
 export class AllegationComplaintModule {}
