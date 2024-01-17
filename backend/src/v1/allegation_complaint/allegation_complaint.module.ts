7cd7bd60bcfa85f3d5aff9e056c8e014f68b0a43
Mike Sears <msears@parcsystems.ca>
2024-01-16 16:42:10 -0800

    allegation_complaint service, controller and dto's removed

--- backend/src/v1/allegation_complaint/allegation_complaint.module.ts
+++ backend/src/v1/allegation_complaint/allegation_complaint.module.ts
@@ -1,24 +1,12 @@
 import { Module } from "@nestjs/common";
-import { AllegationComplaintService } from "./allegation_complaint.service";
-import { AllegationComplaintController } from "./allegation_complaint.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AllegationComplaint } from "./entities/allegation_complaint.entity";
-import { CosGeoOrgUnit } from "../cos_geo_org_unit/entities/cos_geo_org_unit.entity";
-import { Office } from "../office/entities/office.entity";
-import { Officer } from "../officer/entities/officer.entity";
-import { CodeTableModule } from "../code-table/code-table.module";
-import { AttractantHwcrXrefModule } from "../attractant_hwcr_xref/attractant_hwcr_xref.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AllegationComplaint]),
-    TypeOrmModule.forFeature([CosGeoOrgUnit]),
-    TypeOrmModule.forFeature([Office]),
-    TypeOrmModule.forFeature([Officer]),
-    CodeTableModule,
-    AttractantHwcrXrefModule,
   ],
-  controllers: [AllegationComplaintController],
-  providers: [AllegationComplaintService],
+  controllers: [],
+  providers: [],
 })
 export class AllegationComplaintModule {}
