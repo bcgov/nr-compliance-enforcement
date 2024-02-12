ad59afe8df7240815043fce91fcf844678cbeff3
barrfalk <bfalk@salussystems.com>
2024-02-12 08:57:24 -0800

    Jest and cypress fixes

--- backend/src/v1/staging_complaint/staging_complaint.service.spec.ts
+++ backend/src/v1/staging_complaint/staging_complaint.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StagingComplaintService } from './staging_complaint.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { StagingComplaint } from './entities/staging_complaint.entity';
 
 describe('StagingComplaintService', () => {
   let service: StagingComplaintService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StagingComplaintService],
-    }).compile();
+      providers: [StagingComplaintService, {
+        provide: getRepositoryToken(StagingComplaint),
+        useValue: {
+
+        },
+      },],
+  }).compile();
 
     service = module.get<StagingComplaintService>(StagingComplaintService);
   });
