b620aada641b24d59a7e992ae32a52ce2d7e9d6a
barrfalk <bfalk@salussystems.com>
2024-03-06 16:07:13 -0800

    Added edit mode

--- frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
@@ -4,10 +4,19 @@ import { Button } from "react-bootstrap";
 import { Officer } from "../../../../types/person/person";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 import { selectOfficersByAgency } from "../../../../store/reducers/officer";
-import { getComplaintById, selectComplaint, selectComplaintCallerInformation, selectComplaintHeader } from "../../../../store/reducers/complaints";
-import { selectAssessmentTypeCodeDropdown, selectJustificationCodeDropdown, selectYesNoCodeDropdown } from "../../../../store/reducers/code-table";
+import {
+  getComplaintById,
+  selectComplaint,
+  selectComplaintCallerInformation,
+  selectComplaintHeader,
+} from "../../../../store/reducers/complaints";
+import {
+  selectAssessmentTypeCodeDropdown,
+  selectJustificationCodeDropdown,
+  selectYesNoCodeDropdown,
+} from "../../../../store/reducers/code-table";
 import { useParams } from "react-router-dom";
-import { getSelectedOfficer } from "../../../../common/methods";
+import { formatDate, getAvatarInitials, getSelectedOfficer } from "../../../../common/methods";
 import { CompSelect } from "../../../common/comp-select";
 import { ValidationCheckboxGroup } from "../../../../common/validation-checkbox-group";
 import { resetAssessment, selectAssessment, setAssessment } from "../../../../store/reducers/outcomes/assessment";
@@ -17,6 +26,8 @@ import { ToggleError, ToggleSuccess } from "../../../../common/toast";
 import "react-toastify/dist/ReactToastify.css";
 import { Assessment } from "../../../../types/outcomes/assessment";
 import { ValidationDatePicker } from "../../../../common/validation-date-picker";
+import { BsPencil } from "react-icons/bs";
+import { CompTextIconButton } from "../../../common/comp-text-icon-button";
 
 export const HWCRComplaintAssessment: FC = () => {
   const dispatch = useAppDispatch();
@@ -29,6 +40,7 @@ export const HWCRComplaintAssessment: FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null | undefined>();
   const [selectedOfficer, setSelectedOfficer] = useState<Option>();
   const [selectedAssessmentTypes, setSelectedAssessmentTypes] = useState<string[]>([]);
+  const [editable, setEditable] = useState<boolean>(true);
 
   const handleAssessmentTypesChange = (selectedItems: string[]) => {
     setSelectedAssessmentTypes(selectedItems);
@@ -39,23 +51,28 @@ export const HWCRComplaintAssessment: FC = () => {
   const [actionRequiredErrorMessage, setActionRequiredErrorMessage] = useState<string>("");
   const [justificationRequiredErrorMessage, setJustificationRequiredErrorMessage] = useState<string>("");
   const [assessmentRequiredErrorMessage, setAssessmentRequiredErrorMessage] = useState<string>("");
+  const [showContent, setShowContent] = useState<boolean>(false);
 
   const complaintData = useAppSelector(selectComplaint);
   const assessmentState = useAppSelector(selectAssessment);
   const { id = "", complaintType = "" } = useParams<ComplaintParams>();
-  const {
-    ownedByAgencyCode,
-  } = useAppSelector(selectComplaintCallerInformation);
+  const { ownedByAgencyCode } = useAppSelector(selectComplaintCallerInformation);
   const officersInAgencyList = useAppSelector(selectOfficersByAgency(ownedByAgencyCode?.agency));
-  const assignableOfficers: Option[] = officersInAgencyList !== null
-    ? officersInAgencyList.map((officer: Officer) => ({
-      value: officer.person_guid.person_guid,
-      label: `${officer.person_guid.first_name} ${officer.person_guid.last_name}`,
-    }))
-    : [];
+  const assignableOfficers: Option[] =
+    officersInAgencyList !== null
+      ? officersInAgencyList.map((officer: Officer) => ({
+          value: officer.person_guid.person_guid,
+          label: `${officer.person_guid.first_name} ${officer.person_guid.last_name}`,
+        }))
+      : [];
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
   };
+
+  const toggleEdit = () => {
+    setEditable(true);
+  };
+
   const handleActionRequiredChange = (selected: Option | null) => {
     if (selected) {
       setSelectedActionRequired(selected);
@@ -75,9 +92,7 @@ export const HWCRComplaintAssessment: FC = () => {
   const actionRequiredList = useAppSelector(selectYesNoCodeDropdown);
   const justificationList = useAppSelector(selectJustificationCodeDropdown);
   const assessmentTypeList = useAppSelector(selectAssessmentTypeCodeDropdown);
-  const {
-    personGuid,
-  } = useAppSelector(selectComplaintHeader(complaintType));
+  const { personGuid } = useAppSelector(selectComplaintHeader(complaintType));
 
   useEffect(() => {
     if (id && (!complaintData || complaintData.id !== id)) {
@@ -92,7 +107,6 @@ export const HWCRComplaintAssessment: FC = () => {
     }
   }, [complaintData]);
 
-
   // clear the redux state
   useEffect(() => {
     return () => {
@@ -101,7 +115,8 @@ export const HWCRComplaintAssessment: FC = () => {
   }, [dispatch]);
 
   const justificationLabelClass = selectedActionRequired?.value === "No" ? "" : "comp-outcome-hide";
-  const justificationEditClass = selectedActionRequired?.value === "No" ? "comp-details-input" : "comp-details-input comp-outcome-hide";
+  const justificationEditClass =
+    selectedActionRequired?.value === "No" ? "comp-details-input" : "comp-details-input comp-outcome-hide";
 
   const cancelConfirmed = () => {
     setSelectedDate(assessmentState.date);
@@ -110,6 +125,8 @@ export const HWCRComplaintAssessment: FC = () => {
     setSelectedJustification(assessmentState.justification);
     setSelectedAssessmentTypes(assessmentState.assessment_type);
     resetValidationErrors();
+    setEditable(false);
+    setShowContent(false);
   };
 
   const cancelButtonClick = () => {
@@ -122,10 +139,9 @@ export const HWCRComplaintAssessment: FC = () => {
           description: "Your changes will be lost.",
           cancelConfirmed,
         },
-      })
+      }),
     );
   };
-
 
   // save to redux if no errors.  Otherwise, display error message(s).
   const saveButtonClick = () => {
@@ -134,16 +150,16 @@ export const HWCRComplaintAssessment: FC = () => {
       officer: selectedOfficer,
       action_required: selectedActionRequired,
       justification: selectedJustification,
-      assessment_type: selectedAssessmentTypes
+      assessment_type: selectedAssessmentTypes,
     } as Assessment;
 
     if (!hasErrors()) {
       dispatch(setAssessment({ assessment: updatedAssessmentData }));
       ToggleSuccess(`Assessment has been saved`);
+      setEditable(false);
     } else {
       handleFormErrors();
     }
-  
   };
 
   const handleFormErrors = () => {
@@ -190,7 +206,7 @@ export const HWCRComplaintAssessment: FC = () => {
     }
 
     return hasErrors;
-  }
+  };
 
   return (
     <div className="comp-outcome-report-block">
@@ -198,72 +214,156 @@ export const HWCRComplaintAssessment: FC = () => {
       <div className="comp-outcome-report-complaint-assessment">
         <div className="comp-details-edit-container">
           <div className="comp-details-edit-column">
-            <div className="comp-details-label-checkbox-div-pair">
-              <label htmlFor="checkbox-div" className="comp-details-inner-content-label checkbox-label-padding">
-                Assessment
-              </label>
-              <ValidationCheckboxGroup errMsg={assessmentRequiredErrorMessage} options={assessmentTypeList} onCheckboxChange={handleAssessmentTypesChange} checkedValues={selectedAssessmentTypes}></ValidationCheckboxGroup> 
-            </div>
-          </div>
+            <div className="comp-details-edit-container">
+              <div className="comp-details-edit-column">
+                <div className="comp-details-label-checkbox-div-pair">
+                  <label
+                    htmlFor="checkbox-div"
+                    className="comp-details-inner-content-label checkbox-label-padding"
+                  >
+                    Assessment
+                  </label>
+                  {editable ?
+                  <ValidationCheckboxGroup
+                    errMsg={assessmentRequiredErrorMessage}
+                    options={assessmentTypeList}
+                    onCheckboxChange={handleAssessmentTypesChange}
+                    checkedValues={selectedAssessmentTypes}
+                  ></ValidationCheckboxGroup>
+                  :
+                  <div>
+                    {selectedAssessmentTypes.map((assesmentValue, index) => (
+        <div>
+          {assesmentValue}
         </div>
-        <div className="comp-details-edit-container">
-          <div className="comp-details-edit-column">
-            <div className="comp-details-label-input-pair">
-              <label htmlFor="action-required">Action required?</label>
-              <CompSelect id="action-required" className="comp-details-input" classNamePrefix="comp-select" options={actionRequiredList} enableValidation={true} errorMessage={actionRequiredErrorMessage} value={selectedActionRequired} placeholder="Select" onChange={(e) => handleActionRequiredChange(e)} />
+      ))}
+                  </div>}
+                </div>
+              </div>
+            </div>
+            <div className="comp-details-edit-container">
+              <div className="comp-details-edit-column">
+                <div className="comp-details-label-input-pair">
+                  <label htmlFor="action-required">Action required?</label>
+                  {editable ?
+                  <CompSelect
+                    id="action-required"
+                    className="comp-details-input"
+                    classNamePrefix="comp-select"
+                    options={actionRequiredList}
+                    enableValidation={true}
+                    errorMessage={actionRequiredErrorMessage}
+                    value={selectedActionRequired}
+                    placeholder="Select"
+                    onChange={(e) => handleActionRequiredChange(e)}
+                  /> :
+                  selectedActionRequired?.value
+}
+                </div>
+              </div>
+              <div className="comp-details-edit-column comp-details-right-column">
+                <div className="comp-details-label-input-pair">
+                  <label
+                    className={justificationLabelClass}
+                    htmlFor="justification"
+                  >
+                    Justification
+                  </label>
+                  <CompSelect
+                    id="justification"
+                    className={justificationEditClass}
+                    classNamePrefix="comp-select"
+                    options={justificationList}
+                    enableValidation={true}
+                    errorMessage={justificationRequiredErrorMessage}
+                    value={selectedJustification}
+                    placeholder="Select"
+                    onChange={(e) => handleJustificationChange(e)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="comp-details-edit-container">
+              <div className="comp-details-edit-column">
+                <div className="comp-details-label-input-pair">
+                  <label htmlFor="outcome-officer">Officer</label>
+                  {editable ? 
+                  <CompSelect
+                    id="outcome-officer"
+                    className="comp-details-input"
+                    classNamePrefix="comp-select"
+                    options={assignableOfficers}
+                    enableValidation={true}
+                    errorMessage={officerErrorMessage}
+                    value={selectedOfficer}
+                    placeholder="Select "
+                    onChange={(officer: any) => setSelectedOfficer(officer)}
+                  /> :
+                  <div
+                  data-initials-sm={getAvatarInitials(selectedOfficer?.label ?? "")}
+                  className="comp-orange-avatar-sm comp-details-inner-content"
+                >
+                  <span id="comp-review-required-officer" className="comp-padding-left-xs">
+                    {selectedOfficer?.label ?? ""}
+                  </span>
+                </div>
+}
+                </div>
+              </div>
+              <div className="comp-details-edit-column comp-details-right-column">
+                <div className="comp-details-label-input-pair">
+                  <label htmlFor="justification">Date</label>
+                  { editable ?
+                  <ValidationDatePicker
+                    id="complaint-outcome-date"
+                    selectedDate={selectedDate}
+                    onChange={handleDateChange}
+                    placeholder="Select Date"
+                    className="comp-details-edit-calendar-input" // Adjust class as needed
+                    classNamePrefix="comp-select" // Adjust class as needed
+                    errMsg={assessmentDateErrorMessage} // Pass error message if any
+                  /> :
+                  formatDate(`${selectedDate}`)
+}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="comp-details-edit-column comp-details-right-column">
-            <div className="comp-details-label-input-pair">
-              <label className={justificationLabelClass} htmlFor="justification">Justification</label>
-              <CompSelect id="justification" className={justificationEditClass} classNamePrefix="comp-select" options={justificationList} enableValidation={true} errorMessage={justificationRequiredErrorMessage} value={selectedJustification} placeholder="Select" onChange={(e) => handleJustificationChange(e)} />
-            </div>
-          </div>
-        </div>
-        <div className="comp-details-edit-container">
-          <div className="comp-details-edit-column">
-            <div className="comp-details-label-input-pair">
-              <label htmlFor="outcome-officer">Officer</label>
-              <CompSelect id="outcome-officer" className="comp-details-input" classNamePrefix="comp-select" options={assignableOfficers} enableValidation={true} errorMessage={officerErrorMessage} value={selectedOfficer} placeholder="Select "onChange={(officer: any) => setSelectedOfficer(officer)} />
-            </div>
-          </div>
-          <div className="comp-details-edit-column comp-details-right-column">
-            <div className="comp-details-label-input-pair">
-              <label htmlFor="justification">Date</label>
-              <ValidationDatePicker
-                id="complaint-outcome-date"
-                selectedDate={selectedDate}
-                onChange={handleDateChange}
-                placeholder="Select Date"
-                className="comp-details-edit-calendar-input" // Adjust class as needed
-                classNamePrefix="comp-select" // Adjust class as needed
-                errMsg={assessmentDateErrorMessage} // Pass error message if any
+          {!editable && (
+            <div className="comp-details-right-column">
+              <CompTextIconButton
+                buttonClasses="button-text"
+                text="Edit"
+                icon={BsPencil}
+                click={toggleEdit}
               />
             </div>
-          </div>
+          )}
         </div>
-        <div className="comp-outcome-report-container">
-          <div className="comp-outcome-report-actions">
-            <Button
-              id="outcome-cancel-button"
-              title="Cancel Outcome"
-              className="comp-outcome-cancel"
-              onClick={cancelButtonClick}
-            >
-              Cancel
-            </Button>
-            <Button
-              id="outcome-save-button"
-              title="Save Outcome"
-              className="comp-outcome-save"
-              onClick={saveButtonClick}
-            >
-              Save
-            </Button>
-          </div>
-        </div>
+        {editable && (
+            <div className="comp-outcome-report-container">
+              <div className="comp-outcome-report-actions">
+                <Button
+                  id="outcome-cancel-button"
+                  title="Cancel Outcome"
+                  className="comp-outcome-cancel"
+                  onClick={cancelButtonClick}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  id="outcome-save-button"
+                  title="Save Outcome"
+                  className="comp-outcome-save"
+                  onClick={saveButtonClick}
+                >
+                  Save
+                </Button>
+              </div>
+            </div>
+          )}
+
       </div>
     </div>
   );
 };
-
