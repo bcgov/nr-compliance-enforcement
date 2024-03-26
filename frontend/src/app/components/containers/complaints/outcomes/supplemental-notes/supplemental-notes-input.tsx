fe387402e5ebe1fc117cc26e17cd7d509ca70a07
Mike Sears <mike.2.sears@gov.bc.ca>
2024-03-25 20:52:13 -0700

    ux updated
    supplemental notes updated

---
+++ frontend/src/app/components/containers/complaints/outcomes/supplemental-notes/supplemental-notes-input.tsx
@@ -0,0 +1,159 @@
+import { FC, useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { ValidationTextArea } from "../../../../../common/validation-textarea";
+import { CompSelect } from "../../../../common/comp-select";
+import DatePicker from "react-datepicker";
+import Option from "../../../../../types/app/option";
+import { OfficerDto } from "../../../../../types/app/people/officer";
+import { useAppDispatch } from "../../../../../hooks/hooks";
+import { openModal } from "../../../../../store/reducers/app";
+import { CANCEL_CONFIRM } from "../../../../../types/modal/modal-types";
+
+type props = {
+  id: string;
+  notes: string;
+  currentOfficer: OfficerDto | null;
+
+  setShowInput: Function;
+};
+
+export const SupplementalNotesInput: FC<props> = ({ id, notes, currentOfficer, setShowInput }) => {
+  const maxCharacters = 4000;
+
+  const dispatch = useAppDispatch();
+
+  const [defaultOfficer, setDefaultOfficer] = useState<Option>();
+
+  const [currentNotes, setCurrentNotes] = useState(notes);
+  const [notesError, setNotesError] = useState("");
+
+  useEffect(() => {
+    if (currentOfficer) {
+      const {
+        id,
+        person: { firstName, lastName },
+      } = currentOfficer;
+      setDefaultOfficer({ label: `${firstName} ${lastName}`, value: id });
+    } else {
+      setDefaultOfficer({ label: "Unknown", value: "" });
+    }
+  }, [currentOfficer]);
+
+  const handleNotesChange = (input: string) => {
+    if (input?.trim().length <= maxCharacters) {
+      setNotesError("");
+      setCurrentNotes(input.trim());
+    }
+  };
+
+  const handleCancelChanges = () => {
+    dispatch(
+      openModal({
+        modalSize: "md",
+        modalType: CANCEL_CONFIRM,
+        data: {
+          title: "Cancel Changes?",
+          description: "Your changes will be lost.",
+          cancelConfirmed: () => {
+            setCurrentNotes(notes);
+            setShowInput(false);
+          },
+        },
+      }),
+    );
+  };
+
+  const handleSaveNotes = () => {
+    if (validateInput()) {
+      // dispatch(upsertNote(id, currentNotes))
+    } else {
+      setNotesError("Supporting notes required");
+    }
+  };
+
+  const validateInput = (): boolean => {
+    return !!currentNotes;
+  };
+
+  return (
+    <div className="comp-outcome-supporting-notes">
+      <div>
+        <ValidationTextArea
+          className="comp-form-control"
+          id="supporting-notes-textarea-id"
+          defaultValue={currentNotes}
+          placeholderText="Add supporting notes"
+          rows={4}
+          errMsg={notesError}
+          onChange={handleNotesChange}
+          maxLength={maxCharacters}
+        />
+      </div>
+      <div className="right-float">
+        {currentNotes.length} / {maxCharacters}
+      </div>
+      <div className="clear-right-float" />
+      <div className="comp-details-edit-container">
+        <div className="comp-details-edit-column">
+          <div
+            className="comp-details-label-input-pair"
+            id="officer-supporting-notes-pair-id"
+          >
+            <label
+              id="officer-supporting-notes-pair-id"
+              htmlFor="officer-supporting-notes-select-id"
+            >
+              Officer
+            </label>
+            <CompSelect
+              id="officer-supporting-notes-select-id"
+              classNamePrefix="comp-select"
+              className="comp-details-input"
+              isDisabled={true}
+              enableValidation={false}
+              value={defaultOfficer}
+            />
+          </div>
+        </div>
+        <div className="comp-details-edit-column comp-details-right-column">
+          <div
+            className="comp-details-label-input-pair"
+            id="supporting-notes-time-pair-id"
+          >
+            <label htmlFor="supporting-notes-time-pair-id">Date</label>
+            <DatePicker
+              id="supporting-notes-time-pair-id"
+              selected={new Date()}
+              onChange={(e) => e}
+              dateFormat="yyyy-MM-dd"
+              wrapperClassName="comp-details-edit-calendar-input datepicker-disabled"
+              readOnly
+              disabled
+              showIcon
+            />
+          </div>
+        </div>
+      </div>
+      <div className="comp-outcome-report-container">
+        <div className="comp-outcome-report-actions">
+          <Button
+            id="supporting-notes-cancel-button"
+            title="Cancel Supporting Notes"
+            className="comp-outcome-cancel"
+            onClick={handleCancelChanges}
+          >
+            Cancel
+          </Button>
+          <Button
+            id="supporting-notes-save-button"
+            title="Save Supporting Notes"
+            className="comp-outcome-save"
+            onClick={handleSaveNotes}
+          >
+            Add
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
