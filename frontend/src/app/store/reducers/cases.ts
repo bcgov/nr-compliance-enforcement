80528ccb7e3868b3dac93a339d81aa436f14e09b
Mike Sears <mike.2.sears@gov.bc.ca>
2024-04-05 11:33:30 -0700

    fixed: merge conflict error

--- frontend/src/app/store/reducers/cases.ts
+++ frontend/src/app/store/reducers/cases.ts
@@ -269,13 +269,13 @@ const parsePreventionResponse = async (
     }
     const updatedPreventionData = {
       date: actionDate,
-      officer: { label: officerFullName, value: actor },
+      officer: { key: officerFullName, value: actor },
       prevention_type: res.preventionDetails.actions
         .filter((action) => {
           return action.activeIndicator;
         })
         .map((action) => {
-          return { label: action.longDescription, value: action.actionCode };
+          return { key: action.longDescription, value: action.actionCode };
         }),
     } as Prevention;
     return updatedPreventionData;
@@ -335,7 +335,7 @@ const addAssessment =
         agencyCode: "COS",
         caseCode: "HWCR",
         assessmentDetails: {
-          actionNotRequired: assessment.action_required?.value === "No",
+          actionNotRequired: assessment.action_required === "No",
           actions: assessment.assessment_type.map((item) => {
             return {
               date: assessment.date,
@@ -401,7 +401,7 @@ const updateAssessment =
         agencyCode: "COS",
         caseCode: "HWCR",
         assessmentDetails: {
-          actionNotRequired: assessment.action_required?.value === "No",
+          actionNotRequired: assessment.action_required === "No",
           actionJustificationCode: assessment.justification?.value,
           actions: assessment.assessment_type.map((item) => {
             return {
@@ -474,21 +474,18 @@ const parseAssessmentResponse = async (
 
     const updatedAssessmentData = {
       date: actionDate,
-      officer: { label: officerFullName, value: actor },
-      action_required: {
-        label: res.assessmentDetails.actionNotRequired ? "No" : "Yes",
-        value: res.assessmentDetails.actionNotRequired ? "No" : "Yes",
-      },
+      officer: { key: officerFullName, value: actor },
+      action_required: res.assessmentDetails.actionNotRequired ? "No" : "Yes",
       justification: {
         value: res.assessmentDetails.actionJustificationCode,
-        label: res.assessmentDetails.actionJustificationLongDescription,
+        key: res.assessmentDetails.actionJustificationLongDescription,
       },
       assessment_type: res.assessmentDetails.actions
         .filter((action) => {
           return action.activeIndicator;
         })
         .map((action) => {
-          return { label: action.longDescription, value: action.actionCode };
+          return { key: action.longDescription, value: action.actionCode };
         }),
     } as Assessment;
     return updatedAssessmentData;
