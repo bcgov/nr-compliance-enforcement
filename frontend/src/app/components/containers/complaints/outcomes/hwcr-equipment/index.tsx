af0f4c26bdf5cfd369706165aeadac660cb9aaac
barrfalk <bfalk@salussystems.com>
2024-04-17 12:58:07 -0700

    Update index.tsx

--- frontend/src/app/components/containers/complaints/outcomes/hwcr-equipment/index.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/hwcr-equipment/index.tsx
@@ -4,7 +4,7 @@ import { BsPlusCircle } from "react-icons/bs";
 import { EquipmentForm } from "./equipment-form";
 import { EquipmentItem } from "./equipment-item";
 
-import "../../../../../../assets/sass/hwcr-equipment.scss"
+import "../../../../../../assets/sass/hwcr-equipment.scss";
 import { useParams } from "react-router-dom";
 
 import { useAppDispatch, useAppSelector } from "../../../../../hooks/hooks";
@@ -14,12 +14,11 @@ import { selectComplaintAssignedBy } from "../../../../../store/reducers/complai
 
 export const HWCREquipment: FC = memo(() => {
   const [showEquipmentForm, setShowEquipmentForm] = useState<boolean>(false);
-  const assigned = useAppSelector(selectComplaintAssignedBy);  
+  const assigned = useAppSelector(selectComplaintAssignedBy);
   // used to indicate which equipment's guid is in edit mode (only one can be edited at a time
   const { id = "" } = useParams<{ id: string; complaintType: string }>();
   const dispatch = useAppDispatch();
   const equipmentList = useAppSelector(selectEquipment);
-  
 
   useEffect(() => {
     if (id) {
@@ -42,36 +41,38 @@ export const HWCREquipment: FC = memo(() => {
     setShowEquipmentForm(false);
     setEditingGuid("");
   };
-  
 
   return (
     <div className="comp-outcome-report-block">
       <h6>Equipment</h6>
-      {equipmentList && equipmentList.length > 0 ? equipmentList.map((equipment)=>
-          editingGuid === equipment.id ?
-          <EquipmentForm
-            key={equipment.id}
-            equipment={equipment}
-            onSave={handleSave}
-            onCancel={handleCancel}
-          />
-          :
-          <EquipmentItem
-            key={equipment.id}
-            equipment={equipment}
-            onEdit={handleEdit}
-            isEditDisabled={!!editingGuid && editingGuid !== equipment.id}
-          />
-      ): null}
+      {equipmentList && equipmentList.length > 0
+        ? equipmentList.map((equipment) =>
+            editingGuid === equipment.id ? (
+              <EquipmentForm
+                key={equipment.id}
+                equipment={equipment}
+                onSave={handleSave}
+                onCancel={handleCancel}
+              />
+            ) : (
+              <EquipmentItem
+                key={equipment.id}
+                equipment={equipment}
+                onEdit={handleEdit}
+                isEditDisabled={!!editingGuid && editingGuid !== equipment.id}
+              />
+            ),
+          )
+        : null}
 
       {/* Add Equipment Form */}
-      {showEquipmentForm ?
+      {showEquipmentForm ? (
         <EquipmentForm
-        onSave={handleSave}
-        onCancel={handleCancel}
-        assignedOfficer={assigned}
+          onSave={handleSave}
+          onCancel={handleCancel}
+          assignedOfficer={assigned}
         />
-        :
+      ) : (
         <div className="comp-outcome-report-button">
           <Button
             id="outcome-report-add-equipment"
@@ -79,12 +80,11 @@ export const HWCREquipment: FC = memo(() => {
             variant="primary"
             onClick={() => setShowEquipmentForm(true)}
           >
-              <span>Add equipment</span>
+            <span>Add equipment</span>
             <BsPlusCircle />
           </Button>
         </div>
-      }
+      )}
     </div>
   );
 });
-  
