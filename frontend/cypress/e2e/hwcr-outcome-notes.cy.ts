f66a5e25a498a6b44bdc1dd2b6d0142ac041d3c5
Mike Sears <mike.2.sears@gov.bc.ca>
2024-04-22 13:57:47 -0700

    fix: re-enabled supplemental notes tests

--- frontend/cypress/e2e/hwcr-outcome-notes.cy.ts
+++ frontend/cypress/e2e/hwcr-outcome-notes.cy.ts
@@ -13,124 +13,124 @@ describe("HWCR Outcome Notes", () => {
     cy.kcLogout().kcLogin();
   });
 
-  // it("it requires valid user input", () => {
-  //   cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
+  it("it requires valid user input", () => {
+    cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
 
-  //   //This is required to make the tests re-runnable.  It's not great because it means it will only run the first time.
-  //   //If we ever get the ability to remove an prevention and education this test suite should be rewritten to remove this conditional
-  //   //and to add a test at the end to delete the prevention and education.
-  //   cy.get(".comp-hwcr-outcome-report").then(function ($outcome) {
-  //     if ($outcome.find("#outcome-report-add-note").length > 0) {
-  //       cy.get("#outcome-report-add-note").click();
-  //     } else {
-  //       cy.log("Test was previously run. Skip the Test");
-  //       this.skip();
-  //     }
-  //   });
+    //This is required to make the tests re-runnable.  It's not great because it means it will only run the first time.
+    //If we ever get the ability to remove an prevention and education this test suite should be rewritten to remove this conditional
+    //and to add a test at the end to delete the prevention and education.
+    cy.get(".comp-hwcr-outcome-report").then(function ($outcome) {
+      if ($outcome.find("#outcome-report-add-note").length > 0) {
+        cy.get("#outcome-report-add-note").click();
+      } else {
+        cy.log("Test was previously run. Skip the Test");
+        this.skip();
+      }
+    });
 
-  //   cy.get(".comp-outcome-supporting-notes").then(function () {
-  //     cy.validateComplaint("23-032454", "Black Bear");
+    cy.get(".comp-outcome-supporting-notes").then(function () {
+      cy.validateComplaint("23-032454", "Black Bear");
 
-  //     //click Save Button
-  //     cy.get("#supporting-notes-save-button").click();
+      //click Save Button
+      cy.get("#supporting-notes-save-button").click();
 
-  //     //validate error message
-  //     cy.get(".error-message").then(($error) => {
-  //       expect($error).to.contain.text("Supporting notes required");
-  //     });
-  //   });
-  // });
+      //validate error message
+      cy.get(".error-message").then(($error) => {
+        expect($error).to.contain.text("Supporting notes required");
+      });
+    });
+  });
 
-  // it("it can save note", () => {
-  //   cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
+  it("it can save note", () => {
+    cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
 
-  //   //This is required to make the tests re-runnable.  It's not great because it means it will only run the first time.
-  //   //If we ever get the ability to remove an assessment this test suite should be rewritten to remove this conditional
-  //   //and to add a test at the end to delete the assessment.
-  //   cy.get(".comp-hwcr-outcome-report").then(function ($outcome) {
-  //     if ($outcome.find("#outcome-report-add-note").length > 0) {
-  //       cy.get("#outcome-report-add-note").click();
+    //This is required to make the tests re-runnable.  It's not great because it means it will only run the first time.
+    //If we ever get the ability to remove an assessment this test suite should be rewritten to remove this conditional
+    //and to add a test at the end to delete the assessment.
+    cy.get(".comp-hwcr-outcome-report").then(function ($outcome) {
+      if ($outcome.find("#outcome-report-add-note").length > 0) {
+        cy.get("#outcome-report-add-note").click();
 
-  //       cy.validateComplaint("23-032454", "Black Bear");
+        cy.validateComplaint("23-032454", "Black Bear");
 
-  //       enterNote("This is test supporting note from Cypress");
+        enterNote("This is test supporting note from Cypress");
 
-  //       cy.get("#supporting-notes-save-button").click();
+        cy.get("#supporting-notes-save-button").click();
 
-  //       //validate the note
-  //       cy.get(".comp-outcome-supporting-notes").should(($div) => {
-  //         expect($div).to.contain.text("This is test supporting note from Cypress");
-  //       });
+        //validate the note
+        cy.get(".comp-outcome-supporting-notes").should(($div) => {
+          expect($div).to.contain.text("This is test supporting note from Cypress");
+        });
 
-  //       //validate the officer
-  //       cy.get("#comp-notes-officer").should(($div) => {
-  //         expect($div).to.contain.text("ENV TestAcct");
-  //       });
+        //validate the officer
+        cy.get("#comp-notes-officer").should(($div) => {
+          expect($div).to.contain.text("ENV TestAcct");
+        });
 
-  //       //validate the toast
-  //       cy.get(".Toastify__toast-body").then(($toast) => {
-  //         expect($toast).to.contain.text("Supplemental note created");
-  //       });
-  //     } else {
-  //       cy.log("Test was previously run. Skip the Test");
-  //       this.skip();
-  //     }
-  //   });
-  // });
+        //validate the toast
+        cy.get(".Toastify__toast-body").then(($toast) => {
+          expect($toast).to.contain.text("Supplemental note created");
+        });
+      } else {
+        cy.log("Test was previously run. Skip the Test");
+        this.skip();
+      }
+    });
+  });
 
-  // it("it can cancel note edits", () => {
-  //   cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
+  it("it can cancel note edits", () => {
+    cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
 
-  //   cy.validateComplaint("23-032454", "Black Bear");
+    cy.validateComplaint("23-032454", "Black Bear");
 
-  //   cy.get(".comp-outcome-supporting-notes").then(function ($notes) {
-  //     if ($notes.find("#notes-edit-button").length) {
-  //       cy.get("#notes-edit-button").click();
+    cy.get(".comp-outcome-supporting-notes").then(function ($notes) {
+      if ($notes.find("#notes-edit-button").length) {
+        cy.get("#notes-edit-button").click();
 
-  //       enterNote("This text will be cancelled by Cypress");
+        enterNote("This text will be cancelled by Cypress");
 
-  //       cy.get("#supporting-notes-cancel-button").click();
+        cy.get("#supporting-notes-cancel-button").click();
 
-  //       cy.get(".modal-footer > .btn-primary").click();
+        cy.get(".modal-footer > .btn-primary").click();
 
-  //       cy.get(".comp-outcome-supporting-notes").should(($div) => {
-  //         expect($div).to.not.contain.text("This text will be cancelled by Cypress");
-  //       });
-  //     } else {
-  //       cy.log("Notes Edit Button Not Found, did a previous test fail? Skip the Test");
-  //       this.skip();
-  //     }
-  //   });
-  // });
+        cy.get(".comp-outcome-supporting-notes").should(($div) => {
+          expect($div).to.not.contain.text("This text will be cancelled by Cypress");
+        });
+      } else {
+        cy.log("Notes Edit Button Not Found, did a previous test fail? Skip the Test");
+        this.skip();
+      }
+    });
+  });
 
-  // it("it can edit an existing note", () => {
-  //   cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
+  it("it can edit an existing note", () => {
+    cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
 
-  //   cy.validateComplaint("23-032454", "Black Bear");
+    cy.validateComplaint("23-032454", "Black Bear");
 
-  //   cy.get(".comp-outcome-supporting-notes").then(function ($notes) {
-  //     if ($notes.find("#notes-edit-button").length) {
-  //       cy.get("#notes-edit-button").click();
+    cy.get(".comp-outcome-supporting-notes").then(function ($notes) {
+      if ($notes.find("#notes-edit-button").length) {
+        cy.get("#notes-edit-button").click();
 
-  //       enterNote("This note is edited by Cypress");
+        enterNote("This note is edited by Cypress");
 
-  //       cy.get("#supporting-notes-save-button").click();
+        cy.get("#supporting-notes-save-button").click();
 
-  //       //Validate the text
-  //       cy.get(".comp-outcome-supporting-notes").should(($div) => {
-  //         expect($div).to.contain.text("This note is edited by Cypress");
-  //       });
+        //Validate the text
+        cy.get(".comp-outcome-supporting-notes").should(($div) => {
+          expect($div).to.contain.text("This note is edited by Cypress");
+        });
 
-  //       //validate the toast
-  //       cy.get(".Toastify__toast-body").then(($toast) => {
-  //         expect($toast).to.contain.text("Supplemental note updated");
-  //       });
-  //     } else {
-  //       cy.log("Note Edit Button Not Found, did a previous test fail? Skip the Test");
-  //       this.skip();
-  //     }
-  //   });
-  // });
+        //validate the toast
+        cy.get(".Toastify__toast-body").then(($toast) => {
+          expect($toast).to.contain.text("Supplemental note updated");
+        });
+      } else {
+        cy.log("Note Edit Button Not Found, did a previous test fail? Skip the Test");
+        this.skip();
+      }
+    });
+  });
 
   it("it can delete an existing note", () => {
     cy.navigateToDetailsScreen(COMPLAINT_TYPES.HWCR, "23-032454", true);
