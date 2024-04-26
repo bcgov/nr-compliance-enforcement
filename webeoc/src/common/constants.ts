1a9bba5e0fa32036fa3fd32d15b1523740acfaf5
barrfalk <bfalk@salussystems.com>
2024-04-26 16:10:04 -0700

    Making use of jetstream consumers

--- webeoc/src/common/constants.ts
+++ webeoc/src/common/constants.ts
@@ -1,6 +1,8 @@
 export const NATS_NEW_COMPLAINTS_TOPIC_NAME = "new_complaints";
+export const NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED = "new_complaints_delivered";
 export const NATS_UPDATED_COMPLAINTS_TOPIC_NAME = "updated_complaints";
 export const NEW_STAGING_COMPLAINTS_TOPIC_NAME = "new_staging_complaints";
+export const NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED = "new_staging_complaints_delivered";
 export const NATS_STREAM_NAME = "complaints_stream";
 
 export const STAGING_API_ENDPOINT = "staging-complaint";
--- webeoc/src/complaints-subscriber/complaints-subscriber.service.ts
+++ webeoc/src/complaints-subscriber/complaints-subscriber.service.ts
@@ -1,8 +1,12 @@
 import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import {
+  AckPolicy,
   connect,
+  DeliverPolicy,
   JetStreamClient,
   JetStreamManager,
+  JSONCodec,
+  Msg,
   NatsConnection,
   RetentionPolicy,
   StorageType,
@@ -10,8 +14,10 @@ import {
 } from "nats";
 import {
   NATS_NEW_COMPLAINTS_TOPIC_NAME,
+  NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED,
   NATS_STREAM_NAME,
   NEW_STAGING_COMPLAINTS_TOPIC_NAME,
+  NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED,
 } from "../common/constants";
 import { StagingComplaintsApiService } from "../staging-complaints-api-service/staging-complaints-api-service.service";
 import { Complaint } from "../types/Complaints";
@@ -20,13 +26,10 @@ import { Complaint } from "../types/Complaints";
 export class ComplaintsSubscriberService implements OnModuleInit {
   private readonly logger = new Logger(ComplaintsSubscriberService.name);
   private natsConnection: NatsConnection | null = null;
-  private js: JetStreamClient | null = null;
   private jsm: JetStreamManager | null = null; // For managing streams
-  private processedComplaintIds = new Set();
 
   constructor(private readonly service: StagingComplaintsApiService) {
     this.natsConnection = null;
-    this.js = null;
   }
 
   async onModuleInit() {
@@ -37,7 +40,6 @@ export class ComplaintsSubscriberService implements OnModuleInit {
       });
 
       // Set up JetStream context
-      this.js = this.natsConnection.jetstream();
       this.jsm = await this.natsConnection.jetstreamManager();
 
       // Set up or validate the stream configuration
@@ -77,47 +79,58 @@ export class ComplaintsSubscriberService implements OnModuleInit {
   private async subscribeToNewComplaintsFromWebEOC() {
     const sc = StringCodec();
 
-    const sub = await this.natsConnection.subscribe(NATS_NEW_COMPLAINTS_TOPIC_NAME);
-    (async () => {
-      for await (const msg of sub) {
-        try {
-          const complaintMessage: Complaint = JSON.parse(sc.decode(msg.data));
-          const incidentNumber = complaintMessage.incident_number;
-          // do not process the complaint if it already has been processed
-          if (this.processedComplaintIds.has(incidentNumber)) {
-            this.logger.debug(`Ignoring duplicate complaint: ${incidentNumber}`);
-            continue; // Skip this iteration if we've already processed this incident_number
-          }
-          this.processedComplaintIds.add(incidentNumber);
-          this.logger.debug(`Received complaint, adding to staging table: ${complaintMessage.incident_number}`);
-          await this.service.postComplaintToStaging(complaintMessage);
-        } catch (error) {
-          this.logger.error("Error processing received complaint:", error);
-        }
+    // Consumer configuration
+    const consumerConfig = {
+      filter_subject: NATS_NEW_COMPLAINTS_TOPIC_NAME,
+      deliver_subject: NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED,
+      ack_policy: AckPolicy.Explicit,
+    };
+
+    this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig);
+
+    const sub = this.natsConnection.subscribe(NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED);
+
+    const processMessage = async (msg: Msg) => {
+      const complaintMessage: Complaint = JSON.parse(sc.decode(msg.data));
+      this.logger.debug("Received complaint:", complaintMessage);
+      try {
+        await this.service.postComplaintToStaging(complaintMessage);
+      } catch (error) {
+        this.logger.error("Message not processed");
       }
-    })().catch((error) => {
-      this.logger.error("Error in NATS subscription:", error);
-    });
+    };
+
+    this.logger.debug("Listening for messages...");
+    for await (const message of await sub) {
+      await processMessage(message);
+    }
+
+    this.logger.error("No longer listening for new webeoc complaints");
   }
 
   // subscribe to new nats to listen for new complaints added to the staging table.  These will be moved to the operational Complaints table.
   private async subscribeToNewStagingComplaints() {
     const sc = StringCodec();
 
-    const sub = await this.natsConnection.subscribe(NEW_STAGING_COMPLAINTS_TOPIC_NAME);
+    // Consumer configuration
+    const consumerConfig = {
+      filter_subject: NEW_STAGING_COMPLAINTS_TOPIC_NAME,
+      deliver_subject: NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED,
+    };
 
-    (async () => {
-      for await (const msg of sub) {
-        try {
-          const stagingData = sc.decode(msg.data);
-          this.logger.debug(`New complaint in staging: ${stagingData}`);
-          await this.service.postComplaint(stagingData);
-        } catch (error) {
-          this.logger.error("Error processing complaint in staging:", error);
-        }
-      }
-    })().catch((error) => {
-      this.logger.error("Error in NATS subscription:", error);
-    });
+    this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig);
+
+    const sub = this.natsConnection.subscribe(NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED);
+
+    const processMessage = async (msg: Msg) => {
+      const stagingData = sc.decode(msg.data);
+      this.logger.debug(`New complaint in staging: ${stagingData}`);
+      await this.service.postComplaint(stagingData);
+    };
+
+    this.logger.debug("Listening for messages...");
+    for await (const message of await sub) {
+      await processMessage(message);
+    }
   }
 }
