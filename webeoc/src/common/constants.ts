5fc28d4361aaefe18cc9aabdff7091adcf2bcdaf
barrfalk <bfalk@salussystems.com>
2024-04-29 14:48:21 -0700

    Making better use of jetstream

--- webeoc/src/common/constants.ts
+++ webeoc/src/common/constants.ts
@@ -1,10 +1,11 @@
 export const NATS_NEW_COMPLAINTS_TOPIC_NAME = "new_complaints";
-export const NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED = "new_complaints_delivered";
+export const NATS_NEW_COMPLAINTS_TOPIC_CONSUMER = "new_complaints_consumer";
 export const NATS_UPDATED_COMPLAINTS_TOPIC_NAME = "updated_complaints";
 export const NEW_STAGING_COMPLAINTS_TOPIC_NAME = "new_staging_complaints";
-export const NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED = "new_staging_complaints_delivered";
+export const NEW_STAGING_COMPLAINTS_TOPIC_CONSUMER = "new_staging_complaints_consumer";
 export const NATS_STREAM_NAME = "complaints_stream";
 
 export const STAGING_API_ENDPOINT = "staging-complaint";
 export const COMPLAINT_API_ENDPOINT = "complaint-internal";
-export const NATS_QUEUE_GROUP = "complaints_queue_group";
+export const NATS_QUEUE_GROUP_STAGING = "staging_complaints_queue_group";
+export const NATS_QUEUE_GROUP_COMPLAINTS = "complaints_queue_group";
--- webeoc/src/complaints-subscriber/complaints-subscriber.service.ts
+++ webeoc/src/complaints-subscriber/complaints-subscriber.service.ts
@@ -2,20 +2,22 @@ import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import {
   AckPolicy,
   connect,
+  ConsumerConfig,
   JetStreamManager,
-  Msg,
   NatsConnection,
   RetentionPolicy,
   StorageType,
   StringCodec,
+  SubscriptionOptions,
 } from "nats";
 import {
+  NATS_NEW_COMPLAINTS_TOPIC_CONSUMER,
   NATS_NEW_COMPLAINTS_TOPIC_NAME,
-  NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED,
-  NATS_QUEUE_GROUP,
+  NATS_QUEUE_GROUP_COMPLAINTS,
+  NATS_QUEUE_GROUP_STAGING,
   NATS_STREAM_NAME,
+  NEW_STAGING_COMPLAINTS_TOPIC_CONSUMER,
   NEW_STAGING_COMPLAINTS_TOPIC_NAME,
-  NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED,
 } from "../common/constants";
 import { StagingComplaintsApiService } from "../staging-complaints-api-service/staging-complaints-api-service.service";
 import { Complaint } from "../types/Complaints";
@@ -25,9 +27,13 @@ export class ComplaintsSubscriberService implements OnModuleInit {
   private readonly logger = new Logger(ComplaintsSubscriberService.name);
   private natsConnection: NatsConnection | null = null;
   private jsm: JetStreamManager | null = null; // For managing streams
-  private _queue_group_config = {
-    queue: NATS_QUEUE_GROUP,
-  };
+  private _queue_group_config_staging = {
+    queue: NATS_QUEUE_GROUP_STAGING,
+  } as SubscriptionOptions;
+
+  private _queue_group_config_complaints = {
+    queue: NATS_QUEUE_GROUP_COMPLAINTS,
+  } as SubscriptionOptions;
 
   constructor(private readonly service: StagingComplaintsApiService) {
     this.natsConnection = null;
@@ -61,7 +67,7 @@ export class ComplaintsSubscriberService implements OnModuleInit {
       retention: RetentionPolicy.Limits,
       maxAge: 0,
       storage: StorageType.Memory,
-      duplicateWindow: 10 * 60 * 1000000000, // 10 minutes in nanoseconds
+      duplicateWindow: 10 * 60 * 1000000000, // 10 minutes in nanoseconds,
     };
 
     try {
@@ -83,28 +89,36 @@ export class ComplaintsSubscriberService implements OnModuleInit {
 
     // Consumer configuration
     const consumerConfig = {
-      filter_subject: NATS_NEW_COMPLAINTS_TOPIC_NAME,
-      deliver_subject: NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED,
-      deliver_group: NATS_QUEUE_GROUP,
+      name: NATS_NEW_COMPLAINTS_TOPIC_CONSUMER,
       ack_policy: AckPolicy.Explicit,
-    };
-    await this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig);
+      filter_subject: NATS_NEW_COMPLAINTS_TOPIC_NAME,
+      deliver_group: NATS_QUEUE_GROUP_STAGING,
+    } as Partial<ConsumerConfig>;
 
-    const sub = this.natsConnection.subscribe(NATS_NEW_COMPLAINTS_TOPIC_NAME_DELIVERED, this._queue_group_config);
-
-    const processMessage = async (msg: Msg) => {
-      const complaintMessage: Complaint = JSON.parse(sc.decode(msg.data));
-      this.logger.debug("Received complaint:", complaintMessage);
-      try {
-        await this.service.postComplaintToStaging(complaintMessage);
-      } catch (error) {
-        this.logger.error("Message not processed");
-      }
-    };
-
-    this.logger.debug("Listening for messages...");
-    for await (const message of await sub) {
-      await processMessage(message);
+    try {
+      // Add consumer configuration to JetStream Manager
+      let consumer = null;
+      await this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig).then(async (info) => {
+        consumer = await this.natsConnection.jetstream().consumers.get(NATS_STREAM_NAME, info.name);
+      });
+      (async () => {
+        try {
+          for await (const message of await consumer.consume()) {
+            const complaintMessage: Complaint = JSON.parse(sc.decode(message.data));
+            this.logger.debug("Received complaint:", complaintMessage);
+            try {
+              await this.service.postComplaintToStaging(complaintMessage);
+              message.ack();
+            } catch (error) {
+              this.logger.error("Message not processed");
+            }
+          }
+        } catch (error) {
+          this.logger.error("Error consuming messages:", error);
+        }
+      })();
+    } catch (error) {
+      this.logger.error("Failed to subscribe or process messages:", error);
     }
 
     this.logger.error("No longer listening for new webeoc complaints");
@@ -113,27 +127,40 @@ export class ComplaintsSubscriberService implements OnModuleInit {
   // subscribe to new nats to listen for new complaints added to the staging table.  These will be moved to the operational Complaints table.
   private async subscribeToNewStagingComplaints() {
     const sc = StringCodec();
-
     // Consumer configuration
     const consumerConfig = {
+      name: NEW_STAGING_COMPLAINTS_TOPIC_CONSUMER,
+      ack_policy: AckPolicy.Explicit,
       filter_subject: NEW_STAGING_COMPLAINTS_TOPIC_NAME,
-      deliver_subject: NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED,
-      deliver_group: NATS_QUEUE_GROUP,
-    };
+      deliver_group: NATS_QUEUE_GROUP_COMPLAINTS,
+    } as Partial<ConsumerConfig>;
 
-    this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig);
-
-    const sub = this.natsConnection.subscribe(NEW_STAGING_COMPLAINTS_TOPIC_NAME_DELIVERED, this._queue_group_config);
-
-    const processMessage = async (msg: Msg) => {
-      const stagingData = sc.decode(msg.data);
-      this.logger.debug(`New complaint in staging: ${stagingData}`);
-      await this.service.postComplaint(stagingData);
-    };
-
-    this.logger.debug("Listening for messages...");
-    for await (const message of await sub) {
-      await processMessage(message);
+    try {
+      // Add consumer configuration to JetStream Manager
+      let consumer = null;
+      await this.jsm.consumers.add(NATS_STREAM_NAME, consumerConfig).then(async (info) => {
+        consumer = await this.natsConnection.jetstream().consumers.get(NATS_STREAM_NAME, info.name);
+      });
+      (async () => {
+        try {
+          for await (const message of await consumer.consume()) {
+            const stagingData = sc.decode(message.data);
+            this.logger.debug("Received complaint:", stagingData);
+            try {
+              await this.service.postComplaint(stagingData);
+              message.ack();
+            } catch (error) {
+              this.logger.error("Message not processed");
+            }
+          }
+        } catch (error) {
+          this.logger.error("Error consuming messages:", error);
+        }
+      })();
+    } catch (error) {
+      this.logger.error("Failed to subscribe or process messages:", error);
     }
+
+    this.logger.error("No longer listening for new webeoc complaints");
   }
 }
