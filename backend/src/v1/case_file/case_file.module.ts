84783643d85f55f222c9616ecf327050f9839a57
barrfalk <bfalk@salussystems.com>
2024-05-28 14:46:21 -0700

    Pass jest test

--- backend/src/v1/case_file/case_file.module.ts
+++ backend/src/v1/case_file/case_file.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AutomapperModule } from "@automapper/nestjs";
 import { CaseFileController } from "./case_file.controller";
 import { CaseFileService } from "./case_file.service";
+import { CodeTableModule } from "../code-table/code-table.module";
+import { ComplaintModule } from "../complaint/complaint.module";
 
 @Module({
-  imports: [AutomapperModule],
+  imports: [AutomapperModule, CodeTableModule, ComplaintModule],
   controllers: [CaseFileController],
   providers: [CaseFileService],
 })
