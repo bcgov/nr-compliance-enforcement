3f48d1279276b06e3ea6685757dc5d8cb3df2b67
barrfalk <bfalk@salussystems.com>
2024-06-06 12:47:18 -0700

    Feedback updates

--- frontend/src/app/components/containers/layout/environment-banner.tsx
+++ frontend/src/app/components/containers/layout/environment-banner.tsx
@@ -6,9 +6,7 @@ interface Props {
 
 const EnvironmentBanner: FC<Props> = ({ environmentName }) => {
   return (
-    <div className="environment-banner">
-      This is a {environmentName} environment: data entered here will not transfer to the LIVE application.
-    </div>
+    <div className="environment-banner">This is a {environmentName} environment: data entered here could be lost.</div>
   );
 };
 
