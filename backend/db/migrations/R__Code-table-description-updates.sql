9ca7492fd1bbb1aa41880e2233f30f9ca400ee41
afwilcox <alecwilcox@gmail.com>
2024-06-07 15:36:23 -0700

    Flyway script refactor

--- backend/db/migrations/R__Code-table-description-updates.sql
+++ backend/db/migrations/R__Code-table-description-updates.sql
@@ -1,5 +1,3 @@
--- species
-UPDATE species_code set short_description = 'Grizzly Bear', long_description = 'Grizzly Bear' WHERE species_code = 'GRZBEAR';
-UPDATE species_code set short_description = 'Hog/Pig/Boar (Feral)', long_description = 'Hog/Pig/Boar (Feral)' WHERE species_code = 'FERALHOG';
-UPDATE species_code set short_description = 'Mountain Goat', long_description = 'Mountain Goat' WHERE species_code = 'MTNGOAT';
-UPDATE species_code set short_description = 'River Otter', long_description = 'River Otter' WHERE species_code = 'RVROTTER';
+--
+-- Moved to R__Code-Table-Data.sql
+--
