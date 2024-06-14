e22045525d04cc98e21597ddd2265231210cddcf
barrfalk <bfalk@salussystems.com>
2024-06-14 14:47:00 -0700

    Not authorized error message

--- frontend/src/app/components/containers/pages/not-authorized.tsx
+++ frontend/src/app/components/containers/pages/not-authorized.tsx
@@ -1,3 +1,45 @@
-export const NotAuthorized = () => {
-  return <h1>You are not authorized to view this page.</h1>;
+import { FC, ReactNode } from "react";
+import logoLg from "../../../../assets/images/branding/BCgov-lg.png";
+import { Footer } from "../../containers/layout";
+import { BsPersonFillSlash } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+type props = {
+  children?: ReactNode;
 };
+
+const NotAuthorized: FC<props> = () => {
+  return (
+    <div className="comp-app-container fixed-header">
+      {/* <!-- --> */}
+
+      <div className="comp-header">
+        <Link
+          className="comp-header-logo"
+          to="/"
+        >
+          <picture>
+            <source srcSet={logoLg}></source>
+            <img
+              src={logoLg}
+              alt={"Government of British Columbia"}
+            />
+          </picture>
+          NatComplaints
+        </Link>
+      </div>
+
+      <div className="error-container">
+        <div className="message">
+          <BsPersonFillSlash />
+          <h1 className="comp-padding-top-25">Sorry, you are not authorized to access this site</h1>If you think you
+          should have access, please contact <a href="mailto:CEDS@gov.bc.ca">CEDS@gov.bc.ca</a>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default NotAuthorized;
