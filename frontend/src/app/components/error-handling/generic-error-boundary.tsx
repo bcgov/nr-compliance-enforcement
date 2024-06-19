367a91cd78ad378f63699a026ed6c383390a6c6f
Mike Sears <mike.2.sears@gov.bc.ca>
2024-06-18 17:35:54 -0700

    fix: removed Link component from generic-error-boundary component

--- frontend/src/app/components/error-handling/generic-error-boundary.tsx
+++ frontend/src/app/components/error-handling/generic-error-boundary.tsx
@@ -3,7 +3,6 @@ import { useErrorBoundary } from "../../hooks/error-boundary";
 import logoLg from "../../../assets/images/branding/BCgov-lg.png";
 import { Footer } from "../containers/layout";
 import { TbFaceIdError } from "react-icons/tb";
-import { Link } from "react-router-dom";
 
 type props = {
   children?: ReactNode;
@@ -18,19 +17,19 @@ const GenericErrorBoundary: FC<props> = ({ children }) => {
         {/* <!-- --> */}
 
         <div className="comp-header">
-          {/* <Link
+          <a
+            href="/"
             className="comp-header-logo"
-            to="/"
-          > */}
-          <picture>
-            <source srcSet={logoLg}></source>
-            <img
-              src={logoLg}
-              alt={"Government of British Columbia"}
-            />
-          </picture>
-          NatComplaints
-          {/* </Link> */}
+          >
+            <picture>
+              <source srcSet={logoLg}></source>
+              <img
+                src={logoLg}
+                alt={"Government of British Columbia"}
+              />
+            </picture>
+            NatComplaints
+          </a>
           <div className="comp-header-content">
             <div className="comp-header-left">{/* <!-- future left hand content --> */}</div>
             <div className="comp-header-right">
