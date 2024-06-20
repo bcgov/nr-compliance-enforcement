7614e1ee5738b0170509d2125a0ecd262b586c99
Dmitri Korin <dmitri.korin@gov.bc.ca>
2024-06-20 10:48:46 -0700

    Address Sonar Cloud issue

--- backend/src/v1/cos_geo_org_unit/entities/cos_geo_org_unit.entity.ts
+++ backend/src/v1/cos_geo_org_unit/entities/cos_geo_org_unit.entity.ts
@@ -52,7 +52,7 @@ export class CosGeoOrgUnit {
     description: "Indicates if the unit is a COSHQ Office.",
   })
   @Column("boolean", { name: "administrative_office_ind" })
-  administrative_office_ind: Boolean;
+  administrative_office_ind: boolean;
 
   constructor(zone_code: string) {
     this.zone_code = zone_code;
