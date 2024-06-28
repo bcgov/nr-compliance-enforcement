27e6b7517047eccd5be2da6c7650ce0b294b54a2
afwilcox <alecwilcox@gmail.com>
2024-06-28 13:38:48 -0700

    Reset ERS template

--- backend/db/migrations/R__reset-complaint-templates.sql
+++ backend/db/migrations/R__reset-complaint-templates.sql
@@ -1,15 +1,3 @@
 --
--- Reset the CDOGS template hashes every time the database scripts run 
--- this is to make sure that the most up to date template is used
+-- Not Required
 --
-UPDATE "configuration"
-SET
-  configuration_value = ''
-WHERE
-  configuration_code = 'HWCTMPLATE';
-
-UPDATE "configuration"
-SET
-  configuration_value = ''
-WHERE
-  configuration_code = 'ERSTMPLATE';
