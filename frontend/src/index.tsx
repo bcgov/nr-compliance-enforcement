186a0278af6170c20b27b218fddd06f02b3f028b
barrfalk <bfalk@salussystems.com>
2024-07-16 20:50:58 -0700

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -35,5 +35,5 @@ UserService.initKeycloak(onAuthenticatedCallback);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
