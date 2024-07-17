aad21cf0e855b6a75027dcdf43867ad4c3c9e270
jdunsdon <twenty15creative@gmail.com>
2024-07-17 09:02:50 -0700

    WIP

--- frontend/src/app/components/containers/complaints/details/call-details.tsx
+++ frontend/src/app/components/containers/complaints/details/call-details.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Badge, Card } from "react-bootstrap";
+import { Badge, Card, Col, Row } from "react-bootstrap";
 import { useAppSelector } from "../../../../hooks/hooks";
 import { formatDate, formatTime, renderCoordinates } from "../../../../common/methods";
 import { Coordinates } from "../../../../types/app/coordinate-type";
@@ -7,6 +7,7 @@ import { ComplaintDetailsAttractant } from "../../../../types/complaints/details
 import { selectComplaintDetails } from "../../../../store/reducers/complaints";
 import COMPLAINT_TYPES from "../../../../types/app/complaint-types";
 import { ComplaintDetails } from "../../../../types/complaints/details/complaint-details";
+import { render } from "react-dom";
 
 interface ComplaintHeaderProps {
   complaintType: string;
@@ -30,19 +31,22 @@ export const CallDetails: FC<ComplaintHeaderProps> = ({ complaintType }) => {
 
   return (
     <section className="comp-details-section">
-      <h3>Call Details</h3>
-
       <Card>
         <Card.Body>
+          <Card.Title as="h3">Call Details</Card.Title>
+
           {/* General Call Information */}
-          <dl className="comp-call-details-group">
-            <div>
+          <Row
+            as="dl"
+            className="comp-call-details-group"
+          >
+            <Col xs={12}>
               <dt>Complaint Description</dt>
               <dd>
                 <pre id="comp-details-description">{details}</pre>
               </dd>
-            </div>
-            <div>
+            </Col>
+            <Col xs={6}>
               <dt>Incident Date/Time</dt>
               <dd
                 className="comp-date-time-value"
@@ -56,11 +60,11 @@ export const CallDetails: FC<ComplaintHeaderProps> = ({ complaintType }) => {
                 <i className="bi bi-clock comp-margin-left-xxs comp-margin-right-xxs"></i>
                 {formatTime(incidentDateTime?.toString())}
               </dd>
-            </div>
+            </Col>
 
             {/* HWCR Details */}
             {complaintType === COMPLAINT_TYPES.HWCR && (
-              <div>
+              <Col xs={6}>
                 <dt>Attractants</dt>
                 <dd className="comp-details-attractants">
                   {!attractants ||
@@ -77,17 +81,23 @@ export const CallDetails: FC<ComplaintHeaderProps> = ({ complaintType }) => {
                       );
                     })}
                 </dd>
-              </div>
+              </Col>
             )}
 
             {/* ERS Details */}
             {complaintType === COMPLAINT_TYPES.ERS && (
-              <>
-                <div>
+              <Row>
+                <Col
+                  xs={12}
+                  md={6}
+                >
                   <dt>Violation In Progress</dt>
                   <dd id="comp-details-violation-in-progress">{violationInProgress ? "Yes" : "No"}</dd>
-                </div>
-                <div>
+                </Col>
+                <Col
+                  xs={12}
+                  md={6}
+                >
                   <dt>Violation Observed</dt>
                   <dd
                     id="comp-details-violation-observed"
@@ -95,49 +105,69 @@ export const CallDetails: FC<ComplaintHeaderProps> = ({ complaintType }) => {
                   >
                     {violationObserved ? "Yes" : "No"}
                   </dd>
-                </div>
-              </>
+                </Col>
+              </Row>
             )}
-          </dl>
 
-          {/* Location Information */}
-          <dl className="comp-call-details-group">
-            <div>
-              <dt>Complaint Location</dt>
-              <dd id="comp-details-location">{location}</dd>
-            </div>
-            <div>
-              <dt>Location Description</dt>
-              <dd id="comp-details-location-description">{locationDescription}</dd>
-            </div>
-            <div>
-              <dt>Latitude/Longitude</dt>
-              <dd className="comp-lat-long">
-                <span id="call-details-y-coordinate">{renderCoordinates(coordinates, Coordinates.Latitude)}</span>
-                <span id="call-details-x-coordinate">{renderCoordinates(coordinates, Coordinates.Longitude)}</span>
-              </dd>
-            </div>
-          </dl>
+            {/* Location Information */}
+            <>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Complaint Location</dt>
+                <dd id="comp-details-location">{location}</dd>
+              </Col>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Latitude/Longitude</dt>
+                <dd className="comp-lat-long">
+                  <span id="call-details-y-coordinate">{renderCoordinates(coordinates, Coordinates.Latitude)}</span>
+                  <span id="call-details-x-coordinate">{renderCoordinates(coordinates, Coordinates.Longitude)}</span>
+                </dd>
+              </Col>
+              {locationDescription && (
+                <Col xs={12}>
+                  <dt>Location Description</dt>
+                  <dd id="comp-details-location-description">{locationDescription}</dd>
+                </Col>
+              )}
+            </>
 
-          {/* Other Location Details */}
-          <dl className="comp-call-details-group">
-            <div>
-              <dt>Community</dt>
-              <dd id="comp-details-community">{area}</dd>
-            </div>
-            <div>
-              <dt>Office</dt>
-              <dd id="comp-details-office">{office}</dd>
-            </div>
-            <div>
-              <dt>Zone</dt>
-              <dd id="comp-details-zone">{zone}</dd>
-            </div>
-            <div>
-              <dt>Region</dt>
-              <dd id="comp-details-region">{region}</dd>
-            </div>
-          </dl>
+            {/* Other Location Details */}
+            <>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Community</dt>
+                <dd id="comp-details-community">{area}</dd>
+              </Col>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Office</dt>
+                <dd id="comp-details-office">{office}</dd>
+              </Col>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Zone</dt>
+                <dd id="comp-details-zone">{zone}</dd>
+              </Col>
+              <Col
+                xs={12}
+                md={6}
+              >
+                <dt>Region</dt>
+                <dd id="comp-details-region">{region}</dd>
+              </Col>
+            </>
+          </Row>
         </Card.Body>
       </Card>
     </section>
--- frontend/src/app/components/containers/complaints/details/caller-information.tsx
+++ frontend/src/app/components/containers/complaints/details/caller-information.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { useAppSelector } from "../../../../hooks/hooks";
 import { selectComplaintCallerInformation } from "../../../../store/reducers/complaints";
 import { formatPhoneNumber } from "react-phone-number-input/input";
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 export const CallerInformation: FC = () => {
   const { name, primaryPhone, secondaryPhone, alternatePhone, address, email, reportedByCode } = useAppSelector(
@@ -11,39 +11,60 @@ export const CallerInformation: FC = () => {
 
   return (
     <section className="comp-details-section">
-      <h3>Caller Information</h3>
       <Card>
         <Card.Body>
-          <dl>
-            <div>
+          <Card.Title as="h3">Caller Information</Card.Title>
+          <Row as="dl">
+            <Col
+              xs={12}
+              md={4}
+            >
               <dt>Name</dt>
               <dd id="comp-details-name">{name}</dd>
-            </div>
-            <div>
-              <dt>Primary Phone</dt>
-              <dd id="comp-details-phone">{formatPhoneNumber(primaryPhone !== undefined ? primaryPhone : "")}</dd>
-            </div>
-            <div>
-              <dt>Alternative Phone 1</dt>
-              <dd id="comp-details-phone-1">{formatPhoneNumber(secondaryPhone !== undefined ? secondaryPhone : "")}</dd>
-            </div>
-            <div>
-              <dt>Alternative Phone 2</dt>
-              <dd id="comp-details-phone-2">{formatPhoneNumber(alternatePhone !== undefined ? alternatePhone : "")}</dd>
-            </div>
-            <div>
-              <dt>Address</dt>
-              <dd id="comp-details-address">{address}</dd>
-            </div>
-            <div>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
               <dt>Email</dt>
               <dd id="comp-details-email">{email}</dd>
-            </div>
-            <div>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
               <dt>Organization Reporting the Complaint</dt>
               <dd id="comp-details-reported">{reportedByCode?.longDescription}</dd>
-            </div>
-          </dl>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
+              <dt>Primary Phone</dt>
+              <dd id="comp-details-phone">{formatPhoneNumber(primaryPhone !== undefined ? primaryPhone : "")}</dd>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
+              <dt>Alternative Phone 1</dt>
+              <dd id="comp-details-phone-1">{formatPhoneNumber(secondaryPhone !== undefined ? secondaryPhone : "")}</dd>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
+              <dt>Alternative Phone 2</dt>
+              <dd id="comp-details-phone-2">{formatPhoneNumber(alternatePhone !== undefined ? alternatePhone : "")}</dd>
+            </Col>
+            <Col
+              xs={12}
+              md={4}
+            >
+              <dt>Address</dt>
+              <dd id="comp-details-address">{address}</dd>
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </section>
