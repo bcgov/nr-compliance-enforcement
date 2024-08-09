ee0ada0da7899b2fdf6ce679b8da6f6f09eca75e
gregorylavery <100631366+gregorylavery@users.noreply.github.com>
2024-08-09 11:16:09 -0700

    Changes the phone number returned to be only 14 digits max.

---
+++ migrations/migrations/R__0.25.0__CE-965.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION public.format_phone_number(phone_number text)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    formatted_phone_number TEXT;
+BEGIN
+    -- Remove all non-digit characters
+    formatted_phone_number := regexp_replace(phone_number, '[^0-9]', '', 'g');
+    IF (formatted_phone_number IS NULL or (length(formatted_phone_number) = 0)) then
+		return null;
+    END IF;
+    -- Check if the first character is '1'
+    IF left(formatted_phone_number, 1) = '1' THEN
+        -- Add '+' in front of the phone number
+        RETURN '+' || left(formatted_phone_number, 14);
+    ELSE
+        -- Add '+1' in front of the phone number
+        RETURN '+1' || formatted_phone_number;
+    END IF;
+END;
+$function$
+;
+UPDATE configuration 
+SET    configuration_value = configuration_value::int + 1
+WHERE  configuration_code = 'CDTABLEVER';
+
