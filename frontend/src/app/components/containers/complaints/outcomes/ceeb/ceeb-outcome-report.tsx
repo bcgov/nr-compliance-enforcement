701cd8052a5836d0d000bf8a8385934eecef5f66
Mike Sears <mike.2.sears@gov.bc.ca>
2024-09-06 10:49:40 -0700

    feat: fist commit

--- frontend/src/app/components/containers/complaints/outcomes/ceeb/ceeb-outcome-report.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/ceeb/ceeb-outcome-report.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { OutcomeAttachments } from "../outcome-attachments";
+import { CeebAuthoization } from "./ceeb-authorization/authroization";
 
 export const CeebOutcomeReport: FC = () => {
   return (
     <section className="comp-details-body comp-container comp-hwcr-outcome-report">
       <hr className="comp-details-body-spacer"></hr>
+      <CeebAuthoization />
       <OutcomeAttachments showAddButton={true} />
     </section>
   );
