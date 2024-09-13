114f9879fbc2f68c8570c89851c30f5910937c03
afwilcox <alecwilcox@gmail.com>
2024-09-13 09:06:13 -0700

    Move DML into Repeatable script

--- migrations/test-only-migrations/R__Test-Data-Creation.sql
+++ migrations/test-only-migrations/R__Test-Data-Creation.sql
@@ -1478,3 +1478,497 @@ INSERT INTO public.action_taken
 (action_taken_guid, complaint_identifier,complaint_update_guid, action_details_txt, logged_by_txt,action_utc_timestamp, create_user_id, create_utc_timestamp, update_user_id, update_utc_timestamp)
 VALUES('eafd661b-4c18-4aa6-a39e-170a5462d4ad'::uuid, '24-000261','bafd661b-4c18-4aa6-a39e-a70a5462d4ad'::uuid,'action_details_txt','Logged by','2024-07-25 21:18:23.336', 'FLYWAY', '2024-07-11 21:18:23.336', 'FLYWAY', '2024-07-11 21:18:23.336')
 ON CONFLICT DO NOTHING;
+
+-- CE-946 Add Auth Guids so people show up properly in assignment modals
+
+UPDATE officer
+SET
+  auth_user_guid = 'ef0bc810-58a7-4080-ad7a-b5bbdd0d2efe'
+WHERE
+  user_id = 'NBASKERV';
+
+UPDATE officer
+SET
+  auth_user_guid = '92a81ac4-d767-414f-a759-b0be2373a072'
+WHERE
+  user_id = 'HOLSON';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e6c47d9c-39d2-48c1-a0c9-2c0fa309ac66'
+WHERE
+  user_id = 'VYATES';
+
+UPDATE officer
+SET
+  auth_user_guid = '382e7b5f-57c0-4a89-8836-7dbf8e2bc412'
+WHERE
+  user_id = 'AGUPTA';
+
+UPDATE officer
+SET
+  auth_user_guid = '6def8986-1b18-4205-8283-d6fd633b3eee'
+WHERE
+  user_id = 'BFALK';
+
+UPDATE officer
+SET
+  auth_user_guid = '91959e79-cafa-4b21-bfc6-60012d3e0790'
+WHERE
+  user_id = 'CNESMITH';
+
+UPDATE officer
+SET
+  auth_user_guid = '93a3dee6-6ee6-4f74-8539-69f122648ab1'
+WHERE
+  user_id = 'MSEARS';
+
+UPDATE officer
+SET
+  auth_user_guid = '287d4e72-8409-4dd1-991a-8b1117b8eb2a'
+WHERE
+  user_id = 'AWILCOX';
+
+UPDATE officer
+SET
+  auth_user_guid = '864f2a4a-135e-481c-bec6-2d58d966eb82'
+WHERE
+  user_id = 'TSPRADO';
+
+UPDATE officer
+SET
+  auth_user_guid = 'ef0bc810-58a7-4080-ad7a-b5bbdd0d2efe'
+WHERE
+  user_id = 'NBASKERV';
+
+UPDATE officer
+SET
+  auth_user_guid = '92a81ac4-d767-414f-a759-b0be2373a072'
+WHERE
+  user_id = 'HOLSON';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e6c47d9c-39d2-48c1-a0c9-2c0fa309ac66'
+WHERE
+  user_id = 'VYATES';
+
+UPDATE officer
+SET
+  auth_user_guid = '382e7b5f-57c0-4a89-8836-7dbf8e2bc412'
+WHERE
+  user_id = 'AGUPTA';
+
+UPDATE officer
+SET
+  auth_user_guid = '6def8986-1b18-4205-8283-d6fd633b3eee'
+WHERE
+  user_id = 'BFALK';
+
+UPDATE officer
+SET
+  auth_user_guid = '91959e79-cafa-4b21-bfc6-60012d3e0790'
+WHERE
+  user_id = 'CNESMITH';
+
+UPDATE officer
+SET
+  auth_user_guid = 'a6620175-84d0-436b-b13e-d97457d69588'
+WHERE
+  user_id = 'M2SEARS';
+
+UPDATE officer
+SET
+  auth_user_guid = '287d4e72-8409-4dd1-991a-8b1117b8eb2a'
+WHERE
+  user_id = 'AWILCOX';
+
+UPDATE officer
+SET
+  auth_user_guid = '864f2a4a-135e-481c-bec6-2d58d966eb82'
+WHERE
+  user_id = 'TSPRADO';
+
+UPDATE officer
+SET
+  auth_user_guid = 'b4bb40f5-ee23-4c99-b63c-e741ce61b589'
+WHERE
+  user_id = 'JOCHARTR';
+
+UPDATE officer
+SET
+  auth_user_guid = '0a24b8a9-cb08-4ae2-8287-d5caa3d79007'
+WHERE
+  user_id = 'ADLAI';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c3a081db-a974-449c-9947-8ac8522ba3af'
+WHERE
+  user_id = 'STRUONG';
+
+UPDATE officer
+SET
+  auth_user_guid = '87a1058f-97c0-4bd7-a07c-c31fe40a2230'
+WHERE
+  user_id = 'DVUIA';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd072fbaa-8668-412c-8dc1-a19e19cb9622'
+WHERE
+  user_id = 'DKORIN';
+
+UPDATE officer
+SET
+  auth_user_guid = '9b20d8a3-2870-40e3-b8a2-ef76d5f77708'
+WHERE
+  user_id = 'SROSINSK';
+
+UPDATE officer
+SET
+  auth_user_guid = '0730050a-d985-4260-b1e2-fad8f6484ba1'
+WHERE
+  user_id = 'KNORBERG';
+
+UPDATE officer
+SET
+  auth_user_guid = 'b4bb40f5-ee23-4c99-b63c-e741ce61b589'
+WHERE
+  user_id = 'JOCHARTR';
+
+UPDATE officer
+SET
+  auth_user_guid = 'fa4fc3b6-346c-401b-8a29-cd76a097e7b2'
+WHERE
+  user_id = 'SNORRIS';
+
+UPDATE officer
+SET
+  auth_user_guid = 'eb1ed21e-2da3-40fa-aa82-ae8e52a337ff'
+WHERE
+  user_id = 'TARWILLI';
+
+UPDATE officer
+SET
+  auth_user_guid = '07a5cdfa-ff86-4755-ad77-4a5d20c66bea'
+WHERE
+  user_id = 'CELDER';
+
+UPDATE officer
+SET
+  auth_user_guid = '0b524c8e-d202-44ae-a5a0-559930f3187e'
+WHERE
+  user_id = 'NSMIENK';
+
+UPDATE officer
+SET
+  auth_user_guid = 'eee4b016-7f48-43cb-bd22-325e91bc52c0'
+WHERE
+  user_id = 'MAWILSON';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c3a081db-a974-449c-9947-8ac8522ba3af'
+WHERE
+  user_id = 'STRUONG';
+
+UPDATE officer
+SET
+  auth_user_guid = '87a1058f-97c0-4bd7-a07c-c31fe40a2230'
+WHERE
+  user_id = 'DVUIA';
+
+UPDATE officer
+SET
+  auth_user_guid = '7ecde23e-9142-4d9b-9e6b-f00b4a549a59'
+WHERE
+  user_id = 'MGEUZE';
+
+UPDATE officer
+SET
+  auth_user_guid = 'f97fcfac-8ff3-407a-be02-a1fac8dcd915'
+WHERE
+  user_id = 'HDUMAINE';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd072fbaa-8668-412c-8dc1-a19e19cb9622'
+WHERE
+  user_id = 'DKORIN';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c75021f9-0956-48cf-bde9-20927e56766c'
+WHERE
+  user_id = 'TBECK';
+
+UPDATE officer
+SET
+  auth_user_guid = '9b20d8a3-2870-40e3-b8a2-ef76d5f77708'
+WHERE
+  user_id = 'SROSINSK';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e614ed75-e41b-4a5b-a1d8-c118db970ab8'
+WHERE
+  user_id = 'LGUGLIEL';
+
+UPDATE officer
+SET
+  auth_user_guid = '0730050a-d985-4260-b1e2-fad8f6484ba1'
+WHERE
+  user_id = 'KNORBERG';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd6f05035-df0f-4e6c-aff5-91dc75fe2eaa'
+WHERE
+  user_id = 'BALACEY';
+
+UPDATE officer
+SET
+  auth_user_guid = 'fa4fc3b6-346c-401b-8a29-cd76a097e7b2'
+WHERE
+  user_id = 'SNORRIS';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c559a49f-1992-4641-b8a5-3302833d3a7e'
+WHERE
+  user_id = 'AEAGLES';
+
+UPDATE officer
+SET
+  auth_user_guid = 'fef9665c-35ee-4caa-88ff-05864f400115'
+WHERE
+  user_id = 'BCARUTH';
+
+UPDATE officer
+SET
+  auth_user_guid = 'eb1ed21e-2da3-40fa-aa82-ae8e52a337ff'
+WHERE
+  user_id = 'TARWILLI';
+
+UPDATE officer
+SET
+  auth_user_guid = '07a5cdfa-ff86-4755-ad77-4a5d20c66bea'
+WHERE
+  user_id = 'CELDER';
+
+UPDATE officer
+SET
+  auth_user_guid = '08a17082-bd02-4ef3-9a38-d6d61f5c365f'
+WHERE
+  user_id = 'DJSTRUTH';
+
+UPDATE officer
+SET
+  auth_user_guid = '0b524c8e-d202-44ae-a5a0-559930f3187e'
+WHERE
+  user_id = 'NSMIENK';
+
+UPDATE officer
+SET
+  auth_user_guid = '82e8d3b4-bad0-45e8-ad39-80d426ea781c'
+WHERE
+  user_id = 'JXDUNSDO';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e3ae2a67-83f4-42de-b248-46f217321784'
+WHERE
+  user_id = 'GRLAVERY';
+
+UPDATE officer
+SET
+  auth_user_guid = 'eee4b016-7f48-43cb-bd22-325e91bc52c0'
+WHERE
+  user_id = 'MAWILSON';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd657346c-8be6-42cd-9864-120034167892'
+WHERE
+  user_id = 'MITO';
+
+UPDATE officer
+SET
+  auth_user_guid = '7ecde23e-9142-4d9b-9e6b-f00b4a549a59'
+WHERE
+  user_id = 'MGEUZE';
+
+UPDATE officer
+SET
+  auth_user_guid = '1432407d-f11b-419c-9c26-96259c0600e2'
+WHERE
+  user_id = 'JCLAUSEN';
+
+UPDATE officer
+SET
+  auth_user_guid = 'f97fcfac-8ff3-407a-be02-a1fac8dcd915'
+WHERE
+  user_id = 'HDUMAINE';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c75021f9-0956-48cf-bde9-20927e56766c'
+WHERE
+  user_id = 'TBECK';
+
+UPDATE officer
+SET
+  auth_user_guid = 'a2c7ef60-5ed1-42c2-8a74-6ac1606b4aec'
+WHERE
+  user_id = 'MCHAYER';
+
+UPDATE officer
+SET
+  auth_user_guid = '83fe7ae8-99ad-4153-a459-670c89a954be'
+WHERE
+  user_id = 'ELPEDERS';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e614ed75-e41b-4a5b-a1d8-c118db970ab8'
+WHERE
+  user_id = 'LGUGLIEL';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd6f05035-df0f-4e6c-aff5-91dc75fe2eaa'
+WHERE
+  user_id = 'BALACEY';
+
+UPDATE officer
+SET
+  auth_user_guid = 'ba9f3a7f-9434-4ccb-a861-307622a2d339'
+WHERE
+  user_id = 'BEPARKER';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c559a49f-1992-4641-b8a5-3302833d3a7e'
+WHERE
+  user_id = 'AEAGLES';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e7d909cb-57c7-4e3d-b4fc-17a732111070'
+WHERE
+  user_id = 'JCLANCY';
+
+UPDATE officer
+SET
+  auth_user_guid = 'fef9665c-35ee-4caa-88ff-05864f400115'
+WHERE
+  user_id = 'BCARUTH';
+
+UPDATE officer
+SET
+  auth_user_guid = 'a5354b38-ab6a-4b5d-8046-5f4bd1bc3c4b'
+WHERE
+  user_id = 'EHARBICH';
+
+UPDATE officer
+SET
+  auth_user_guid = 'da2fe7e1-fb07-4679-9e2f-1b66df8e7e1b'
+WHERE
+  user_id = 'JDAMERT';
+
+UPDATE officer
+SET
+  auth_user_guid = '08a17082-bd02-4ef3-9a38-d6d61f5c365f'
+WHERE
+  user_id = 'DJSTRUTH';
+
+UPDATE officer
+SET
+  auth_user_guid = '82e8d3b4-bad0-45e8-ad39-80d426ea781c'
+WHERE
+  user_id = 'JXDUNSDO';
+
+UPDATE officer
+SET
+  auth_user_guid = '091e810a-c6ac-4754-b762-f10bbc4651e2'
+WHERE
+  user_id = 'RGROEGER';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c7e86014-2cdd-4e87-9a85-efa280842a43'
+WHERE
+  user_id = 'KMAYOWSK';
+
+UPDATE officer
+SET
+  auth_user_guid = 'd657346c-8be6-42cd-9864-120034167892'
+WHERE
+  user_id = 'MITO';
+
+UPDATE officer
+SET
+  auth_user_guid = '303c6924-2540-4490-8581-70fddf8b3d70'
+WHERE
+  user_id = 'TYILDIRO';
+
+UPDATE officer
+SET
+  auth_user_guid = '1432407d-f11b-419c-9c26-96259c0600e2'
+WHERE
+  user_id = 'JCLAUSEN';
+
+UPDATE officer
+SET
+  auth_user_guid = 'a2c7ef60-5ed1-42c2-8a74-6ac1606b4aec'
+WHERE
+  user_id = 'MCHAYER';
+
+UPDATE officer
+SET
+  auth_user_guid = '83fe7ae8-99ad-4153-a459-670c89a954be'
+WHERE
+  user_id = 'ELPEDERS';
+
+UPDATE officer
+SET
+  auth_user_guid = 'ba9f3a7f-9434-4ccb-a861-307622a2d339'
+WHERE
+  user_id = 'BEPARKER';
+
+UPDATE officer
+SET
+  auth_user_guid = 'e7d909cb-57c7-4e3d-b4fc-17a732111070'
+WHERE
+  user_id = 'JCLANCY';
+
+UPDATE officer
+SET
+  auth_user_guid = 'a5354b38-ab6a-4b5d-8046-5f4bd1bc3c4b'
+WHERE
+  user_id = 'EHARBICH';
+
+UPDATE officer
+SET
+  auth_user_guid = 'da2fe7e1-fb07-4679-9e2f-1b66df8e7e1b'
+WHERE
+  user_id = 'JDAMERT';
+
+UPDATE officer
+SET
+  auth_user_guid = '091e810a-c6ac-4754-b762-f10bbc4651e2'
+WHERE
+  user_id = 'RGROEGER';
+
+UPDATE officer
+SET
+  auth_user_guid = 'c7e86014-2cdd-4e87-9a85-efa280842a43'
+WHERE
+  user_id = 'KMAYOWSK';
+
+UPDATE officer
+SET
+  auth_user_guid = '303c6924-2540-4490-8581-70fddf8b3d70'
+WHERE
+  user_id = 'TYILDIRO';
