9b88dc46869e1e64c64b3a764edbd5923952cffc
Mike <100624415+marqueone-ps@users.noreply.github.com>
2024-09-25 16:06:00 -0700

    feat: CE-1064 Hide ‘Office’ field on the CEEB forms (#659)
    Co-authored-by: Mike Sears <mike.2.sears@gov.bc.ca>
    Co-authored-by: afwilcox <alecwilcox@gmail.com>

--- migrations/migrations/R__Create-Test-Data.sql
+++ migrations/migrations/R__Create-Test-Data.sql
@@ -9466,6 +9466,96 @@ VALUES
   ) ON CONFLICT
 DO NOTHING;
 
+--
+-- create new feature for Enable Office
+--
+INSERT INTO
+  feature_code (
+    feature_code,
+    short_description,
+    long_description,
+    display_order,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+SELECT
+  'ENBL_OFF',
+  'Enable Office',
+  'Enables the ability to render the office input and field on a complaint',
+  150,
+  'Y',
+  user,
+  now(),
+  user,
+  now() ON CONFLICT
+DO NOTHING;
+
+--
+-- create new xrefs for new enable office feature
+--
+INSERT INTO
+  feature_agency_xref (
+    feature_code,
+    agency_code,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+SELECT
+  'ENBL_OFF',
+  'EPO',
+  'N',
+  user,
+  now(),
+  user,
+  now() ON CONFLICT
+DO NOTHING;
+
+INSERT INTO
+  feature_agency_xref (
+    feature_code,
+    agency_code,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+SELECT
+  'ENBL_OFF',
+  'COS',
+  'Y',
+  user,
+  now(),
+  user,
+  now() ON CONFLICT
+DO NOTHING;
+
+INSERT INTO
+  feature_agency_xref (
+    feature_code,
+    agency_code,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+SELECT
+  'ENBL_OFF',
+  'PARKS',
+  'Y',
+  user,
+  now(),
+  user,
+  now() ON CONFLICT
+DO NOTHING;
+
 --------------------------
 -- New Changes above this line
 -------------------------
