7eebeea2720b725536b6c44170e64afd18e1a10e
Mike Vesprini <mike@button.is>
2024-10-17 11:48:11 -0700

    test: make e2e search test rerunnable

--- frontend/cypress/e2e/complaint-search.v2.cy.ts
+++ frontend/cypress/e2e/complaint-search.v2.cy.ts
@@ -158,24 +158,42 @@ describe("Verify CEEB specific search filters work", () => {
     cy.kcLogout().kcLogin(Roles.CEEB);
   });
 
-  it("allows filtering of complaints by Action Taken", function () {
+  function needsDecision() {
+    let needsDecision = false;
+    cy.get("#ceeb-decision").then((decisionWrapper) => {
+      // If the action taken input is on the page, a decision needs to be made
+      if (decisionWrapper.find("#outcome-decision-action-taken").length > 0) {
+        needsDecision = true;
+      }
+    });
+    return needsDecision;
+  }
+
+  it.only("allows filtering of complaints by Action Taken", function () {
     // Navigate to the complaint list
     const complaintWithActionTakenID = "23-030990";
     const actionTaken = "Forward to lead agency";
 
-    // Set an 'action taken' on a complaint, so it can be filtered
+    // Check if complaintWithActionTakenID already has a decision.
     cy.navigateToDetailsScreen(COMPLAINT_TYPES.ERS, complaintWithActionTakenID, true);
-    cy.selectItemById("outcome-decision-schedule-sector", "Other");
-    cy.selectItemById("outcome-decision-sector-category", "None");
-    cy.selectItemById("outcome-decision-discharge", "Pesticides");
-    cy.selectItemById("outcome-decision-action-taken", actionTaken);
-    cy.selectItemById("outcome-decision-lead-agency", "Other");
-    cy.enterDateTimeInDatePicker("outcome-decision-outcome-date", "01");
-    cy.get("#details-screen-assign-button").should("exist").click();
-    cy.get("#self_assign_button").should("exist").click();
-    cy.get(".modal").should("not.exist");
-    cy.get("#ceeb-decision > .card-body > .comp-details-form-buttons > #outcome-decision-save-button").click();
-    cy.contains("div", "Decision added").should("exist");
+    // If the action taken input is available then the complaint does not yet have a decision made on it.
+    // Set an action taken so that the filter will have results to return.
+    if (needsDecision()) {
+      cy.selectItemById("outcome-decision-schedule-sector", "Other");
+      cy.selectItemById("outcome-decision-sector-category", "None");
+      cy.selectItemById("outcome-decision-discharge", "Pesticides");
+      cy.selectItemById("outcome-decision-action-taken", actionTaken);
+      cy.selectItemById("outcome-decision-lead-agency", "Other");
+      cy.enterDateTimeInDatePicker("outcome-decision-outcome-date", "01");
+      // If the complaint is not assigned to anyone, assign it to self
+      if (cy.get("#comp-details-assigned-officer-name-text-id").contains("Not Assigned")) {
+        cy.get("#details-screen-assign-button").should("exist").click();
+        cy.get("#self_assign_button").should("exist").click();
+      }
+      cy.get(".modal").should("not.exist"); // Ensure that the quick assign modal has closed
+      cy.get("#ceeb-decision > .card-body > .comp-details-form-buttons > #outcome-decision-save-button").click();
+      cy.contains("div", "Decision added").should("exist");
+    }
 
     // Return to the complaints view
     cy.get("#complaints-link").click();
