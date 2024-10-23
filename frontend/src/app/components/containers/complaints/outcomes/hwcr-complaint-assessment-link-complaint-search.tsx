dae9f804a60f99d471266adb2198a868a7ff99bd
Ryan Rondeau <ryan_rondeau@live.ca>
2024-10-23 11:03:39 -0700

    Improve rendering of dropdown link component

--- frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment-link-complaint-search.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment-link-complaint-search.tsx
@@ -1,8 +1,8 @@
-import { AsyncTypeahead, Highlighter } from "react-bootstrap-typeahead"; // ES2015
+import { AsyncTypeahead, Highlighter, useHint } from "react-bootstrap-typeahead";
 import "react-bootstrap-typeahead/css/Typeahead.bs5.css";
 
-import { FC, useState } from "react";
-import { Badge } from "react-bootstrap";
+import { FC, useState, ReactNode } from "react";
+import { Badge, Form } from "react-bootstrap";
 import Option from "@apptypes/app/option";
 import { useAppDispatch, useAppSelector } from "@hooks/hooks";
 import { selectCodeTable } from "@store/reducers/code-table";
@@ -10,11 +10,55 @@ import { CODE_TABLE_TYPES } from "@constants/code-table-types";
 import { generateApiParameters, get } from "@common/api";
 import { applyStatusClass } from "@common/methods";
 import config from "@/config";
+import { is } from "cypress/types/bluebird";
 
 type Props = {
   id?: string;
   onChange?: (selected: Option | null, status: string | null) => void;
   errorMessage?: string;
+};
+
+type HintProps = {
+  children: ReactNode;
+  className?: string;
+  hintText: string;
+};
+
+const CustomHint = ({ children, className, hintText }: HintProps) => {
+  const { hintRef } = useHint();
+
+  return (
+    <div
+      className={className}
+      style={{
+        display: "flex",
+        flex: 1,
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      {children}
+      <input
+        aria-hidden
+        className="rbt-input-hint"
+        ref={hintRef}
+        readOnly
+        style={{
+          backgroundColor: "transparent",
+          borderColor: "transparent",
+          boxShadow: "none",
+          color: "rgba(0, 0, 0, 0.54)",
+          left: 0,
+          pointerEvents: "none",
+          position: "absolute",
+          top: 0,
+          width: "100%",
+        }}
+        tabIndex={-1}
+        value={hintText}
+      />
+    </div>
+  );
 };
 
 export const HWCRComplaintAssessmentLinkComplaintSearch: FC<Props> = ({
@@ -27,6 +71,8 @@ export const HWCRComplaintAssessmentLinkComplaintSearch: FC<Props> = ({
   const speciesCodes = useAppSelector(selectCodeTable(CODE_TABLE_TYPES.SPECIES));
   const natureOfComplaints = useAppSelector(selectCodeTable(CODE_TABLE_TYPES.NATURE_OF_COMPLAINT));
 
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [hintText, setHintText] = useState<string>("");
   const [complaintData, setComplaintData] = useState<any[]>([]);
 
   const getStatusDescription = (input: string): string => {
@@ -44,12 +90,17 @@ export const HWCRComplaintAssessmentLinkComplaintSearch: FC<Props> = ({
     return code.longDescription;
   };
 
-  const handleChange = (selected: any[]) =>
+  const handleChange = (selected: any[]) => {
     onChange(
       selected.length > 0 ? ({ label: selected[0].id as string, value: selected[0].id as string } as Option) : null,
       selected[0]?.status,
     );
-
+    setHintText(
+      isFocused && selected.length > 0
+        ? `${selected[0].id}, ${getSpecies(selected[0].species)}, ${getNatureOfComplaint(selected[0].natureOfComplaint)}`
+        : "",
+    );
+  };
   const handleSearch = async (query: string) => {
     const parameters = generateApiParameters(
       `${config.API_BASE_URL}/v1/complaint/search/HWCR?sortBy=incident_reported_utc_timestmp&orderBy=DESC&zone=SPCE&page=1&pageSize=10&query=${query}`,
@@ -68,11 +119,25 @@ export const HWCRComplaintAssessmentLinkComplaintSearch: FC<Props> = ({
         minLength={2}
         onSearch={handleSearch}
         onChange={handleChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         filterBy={() => true}
         isLoading={false}
         options={complaintData}
         placeholder="Search for a complaint"
         isInvalid={errorMessage.length > 0}
+        renderInput={({ inputRef, referenceElementRef, ...inputProps }: any) => (
+          <CustomHint hintText={hintText}>
+            <Form.Control
+              {...inputProps}
+              ref={(node: any) => {
+                inputRef(node);
+                referenceElementRef(node);
+              }}
+              type="text"
+            />
+          </CustomHint>
+        )}
         renderMenuItemChildren={(option: any, props: any) => (
           <>
             <div>
