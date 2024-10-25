14760b5d29c8ae6f2216814142ca8f02fa65460c
Ryan Rondeau <ryan_rondeau@live.ca>
2024-10-25 13:06:16 -0700

    Update change-complaint-assignee.cy.ts

--- frontend/cypress/e2e/change-complaint-assignee.cy.ts
+++ frontend/cypress/e2e/change-complaint-assignee.cy.ts
@@ -27,9 +27,9 @@ describe("Complaint Assign Popover spec", { scrollBehavior: false }, () => {
 
       cy.get("td").first().click({ force: true });
 
-      cy.get("#quick-action-button").filter(":visible").click({ force: true });
+      cy.get("#quick-action-button").first().click({ force: true });
 
-      cy.get("#update-assignee-menu-item").filter(":visible").click({ force: true });
+      cy.get("#update-assignee-menu-item").first().click({ force: true });
 
       // self assign the complaint
       cy.get("#self_assign_button").click();
