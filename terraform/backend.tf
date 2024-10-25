0ad13002155fa62401e56ddb324bc9e412c01020
Jonathan Funk <johnthomasfunk@gmail.com>
2024-10-25 14:00:58 -0700

    add backend skips

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,6 +9,8 @@ terraform {
         # avoid aws-specific api checks
         skip_credentials_validation = true
         skip_metadata_api_check = true
+        skip_region_validation = true
+        skip_requesting_account_id = true
         force_path_style = true
     }
 }
