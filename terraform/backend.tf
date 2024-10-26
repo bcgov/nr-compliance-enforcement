91e0726732c6ffafb2396421f96f42cad5f4e5a4
Jonathan Funk <johnthomasfunk@gmail.com>
2024-10-25 19:08:21 -0700

    skip checksum

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,6 +11,7 @@ terraform {
         skip_metadata_api_check = true
         skip_region_validation = true
         skip_requesting_account_id = true
+        skip_s3_checksum = true
         force_path_style = true
     }
 }
