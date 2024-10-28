7deba15501d44a1a9c3d91e381ad2fd79f1ed9d0
Jonathan Funk <johnthomasfunk@gmail.com>
2024-10-27 22:26:15 -0700

    split out test and prod alerts

---
+++ terraform/alerts_prod.tf
@@ -0,0 +1,259 @@
+### Backend Alerts
+resource "sysdig_monitor_alert_v2_prometheus" "backend_prod_cpu_quota" {
+  name = "Backend CPU Requests Quota Alert"
+  description = "Alert when the CPU requests usage is too high"
+  severity = "medium"
+  query = "sysdig_container_cpu_quota_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-backend\",container_name=\"nr-compliance-enforcement-prod-backend\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Backend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "backend_prod_mem_usage" {
+  name = "Backend Mem Usage Alert"
+  description = "Alert when the mem usage is too high"
+  severity = "medium"
+  query = "sysdig_container_memory_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-backend\",container_name=\"nr-compliance-enforcement-prod-backend\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Backend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "backend_prod_mem_limit" {
+  name = "Backend Mem Limit Alert"
+  description = "Alert when the mem usage is near the limit for too long"
+  severity = "high"
+  query = "sysdig_container_memory_limit_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-backend\",container_name=\"nr-compliance-enforcement-prod-backend\"}  > 70"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Backend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "backend_prod_uptime_score" {
+  name = "Backend Uptime Alert"
+  description = "Alert when the backend container has too much downtime"
+  severity = "high"
+  query = "sysdig_container_up{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-backend\",container_name=\"nr-compliance-enforcement-prod-backend\"} < 0.7"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Backend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "backend_prod_http_silent" {
+  name = "Backend Unresponsive Alert"
+  description = "Alert when the backend container has been unresponsive or silent for too long"
+  severity = "high"
+  query = "sysdig_container_net_http_request_count{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-backend\",container_name=\"nr-compliance-enforcement-prod-backend\"} < 0.1"
+  enabled = true
+  duration_seconds = 300
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Backend"
+    app = "NatCom"
+  }
+}
+### Frontend Alerts
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_cpu_quota" {
+  name = "Frontend CPU Requests Quota Alert"
+  description = "Alert when the CPU requests usage is too high"
+  severity = "medium"
+  query = "sysdig_container_cpu_quota_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\",container_name=\"nr-compliance-enforcement-prod-frontend\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_mem_usage" {
+  name = "Frontend Mem Usage Alert"
+  description = "Alert when the mem usage is too high"
+  severity = "medium"
+  query = "sysdig_container_memory_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\",container_name=\"nr-compliance-enforcement-prod-frontend\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_mem_limit" {
+  name = "Frontend Mem Limit Alert"
+  description = "Alert when the mem usage is near the limit for too long"
+  severity = "high"
+  query = "sysdig_container_memory_limit_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\",container_name=\"nr-compliance-enforcement-prod-frontend\"}  > 70"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_uptime_score" {
+  name = "Frontend Uptime Alert"
+  description = "Alert when the frontend container has too much downtime"
+  severity = "high"
+  query = "sysdig_container_up{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\",container_name=\"nr-compliance-enforcement-prod-frontend\"} < 0.7"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_http_silent" {
+  name = "Frontend Unresponsive Alert"
+  description = "Alert when the frontend container has been unresponsive or silent for too long"
+  severity = "high"
+  query = "sysdig_container_net_http_request_count{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\",container_name=\"nr-compliance-enforcement-prod-frontend\"} < 0.1"
+  enabled = true
+  duration_seconds = 300
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "frontend_prod_http_error_rate" {
+  name = "Frontend HTTP Error Rate Alert"
+  description = "Alert when the frontend container has too many HTTP errors over a period"
+  severity = "high"
+  query = "(sysdig_container_net_http_error_count{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\"} / sysdig_container_net_http_request_count{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_deployment_name=\"nr-compliance-enforcement-prod-frontend\"} ) > 0.05"
+  enabled = true
+  duration_seconds = 300
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Frontend"
+    app = "NatCom"
+  }
+}
+### Database Alerts
+resource "sysdig_monitor_alert_v2_prometheus" "database_prod_cpu_quota" {
+  name = "Database CPU Requests Quota Alert"
+  description = "Alert when the CPU requests usage is too high"
+  severity = "medium"
+  query = "sysdig_container_cpu_quota_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_statefulset_name=\"nr-compliance-enforcement-prod-bitnami-pg\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Database"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "database_prod_mem_usage" {
+  name = "Database Mem Usage Alert"
+  description = "Alert when the mem usage is too high"
+  severity = "medium"
+  query = "sysdig_container_memory_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_statefulset_name=\"nr-compliance-enforcement-prod-bitnami-pg\"}  > 90"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Database"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "database_prod_mem_limit" {
+  name = "Database Mem Limit Alert"
+  description = "Alert when the mem usage is near the limit for too long"
+  severity = "high"
+  query = "sysdig_container_memory_limit_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_statefulset_name=\"nr-compliance-enforcement-prod-bitnami-pg\"}  > 70"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Database"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "database_prod_uptime_score" {
+  name = "Database Uptime Alert"
+  description = "Alert when the database container has too much downtime"
+  severity = "high"
+  query = "sysdig_container_up{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_statefulset_name=\"nr-compliance-enforcement-prod-bitnami-pg\"} < 0.7"
+  enabled = true
+  duration_seconds = 30
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Database"
+    app = "NatCom"
+  }
+}
+resource "sysdig_monitor_alert_v2_prometheus" "database_prod_storage_usage" {
+  name = "Database Storage Alert"
+  description = "Alert when the database storage usage is too high"
+  severity = "high"
+  query = "sysdig_fs_used_percent{kube_cluster_name=\"silver\",kube_namespace_name=\"c1c7ed-prod\",kube_statefulset_name=\"nr-compliance-enforcement-prod-bitnami-pg\"} > 70"
+  enabled = true
+  duration_seconds = 600
+  notification_channels {
+    id = sysdig_monitor_notification_channel_email.prod_environment_alerts.id
+    renotify_every_minutes = 120
+  }
+  labels = {
+    service = "NatCom Database"
+    app = "NatCom"
+  }
+}
