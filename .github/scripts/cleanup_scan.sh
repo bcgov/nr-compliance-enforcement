34a9ffffc43bb4339711530973bbf4ca840e3fdc
Jonathan Funk <johnthomasfunk@gmail.com>
2024-10-31 09:44:36 -0700

    Script hardening

--- .github/scripts/cleanup_scan.sh
+++ .github/scripts/cleanup_scan.sh
@@ -7,18 +7,23 @@
 # in order to see the names and perform the delete themselves.
 #
 set -e # failfast
+trap 'echo "Error occurred at line $LINENO while executing function $FUNCNAME"' ERR
 
 # ENV:
 # OC_NAMESPACE: namespace to scan
 # SKIP_AUTH: set to true to skip auth and use your existing local kubeconfig
 # OC_SERVER: OpenShift server URL
 # OC_TOKEN: OpenShift token
-# ALLOW_LIST: comma separated list of workloads to ignore
-if [ -z "$ALLOW_LIST" ]; then
-    ALLOW_LIST=""
+# ALLOW_EXPR: comma separated list of workloads to ignore
+
+# THIRTY_DAYS_IN_SECONDS=2592000 - variables in the jq dont play nice so we will just hardcode it
+
+# Configure globals
+if [ -z "$ALLOW_EXPR" ]; then
+    ALLOW_EXPR="placeholder"
 fi
-ALLOW_LIST=$(echo "$ALLOW_LIST" | tr "," "\n")
-echo "ALLOW_LIST: $ALLOW_LIST"
+echo "ALLOW_EXPR: $ALLOW_EXPR"
+
 OC_TEMP_TOKEN=""
 if [ -z "$OC_NAMESPACE" ]; then
     echo "OC_NAMESPACE is not set. Exiting..."
@@ -38,35 +43,62 @@ if [ "$SKIP_AUTH" != "true" ]; then
     oc login --token=$OC_TEMP_TOKEN --server=$OC_SERVER
     oc project $OC_NAMESPACE # Safeguard!
 fi
+OK=0
 
+# checks if the resource name appears in the allow expression
+# and removes it if found, preventing a false positive
+filter_items() {
+    local items="$1"
+    local filtered_items=()
+    # convert items to an array for easy manipulation
+    local iter_array_items=()
+    mapfile -t array_items < <(echo "$items")
+    for item in "${array_items[@]}"; do
+        if ! echo "$item" | grep -Eq "$ALLOW_EXPR"; then
+            iter_array_items+=("$item")
+        fi
+    done
+    # organize the filtered items
+    for item in "${iter_array_items[@]}"; do
+        if [[ -n "$item" ]]; then
+            filtered_items+=("$item")
+        fi
+    done
+    duplicates_result=$(printf "%s\n" "${filtered_items[@]}")
+    unique_result=$(echo "$duplicates_result" | sort | uniq)
+    echo "$unique_result"
+}
 
-ok=0
-touch /tmp/old_workloads.txt
-touch /tmp/secrets_to_delete.txt
-touch /tmp/pvcs_to_delete.txt
-touch /tmp/configmaps_to_delete.txt
+# standard function to output found deletions to a file, and set the OK flag
+# note that the message can contain NUM_ITEMS which will be replaced with the count found
+found_deletions() {
+    local file_name=$1
+    local err_message=$2
+    local ok_message=$3
+    local items=$4
+    local num_items
+    num_items=$(echo "$items" | grep -cve '^\s*$' || echo 0)
+    num_items=${num_items//[^0-9]/} # ensure num_items is an integer
+    if [ "$num_items" -gt 0 ]; then
+        echo -e "$items" > "/tmp/$file_name"
+        echo "${err_message//NUM_ITEMS/$num_items}"
+        OK=1
+    else
+        echo "$ok_message"
+    fi
+}
 
 # First, get workloads older than 30 days
 echo "Scanning for workloads older than 30 days in the targeted namespace"
 echo "..."
-# THIRTY_DAYS_IN_SECONDS=2592000
 old_workloads=$(oc get deploy,service,statefulset -n $OC_NAMESPACE -ojson | jq -r '.items[] | select(.metadata.creationTimestamp | fromdateiso8601 | (. + 2592000) < now) | "\(.kind)/\(.metadata.name)"')
-filtered_workloads=()
-for workload in $old_workloads; do
-    if ! echo "$ALLOW_LIST" | grep -q "^$workload$"; then
-        filtered_workloads+=("$workload")
-    fi
-done
-old_workloads="${filtered_workloads[*]}"
-num_old_workloads=$(echo $old_workloads | wc -w)
-if [ $num_old_workloads -gt 0 ]; then
-    echo -e "$old_workloads" > /tmp/old_workloads.txt
-    echo -e "\n" >> /tmp/old_workloads.txt
-    echo "Found $num_old_workloads workloads older than 30 days in the targeted namespace"
-    ok=1
-else
-    echo "Found no stale workloads in the targeted namespace"
-fi
+old_workloads=$(filter_items "$old_workloads")
+old_workloads_to_delete=$old_workloads
+found_deletions \
+    "old_workloads_to_delete.txt" \
+    "Found NUM_ITEMS workloads older than 30 days in the targeted namespace" \
+    "Found no stale workloads in the targeted namespace" \
+    "$old_workloads_to_delete"
 echo ""
 echo ""
 
@@ -82,21 +114,13 @@ for secret in $secret_names; do
         secrets_to_delete+=("secret/$secret\n")
     fi
 done
-filtered_secrets=()
-for secret in "${secrets_to_delete[@]}"; do
-    if ! echo "$ALLOW_LIST" | grep -q "$secret\n"; then
-        filtered_secrets+=("$secret")
-    fi
-done
-secrets_to_delete=("${filtered_secrets[@]}")
-num_secrets_to_delete=${#secrets_to_delete[@]}
-if [ $num_secrets_to_delete -gt 0 ]; then
-    echo "Found $num_secrets_to_delete secrets older than 30 days not used by any pod in the targeted namespace"
-    echo -e "${secrets_to_delete[@]}" > /tmp/secrets_to_delete.txt
-    ok=1
-else
-    echo "Found no stale and dangling secrets in the targeted namespace"
-fi
+secrets_list=$(echo -e "${secrets_to_delete[@]}")
+filtered_secrets=$(filter_items "$secrets_list")
+found_deletions \
+    "secrets_to_delete.txt" \
+    "Found NUM_ITEMS dangling secrets older than 30 days in the targeted namespace" \
+    "Found no stale and dangling secrets in the targeted namespace" \
+    "${filtered_secrets}"
 echo ""
 echo ""
 
@@ -107,27 +131,20 @@ echo "..."
 oc get pods -n $OC_NAMESPACE -ojson | jq -r '.items[] | "\(.metadata.name):\(.spec.volumes[]?.persistentVolumeClaim.claimName)"' > /tmp/in_use_pvc.txt
 in_use_pvcs=$(cat /tmp/in_use_pvc.txt | grep -v "null" | sort | uniq)
 echo -e "$in_use_pvcs" > /tmp/in_use_pvc.txt
+pvc_list=$(oc get pvc -n $OC_NAMESPACE -ojson | jq -r '.items[] | select(.metadata.creationTimestamp | fromdateiso8601 | (. + 2592000) < now) | .metadata.name')
 pvcs_to_delete=()
-for pvc in $(oc get pvc -n $OC_NAMESPACE -ojson | jq -r '.items[] | select(.metadata.creationTimestamp | fromdateiso8601 | (. + 2592000) < now) | .metadata.name'); do
+for pvc in $pvc_list; do
     if ! grep -q $pvc /tmp/in_use_pvc.txt; then
         pvcs_to_delete+=("pvc/$pvc\n")
     fi
 done
-filtered_pvcs=()
-for pvc in "${pvcs_to_delete[@]}"; do
-    if ! echo "$ALLOW_LIST" | grep -q "^$pvc\n$"; then
-        filtered_pvcs+=("$pvc")
-    fi
-done
-pvcs_to_delete=("${filtered_pvcs[@]}")
-num_pvcs_to_delete=${#pvcs_to_delete[@]}
-if [ $num_pvcs_to_delete -gt 0 ]; then
-    echo "Found $num_pvcs_to_delete pvcs older than 30 days not used by any pod in the targeted namespace"
-    echo -e "${pvcs_to_delete[@]}" > /tmp/pvcs_to_delete.txt
-    ok=1
-else
-    echo "Found no stale and dangling pvcs in the targeted namespace"
-fi
+pvc_list=$(echo -e "${pvcs_to_delete[@]}")
+filtered_pvcs=$(filter_items "$pvc_list")
+found_deletions \
+    "pvcs_to_delete.txt" \
+    "Found NUM_ITEMS dangling PVCs older than 30 days in the targeted namespace" \
+    "Found no stale and dangling PVCs in the targeted namespace" \
+    "${filtered_pvcs}"
 echo ""
 echo ""
 
@@ -142,31 +159,23 @@ for configmap in $configmap_names; do
         configmaps_to_delete+=("configmap/$configmap\n")
     fi
 done
-filtered_configmaps=()
-for configmap in "${configmaps_to_delete[@]}"; do
-    if ! echo "$ALLOW_LIST" | grep -q "^$configmap\n$"; then
-        filtered_configmaps+=("$configmap")
-    fi
-done
-configmaps_to_delete=("${filtered_configmaps[@]}")
-num_configmaps_to_delete=${#configmaps_to_delete[@]}
-if [ $num_configmaps_to_delete -gt 0 ]; then
-    echo "Found $num_configmaps_to_delete configmaps older than 30 days not used by any pod in the targeted namespace"
-    echo -e "${configmaps_to_delete[@]}" > /tmp/configmaps_to_delete.txt
-    ok=1
-else
-    echo "Found no stale and dangling configmaps in the targeted namespace"
-fi
+configmap_list=$(echo -e "${configmaps_to_delete[@]}")
+filtered_configmaps=$(filter_items "$configmap_list")
+found_deletions \
+    "configmaps_to_delete.txt" \
+    "Found NUM_ITEMS dangling configmaps older than 30 days in the targeted namespace" \
+    "Found no stale and dangling configmaps in the targeted namespace" \
+    "${filtered_configmaps}"
 echo ""
 echo ""
 
-if [ $ok -eq 1 ]; then
+if [ $OK -eq 1 ]; then
     echo "To delete these found workloads, locally run the following to see them:"
     echo "Note: skip flag uses your existing oc authentication"
     echo ""
     echo "SKIP_AUTH=true ./.github/scripts/cleanup_scan.sh"
-    echo "cat /tmp/old_workloads.txt && cat /tmp/secrets_to_delete.txt && cat /tmp/pvcs_to_delete.txt && cat /tmp/configmaps_to_delete.txt"
+    echo "cat /tmp/old_workloads_to_delete.txt && cat /tmp/secrets_to_delete.txt && cat /tmp/pvcs_to_delete.txt && cat /tmp/configmaps_to_delete.txt"
 
 fi
 
-exit $ok
+exit $OK
