f3c95bf0fcde54a17e9b1d3b8168860b0e8f450e
Ryan Rondeau <ryan_rondeau@live.ca>
2024-12-11 13:36:55 -0800

    Update officer.service.ts

--- backend/src/v1/officer/officer.service.ts
+++ backend/src/v1/officer/officer.service.ts
@@ -34,7 +34,7 @@ export class OfficerService {
       .leftJoinAndSelect("officer.office_guid", "office")
       .leftJoinAndSelect("officer.person_guid", "person")
       .leftJoinAndSelect("office.agency_code", "agency")
-      // This view is slow, no need to join and select for the properties that are mapped in this call
+      // This view is slow :(
       .leftJoinAndSelect("office.cos_geo_org_unit", "cos_geo_org_unit")
       .leftJoinAndSelect("office.agency_code", "agency_code")
       .orderBy("person.last_name", "ASC")
