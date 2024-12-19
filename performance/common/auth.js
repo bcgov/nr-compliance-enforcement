a5e1d7db82d8e606b25bc43bdb00e6468e4dbc1e
Mike Vesprini <mike@button.is>
2024-12-19 13:30:43 -0800

    Update test auth format and generate config

--- performance/common/auth.js
+++ performance/common/auth.js
@@ -1,15 +1,19 @@
-const COS_USER_TOKEN = "";
+export const INITIAL_COS_TOKEN = "";
+export const INITIAL_COS_REFRESH_TOKEN = "";
 
 export const COS_USER_CREDS = {
   username: "",
   password: "",
+  officerGuid: "",
 };
 
-export const COS_USER_HEADERS = {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${COS_USER_TOKEN}`,
-  },
+export const generateRequestConfig = (token) => {
+  return {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
 };
 
 const CEEB_USER_TOKEN = "xxxxxxx";
