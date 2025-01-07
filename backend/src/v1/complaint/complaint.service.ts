6aaef77d81aa1bbc6116e61c5a034fdbc2d8c7b9
Dmitri Korin <dmitri.korin@gov.bc.ca>
2025-01-07 14:49:25 -0800

    Optimize query

--- backend/src/v1/complaint/complaint.service.ts
+++ backend/src/v1/complaint/complaint.service.ts
@@ -376,7 +376,7 @@ export class ComplaintService {
     }
 
     if (officerAssigned && officerAssigned === "Unassigned") {
-      builder.andWhere("delegate.person_complaint_xref_guid IS NULL");
+      builder.andWhere("delegate.complaint_identifier IS NULL");
     } else if (officerAssigned) {
       builder
         .andWhere("delegate.person_complaint_xref_code = :Assignee", {
@@ -1063,7 +1063,7 @@ export class ComplaintService {
             sortBy === "incident_reported_utc_timestmp" ? orderBy : "DESC",
           );
       }
-
+      let sql = builder.getQuery();
       //-- search and count
       // Workaround for the issue with getManyAndCount() returning the wrong count and results in complex queries
       // introduced by adding an IN clause in a OrWhere statement: https://github.com/typeorm/typeorm/issues/320
