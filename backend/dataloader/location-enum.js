8ce4f196d535a06a869be1bafbea4214d3ed0937
afwilcox <alecwilcox@gmail.com>
2025-01-07 12:29:40 -0800

    chore: NatCom bulk data loader (#839)
    Co-authored-by: jon-funk <johnthomasfunk@gmail.com>
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>
    Co-authored-by: Ryan Rondeau <nayr974@users.noreply.github.com>
    Co-authored-by: Scarlett <35635257+Scarlett-Truong@users.noreply.github.com>
    Co-authored-by: dmitri-korin-bcps <108112696+dmitri-korin-bcps@users.noreply.github.com>

---
+++ backend/dataloader/location-enum.js
@@ -0,0 +1,159 @@
+const regions = {
+  "Skeena": {
+    "zones": {
+      "Bulkley-Cassiar": {
+        "districts": {
+          "Atlin": ["Atlin"],
+          "Dease Lake": ["Bell II North"],
+          "Smithers": ["Bell II South"]
+        }
+      },
+      "North Coast": {
+        "districts": {
+          "Terrace": ["Terrace", "Klemtu", "Usk"]
+        }
+
+      }
+    }
+  },
+  "West Coast": {
+    "zones": {
+      "Central Island": {
+        "districts": {
+          "Port Alberni": ["Ahousat", "Bamfield", "Beaver Creek"]
+        }
+      },
+      "North Island": {
+        "districts": {
+          "Port McNeill": ["Alert Bay", "Beaver Cove", "Bella Bella"]
+        }
+      },
+      "South Island": {
+        "districts": {
+          "Duncan": ["Caycuse", "Chemainus"],
+          "Victoria": ["Central Saanich"]
+        }
+      }
+    }
+  },
+  "Okanagan": {
+    "zones": {
+      "Central Okanagan": {
+        "districts": {
+          "Kelowna": ["Beaverdell", "Big White", "Black Mountain"]
+        }
+      },
+      "North Okanagan": {
+        "districts": {
+          "Salmon Arm": ["Albert Canyon", "Arrowhead"],
+          "Vernon": ["Armstrong"]
+        }
+      },
+      "South Okanagan": {
+        "districts": {
+          "Penticton": ["Bridesville", "Cawston"],
+          "Grand Forks": ["Christian Valley"]
+        }
+      }
+    }
+  },
+  "Kootenay": {
+    "zones": {
+      "Columbia/Kootenay": {
+        "districts": {
+          "Invermere": ["Athalmer"],
+          "Golden": ["Blaeberry", "Brisco"]
+        }
+      },
+      "East Kootenay": {
+        "districts": {
+          "Fernie": ["Baynes Lake"],
+          "Cranbrook": ["Bull River", "Coyote Creek"]
+        }
+      },
+      "West Kootenay": {
+        "districts": {
+          "Nelson": ["Ainsworth"],
+          "Castlegar": ["Appledale", "Applegrove"]
+        }
+      }
+    }
+  },
+  "South Coast": {
+    "zones": {
+      "Fraser North": {
+        "districts": {
+          "Mission/Maple Ridge": ["Albion", "Alvin", "Anmore"]
+        }
+      },
+      "Fraser South": {
+        "districts": {
+          "Mission/Chilliwack": ["Abbotsford", "Agassiz"],
+          "Mission/Surrey": ["Aldergrove"]
+        }
+      },
+      "Sea to Sky": {
+        "districts": {
+          "Squamish/Whistler": ["Birken", "Birkenhead Estates", "Devine"]
+        }
+      },
+      "Sunshine Coast": {
+        "districts": {
+          "Powell River": ["Black Point", "Bliss Landing", "Blubber Bay"]
+        }
+      }
+    }
+  },
+  "Omineca": {
+    "zones": {
+      "Nechako-Lakes": {
+        "districts": {
+          "Burns Lake": ["Burns Lake", "Clemretta"],
+          "Vanderhoof": ["Clucluz Lake - Brookside Resort West"]
+        }
+      },
+      "Omineca": {
+        "districts": {
+          "Prince George": ["Aleza Lake", "Bear Lake", "Beaverly"]
+        }
+      }
+    }
+  },
+  "Peace Liard": {
+    "zones": {
+      "North Peace": {
+        "districts": {
+          "Fort St. John": ["Altona", "Attachie", "Baldonell"]
+        }
+      },
+      "South Peace": {
+        "districts": {
+          "Dawson Creek": ["Arras", "Bear Mountain"],
+          "Chetwynd": ["Beryl Prairie"]
+        }
+      }
+    }
+  },
+  "Thompson Cariboo": {
+    "zones": {
+      "Thompson Nicola": {
+        "districts": {
+          "Kamloops": ["Adams Lake", "Anglemont"],
+          "Merritt": ["Ashcroft"]
+        }
+      },
+      "Cariboo Chilcotin": {
+        "districts": {
+          "Williams Lake": ["140 Mile House", "Horsefly", "Likely"]
+        }
+      },
+      "Cariboo Thompson": {
+        "districts": {
+          "Lillooet": ["Big Bar", "Pavillion", "Clinton"]
+        }
+      }
+    }
+  },
+};
+
+module.exports = regions;
