bae89d05c75334a8d65c4ab0f1a05ee5f54f7a7b
Ryan Rondeau <ryan_rondeau@live.ca>
2025-01-09 11:14:37 -0800

    Update V0.33.0__CE-1336.sql

--- migrations/migrations/V0.33.0__CE-1336.sql
+++ migrations/migrations/V0.33.0__CE-1336.sql
@@ -1,2 +1,5 @@
 UPDATE hwcr_complaint_nature_code SET long_description = 'Livestock/pets - killed/injured - not present' WHERE hwcr_complaint_nature_code = 'LIVNCOU';
 UPDATE hwcr_complaint_nature_code SET long_description = 'Livestock/pets - killed/injured - present/recent/suspected' WHERE hwcr_complaint_nature_code = 'LIVPRES';
+
+-- Add unique constraing on staging_metadata_mapping for entity_code, staged_data_value, live_data_value
+ALTER TABLE public.staging_metadata_mapping ADD CONSTRAINT staging_metadata_mapping_unique UNIQUE (entity_code, staged_data_value, live_data_value);
