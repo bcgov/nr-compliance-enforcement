8e7483c61d81a297d6db9bf7cc4e281a54456e19
Mike Vesprini <mike@button.is>
2025-01-13 16:46:19 -0800

    Replace COS_OFFICER with COS role

--- backend/src/v1/officer/officer.controller.ts
+++ backend/src/v1/officer/officer.controller.ts
@@ -20,43 +20,43 @@ export class OfficerController {
   constructor(private readonly officerService: OfficerService) {}
 
   @Post()
-  @Roles(Role.COS_OFFICER)
+  @Roles(Role.COS)
   create(@Body() createOfficerDto: NewOfficer) {
     return this.officerService.create(createOfficerDto);
   }
 
   @Get()
-  @Roles(Role.COS_OFFICER, Role.CEEB)
+  @Roles(Role.COS, Role.CEEB)
   findAll() {
     return this.officerService.findAll();
   }
 
   @Get(":id")
-  @Roles(Role.COS_OFFICER)
+  @Roles(Role.COS)
   findOne(@Param("id") id: string) {
     return this.officerService.findOne(id);
   }
 
   @Get("/find-by-office/:office_guid")
-  @Roles(Role.COS_OFFICER)
+  @Roles(Role.COS)
   findByOffice(@Param("office_guid") office_guid: string) {
     return this.officerService.findByOffice(office_guid);
   }
 
   @Get("/find-by-auth-user-guid/:auth_user_guid")
-  @Roles(Role.COS_OFFICER, Role.CEEB)
+  @Roles(Role.COS, Role.CEEB)
   findByAuthUserGuid(@Param("auth_user_guid") auth_user_guid: string) {
     return this.officerService.findByAuthUserGuid(auth_user_guid);
   }
 
   @Get("/find-by-userid/:userid")
-  @Roles(Role.COS_OFFICER, Role.CEEB)
+  @Roles(Role.COS, Role.CEEB)
   findByUserId(@Param("userid") userid: string) {
     return this.officerService.findByUserId(userid);
   }
 
   @Get("/find-by-person-guid/:person_guid")
-  @Roles(Role.COS_OFFICER, Role.CEEB, Role.TEMPORARY_TEST_ADMIN)
+  @Roles(Role.COS, Role.CEEB, Role.TEMPORARY_TEST_ADMIN)
   findByPersonId(@Param("person_guid") person_guid: string) {
     return this.officerService.findByPersonGuid(person_guid);
   }
@@ -68,19 +68,19 @@ export class OfficerController {
   }
 
   @Patch(":id")
-  @Roles(Role.COS_OFFICER, Role.CEEB, Role.TEMPORARY_TEST_ADMIN)
+  @Roles(Role.COS, Role.CEEB, Role.TEMPORARY_TEST_ADMIN)
   update(@Param("id") id: UUID, @Body() updateOfficerDto: UpdateOfficerDto) {
     return this.officerService.update(id, updateOfficerDto);
   }
 
   @Put("/request-coms-access/:officer_guid")
-  @Roles(Role.CEEB, Role.COS_OFFICER, Role.READ_ONLY)
+  @Roles(Role.CEEB, Role.COS, Role.READ_ONLY)
   requestComsAccess(@Token() token, @Param("officer_guid") officer_guid: UUID, @User() user) {
     return this.officerService.requestComsAccess(token, officer_guid, user);
   }
 
   @Delete(":id")
-  @Roles(Role.COS_OFFICER)
+  @Roles(Role.COS)
   remove(@Param("id") id: string) {
     return this.officerService.remove(+id);
   }
