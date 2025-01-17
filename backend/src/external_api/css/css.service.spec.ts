b32cae91120e9e56b98536a4804bfd3dd714ea6e
Ryan Rondeau <ryan_rondeau@live.ca>
2025-01-16 20:51:37 -0800

    Update css.service.spec.ts

--- backend/src/external_api/css/css.service.spec.ts
+++ backend/src/external_api/css/css.service.spec.ts
@@ -3,12 +3,14 @@ import { getRepositoryToken } from "@nestjs/typeorm";
 import { ConfigurationService } from "../../v1/configuration/configuration.service";
 import { Configuration } from "../../v1/configuration/entities/configuration.entity";
 import { CssService } from "./css.service";
+import { CacheModule } from "@nestjs/cache-manager";
 
 describe("CssService", () => {
   let service: CssService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       providers: [
         CssService,
         ConfigurationService,
