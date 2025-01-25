d5e42ec65a7043791ae94065a1b5b0c076d517dc
afwilcox <alecwilcox@gmail.com>
2025-01-24 16:23:59 -0800

    feat: change filename on exports to date logged (#901)

--- frontend/src/app/types/complaints/export-complaint-input.ts
+++ frontend/src/app/types/complaints/export-complaint-input.ts
@@ -3,6 +3,7 @@ import { AttachmentsState } from "../state/attachments-state";
 export interface ExportComplaintInput {
   id: string;
   type: string;
+  fileName: string;
   tz: string;
   attachments: AttachmentsState;
 }
