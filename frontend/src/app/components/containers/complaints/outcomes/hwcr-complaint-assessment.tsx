e8deb0e056dde03a4400b391f813f8e28c2f7509
Jonathan Funk <johnthomasfunk@gmail.com>
2025-02-03 14:44:37 -0800

    assessment officer used for auto assign

--- frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/hwcr-complaint-assessment.tsx
@@ -10,6 +10,7 @@ import {
   selectComplaintLargeCarnivoreInd,
   selectLinkedComplaints,
   selectComplaintViewMode,
+  selectComplaint,
 } from "@store/reducers/complaints";
 import {
   selectAssessmentCat1Dropdown,
@@ -39,6 +40,8 @@ import { OptionLabels } from "@constants/option-labels";
 import { HWCRComplaintAssessmentLinkComplaintSearch } from "./hwcr-complaint-assessment-link-complaint-search";
 import { CompRadioGroup } from "@/app/components/common/comp-radiogroup";
 import useValidateComplaint from "@hooks/validate-complaint";
+import { assignComplaintToOfficer } from "@store/reducers/officer";
+import { Officer } from "@/app/types/person/person";
 
 type Props = {
   id: string;
@@ -65,6 +68,7 @@ export const HWCRComplaintAssessment: FC<Props> = ({
   const [editable, setEditable] = useState<boolean>(true);
   const [validateOnChange, setValidateOnChange] = useState<boolean>(false);
   const [selectedContacted, setSelectedContacted] = useState<string | null>("No");
+  const [selectedOfficerData, setSelectedOfficerData] = useState<Officer | null>();
   const [selectedAttended, setSelectedAttended] = useState<string | null>("No");
   const [selectedLocation, setSelectedLocation] = useState<Option | null>(null);
   const [selectedConflictHistory, setSelectedConflictHistory] = useState<Option | null>(null);
@@ -84,6 +88,7 @@ export const HWCRComplaintAssessment: FC<Props> = ({
   const [assessmentRequiredErrorMessage, setAssessmentRequiredErrorMessage] = useState<string>("");
   const [locationErrorMessage, setLocationErrorMessage] = useState<string>("");
 
+  const complaintData = useAppSelector(selectComplaint);
   const linkedComplaintData = useAppSelector(selectLinkedComplaints);
   const assessmentState = useAppSelector(selectAssessment);
   const { ownedByAgencyCode } = useAppSelector(selectComplaintCallerInformation);
@@ -155,6 +160,14 @@ export const HWCRComplaintAssessment: FC<Props> = ({
     } else {
       setSelectedLinkedComplaint(null);
     }
+  };
+
+  const handleSelectedOfficerChange = (selected: Option | null) => {
+    if (selected && officersInAgencyList) {
+      let filteredOfficer = officersInAgencyList?.find((officer) => officer.auth_user_guid === selected.value);
+      setSelectedOfficerData(filteredOfficer);
+    }
+    setSelectedOfficer(selected);
   };
 
   const actionRequiredList = useAppSelector(selectYesNoCodeDropdown);
@@ -379,6 +392,14 @@ export const HWCRComplaintAssessment: FC<Props> = ({
       };
 
       dispatch(upsertAssessment(id, updatedAssessmentData));
+      if (
+        selectedOfficer?.value &&
+        !assigned &&
+        selectedOfficerData?.person_guid?.person_guid &&
+        (complaintData?.delegates.length === 0 || complaintData?.delegates?.every((delegate) => !delegate.isActive))
+      ) {
+        dispatch(assignComplaintToOfficer(id, selectedOfficerData?.person_guid?.person_guid));
+      }
       setEditable(false);
       handleSave();
     } else {
@@ -828,7 +849,7 @@ export const HWCRComplaintAssessment: FC<Props> = ({
                     errorMessage={officerErrorMessage}
                     value={selectedOfficer}
                     placeholder="Select "
-                    onChange={(officer: any) => setSelectedOfficer(officer)}
+                    onChange={handleSelectedOfficerChange}
                     isDisabled={isReadOnly}
                   />
                 </div>
