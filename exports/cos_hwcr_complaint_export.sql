a4d1b4023f01d8903ad0adc0a75bc3b4f26dcea8
afwilcox <alecwilcox@gmail.com>
2025-02-11 10:52:45 -0800

    feat: timezone improvements

--- exports/cos_hwcr_complaint_export.sql
+++ exports/cos_hwcr_complaint_export.sql
@@ -8,15 +8,39 @@ select distinct
         	when hwc.complaint_identifier is not null then 'HWCR'
 		else 'Unknown' 
     	end as "Complaint Type",
-        ((cmp.incident_reported_utc_timestmp at time zone 'UTC') at time zone 'PDT') as "Date Logged (PDT)",
-	    ((cmp.incident_utc_datetime at time zone 'UTC') at time zone 'PDT') as "Incident Datetime (PDT)",
+	case -- This is a temporary solution as it isn't really scalable.
+	    when cmp.incident_reported_utc_timestmp at time zone 'UTC' at time zone 'America/Los_Angeles' 
+	         between 
+	             '2025-03-08 02:00:00' and '2025-11-02 01:59:59' -- DST start and end for this year (2025)
+	         or
+	         cmp.incident_reported_utc_timestmp at time zone 'UTC' at time zone 'America/Los_Angeles' 
+	         between 
+	             '2024-03-10 02:00:00' and '2024-11-03 01:59:59' -- DST start and end for last year (2024)
+	    then
+	        ((cmp.incident_reported_utc_timestmp at time zone 'UTC') at time zone 'PDT')  -- PDT if within DST
+	    else
+	        ((cmp.incident_reported_utc_timestmp at time zone 'UTC') at time zone 'PST')  -- PST if outside DST
+	end as "Date Logged (PDT/PST)",
+	case -- This is a temporary solution as it isn't really scalable.
+	    when cmp.incident_utc_datetime at time zone 'UTC' at time zone 'America/Los_Angeles' 
+	         between 
+	             '2025-03-08 02:00:00' and '2025-11-02 01:59:59' -- DST start and end for this year (2025)
+	         or
+	         cmp.incident_utc_datetime at time zone 'UTC' at time zone 'America/Los_Angeles' 
+	         between 
+	             '2024-03-10 02:00:00' and '2024-11-03 01:59:59' -- DST start and end for last year (2024)
+	    then
+	        ((cmp.incident_utc_datetime at time zone 'UTC') at time zone 'PDT')  -- PDT if within DST
+	    else
+	        ((cmp.incident_utc_datetime at time zone 'UTC') at time zone 'PST')  -- PST if outside DST
+	end as "Incident Datetime (PDT/PST)",
 	cst.short_description as "Complaint Status",
 	spc.short_description as "Species",
 	case
         when hch.data_after_executed_operation ->> 'species_code' != hwc.species_code THEN 
         	prev_spc.short_description
         else ''
-    END AS "Previous Species",
+    END AS "Original Species",
 	hnc.long_description as "Nature of Complaint",
 	coalesce (cup.update_count, 0) as "Number of Updates",
 	goc.short_description as "Area/Community",
@@ -55,4 +79,7 @@ LEFT JOIN
 LEFT JOIN 
     species_code prev_spc ON prev_spc.species_code = hch.data_after_executed_operation ->> 'species_code' 
 order by
-	cmp.complaint_identifier asc
+	cmp.complaint_identifier desc
+	
+	
+	
