e443f40b28eee189ff4de0bc5df547223d2199fa
afwilcox <alecwilcox@gmail.com>
2025-02-14 15:39:43 -0800

    feat: Export updates (#937)

--- exports/ers_hwcr_complaint_export.sql
+++ exports/ers_hwcr_complaint_export.sql
@@ -5,9 +5,10 @@
 select distinct
 	cmp.complaint_identifier as "Complaint Identifier",
 	case 
-        	when hwc.complaint_identifier is not null then 'HWCR'
+    when alc.complaint_identifier is not null then 'ERS'
 		else 'Unknown' 
-    	end as "Complaint Type",
+  end as "Complaint Type",
+  vc.long_description as "Violation Type",
 	case -- This is a temporary solution as it isn't really scalable.
 	    when cmp.incident_reported_utc_timestmp at time zone 'UTC' at time zone 'America/Los_Angeles' 
 	         between 
@@ -35,13 +36,6 @@ select distinct
 	        ((cmp.incident_utc_datetime at time zone 'UTC') at time zone 'PST')  -- PST if outside DST
 	end as "Incident Datetime (PDT/PST)",
 	cst.short_description as "Complaint Status",
-	spc.short_description as "Species",
-	case
-        when hch.data_after_executed_operation ->> 'species_code' != hwc.species_code THEN 
-        	prev_spc.short_description
-        else ''
-    END AS "Original Species",
-	hnc.long_description as "Nature of Complaint",
 	coalesce (cup.update_count, 0) as "Number of Updates",
 	cu.create_utc_timestamp as "Update Logged Date",
 	goc.short_description as "Area/Community",
@@ -51,7 +45,7 @@ select distinct
 	ST_X(cmp.location_geometry_point) AS "Longitude (X)",
     ST_Y(cmp.location_geometry_point) AS "Latitude (Y)",
 	per.last_name || ', ' || per.first_name as "Officer Assigned",
-	cmp.reference_number as "COORS Number"
+	cmp.reference_number AS "COORS Number"
 from 
 	complaint cmp
 join 
@@ -64,23 +58,17 @@ left join
 	person_complaint_xref pcx on pcx.complaint_identifier = cmp.complaint_identifier and pcx.active_ind = true
 left join 
 	person per on per.person_guid = pcx.person_guid 
-right join 
-	hwcr_complaint hwc on hwc.complaint_identifier = cmp.complaint_identifier 
-left join
-	species_code spc on spc.species_code = hwc.species_code
-left join 
-	hwcr_complaint_nature_code hnc on hnc.hwcr_complaint_nature_code = hwc.hwcr_complaint_nature_code 
+right join
+	allegation_complaint alc on alc.complaint_identifier = cmp.complaint_identifier 
 left join (
     select complaint_identifier, count(*) as update_count
     from complaint_update
     group by complaint_identifier
 ) cup on cup.complaint_identifier = cmp.complaint_identifier
 left join
-   complaint_update cu ON cu.complaint_identifier = cmp.complaint_identifier 
-LEFT JOIN 
-    hwcr_complaint_h hch ON hch.target_row_id = hwc.hwcr_complaint_guid
-    AND hch.operation_type = 'I'
-LEFT JOIN 
-    species_code prev_spc ON prev_spc.species_code = hch.data_after_executed_operation ->> 'species_code' 
+   complaint_update cu ON cu.complaint_identifier = cmp.complaint_identifier
+left join
+	violation_code vc  on alc.violation_code = vc.violation_code 
+where cmp.owned_by_agency_code = 'COS'
 order by
 	cmp.complaint_identifier asc
