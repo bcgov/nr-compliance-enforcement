710f4bf1b6072190a94ad8435636ccc96db4504c
Scarlett <scarlett.truong307@gmail.com>
2025-03-05 16:37:55 -0800

    update yml

--- webeoc/src/webeoc-scheduler/webeoc-scheduler.service.ts
+++ webeoc/src/webeoc-scheduler/webeoc-scheduler.service.ts
@@ -98,6 +98,7 @@ export class WebEocScheduler {
       const files = await fs.promises.readdir(logDir);
       const cutoffDate = new Date();
       cutoffDate.setDate(cutoffDate.getDate() - this.retentionDays);
+      this.logger.debug(`Retention days: ${this.retentionDays}`);
       this.logger.debug(`Files: ${files}`);
 
       for (const file of files) {
