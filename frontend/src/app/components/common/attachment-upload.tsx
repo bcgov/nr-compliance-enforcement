b5ff5968e3f379fca85f3489d3957714def297bf
Mike Vesprini <mike@button.is>
2025-03-10 13:59:33 -0700

    Add dropzone for attachment uploads

--- frontend/src/app/components/common/attachment-upload.tsx
+++ frontend/src/app/components/common/attachment-upload.tsx
@@ -1,4 +1,5 @@
-import { FC, useRef } from "react";
+import { FC, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import { BsPlus } from "react-icons/bs";
 
 type Props = {
@@ -7,48 +8,35 @@ type Props = {
 };
 
 export const AttachmentUpload: FC<Props> = ({ onFileSelect, disabled }) => {
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      onFileSelect(event.target.files);
-    }
-  };
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const dataTransfer = new DataTransfer();
+      acceptedFiles.forEach((file) => {
+        dataTransfer.items.add(file);
+      });
+      onFileSelect(dataTransfer.files);
+    },
+    [onFileSelect],
+  );
 
-  // Without this, I'm unable to re-add the same file twice.
-  const handleFileClick = () => {
-    // Clear the current file input value
-    if (fileInputRef.current) {
-      fileInputRef.current.value = "";
-    }
-  };
-
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleDivClick = () => {
-    fileInputRef.current?.click();
-  };
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    disabled: disabled ?? false,
+  });
 
   return (
-    <div>
-      <input
-        type="file"
-        multiple
-        onChange={handleFileChange}
-        onClick={handleFileClick}
-        ref={fileInputRef}
-        style={{ display: "none" }}
-      />
-      <button
-        className="comp-attachment-upload-btn"
-        tabIndex={0}
-        onClick={handleDivClick}
-        disabled={disabled ?? false}
-        style={disabled ?? false ? { cursor: "default" } : {}}
-      >
-        <div className="upload-icon">
-          <BsPlus />
-        </div>
-        <div className="upload-text">Upload</div>
-      </button>
+    <div
+      {...getRootProps()}
+      className="comp-attachment-upload-btn"
+      style={disabled ?? false ? { cursor: "default" } : {}}
+    >
+      <input {...getInputProps()} />
+      <div className="upload-icon">
+        <BsPlus />
+      </div>
+      <div className="upload-text">{isDragActive ? "Drop files here" : "Upload"}</div>
     </div>
   );
 };
+
+export default AttachmentUpload;
