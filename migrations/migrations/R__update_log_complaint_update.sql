0d160770ab61157addb845edc62030a276a0caf5
Mike Vesprini <mike@button.is>
2025-03-26 15:06:48 -0700

    Fix ELSIF statements

--- migrations/migrations/R__update_log_complaint_update.sql
+++ migrations/migrations/R__update_log_complaint_update.sql
@@ -118,7 +118,7 @@ BEGIN
         (_upd_detail_text IS DISTINCT FROM current_complaint_record.detail_text OR current_complaint_record.detail_text IS NULL)) THEN
         insert_upd_detail_text := _upd_detail_text;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_detail_text := prev_complaint_update_record.upd_detail_text;
     ELSE
         insert_upd_detail_text := NULL;
@@ -129,7 +129,7 @@ BEGIN
         (_upd_location_summary_text IS DISTINCT FROM current_complaint_record.location_summary_text OR current_complaint_record.location_summary_text IS NULL)) THEN
         insert_upd_location_summary_text := _upd_location_summary_text;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_location_summary_text := prev_complaint_update_record.upd_location_summary_text;
     ELSE
         insert_upd_location_summary_text := NULL;
@@ -140,7 +140,7 @@ BEGIN
         (_upd_location_detailed_text IS DISTINCT FROM current_complaint_record.location_detailed_text OR current_complaint_record.location_detailed_text IS NULL)) THEN
         insert_upd_location_detailed_text := _upd_location_detailed_text;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_location_detailed_text := prev_complaint_update_record.upd_location_detailed_text;
     ELSE
         insert_upd_location_detailed_text := NULL;
@@ -151,7 +151,7 @@ BEGIN
         (_upd_location_geometry_point IS DISTINCT FROM current_complaint_record.location_geometry_point OR current_complaint_record.location_geometry_point IS NULL)) THEN
         insert_upd_location_geometry_point := _upd_location_geometry_point;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_location_geometry_point := prev_complaint_update_record.upd_location_geometry_point;
     ELSE
         insert_upd_location_geometry_point := NULL;
@@ -162,7 +162,7 @@ BEGIN
         (_upd_caller_name IS DISTINCT FROM current_complaint_record.caller_name OR current_complaint_record.caller_name IS NULL)) THEN
         insert_upd_caller_name := _upd_caller_name;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_name := prev_complaint_update_record.upd_caller_name;
     ELSE
         insert_upd_caller_name := NULL;
@@ -173,7 +173,7 @@ BEGIN
         (_upd_caller_phone_1 IS DISTINCT FROM current_complaint_record.caller_phone_1 OR current_complaint_record.caller_phone_1 IS NULL)) THEN
         insert_upd_caller_phone_1 := _upd_caller_phone_1;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_phone_1 := prev_complaint_update_record.upd_caller_phone_1;
     ELSE
         insert_upd_caller_phone_1 := NULL;
@@ -184,7 +184,7 @@ BEGIN
         (_upd_caller_phone_2 IS DISTINCT FROM current_complaint_record.caller_phone_2 OR current_complaint_record.caller_phone_2 IS NULL)) THEN
         insert_upd_caller_phone_2 := _upd_caller_phone_2;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_phone_2 := prev_complaint_update_record.upd_caller_phone_2;
     ELSE
         insert_upd_caller_phone_2 := NULL;
@@ -195,7 +195,7 @@ BEGIN
         (_upd_caller_phone_3 IS DISTINCT FROM current_complaint_record.caller_phone_3 OR current_complaint_record.caller_phone_3 IS NULL)) THEN
         insert_upd_caller_phone_3 := _upd_caller_phone_3;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_phone_3 := prev_complaint_update_record.upd_caller_phone_3;
     ELSE
         insert_upd_caller_phone_3 := NULL;
@@ -206,7 +206,7 @@ BEGIN
         (_upd_caller_address IS DISTINCT FROM current_complaint_record.caller_address OR current_complaint_record.caller_address IS NULL)) THEN
         insert_upd_caller_address := _upd_caller_address;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_address := prev_complaint_update_record.upd_caller_address;
     ELSE
         insert_upd_caller_address := NULL;
@@ -217,7 +217,7 @@ BEGIN
         (_upd_caller_email IS DISTINCT FROM current_complaint_record.caller_email OR current_complaint_record.caller_email IS NULL)) THEN
         insert_upd_caller_email := _upd_caller_email;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_caller_email := prev_complaint_update_record.upd_caller_email;
     ELSE
         insert_upd_caller_email := NULL;
@@ -228,7 +228,7 @@ BEGIN
         (_upd_reported_by_code IS DISTINCT FROM current_complaint_record.reported_by_code OR current_complaint_record.reported_by_code IS NULL)) THEN
         insert_upd_reported_by_code := _upd_reported_by_code;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_reported_by_code := prev_complaint_update_record.upd_reported_by_code;
     ELSE
         insert_upd_reported_by_code := NULL;
@@ -239,7 +239,7 @@ BEGIN
         (_upd_reported_by_other_text IS DISTINCT FROM current_complaint_record.reported_by_other_text OR current_complaint_record.reported_by_other_text IS NULL)) THEN
         insert_upd_reported_by_other_text := _upd_reported_by_other_text;
         has_difference := TRUE;
-    ELSE IF _update_number_exists THEN
+    ELSIF _update_number_exists THEN
         insert_upd_reported_by_other_text := prev_complaint_update_record.upd_reported_by_other_text;
     ELSE
         insert_upd_reported_by_other_text := NULL;
