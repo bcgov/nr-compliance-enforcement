26c1fe264188885778586f989a2fab09d99e050f
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-07 09:14:44 -0700

    Update R__Test-Data-Creation.sql

--- migrations/test-only-migrations/R__Test-Data-Creation.sql
+++ migrations/test-only-migrations/R__Test-Data-Creation.sql
@@ -1938,16 +1938,3 @@ WHERE complaint_identifier IN ('23-031744', '23-006888', '23-032456');
 UPDATE public.feature_agency_xref
 SET active_ind = 'Y'
 WHERE feature_code = 'COMPREF';
-
----------------------
--- Add test park data to dev/test
----------------------
-
-insert into "shared".park ("external_id", "name", "create_user_id", "update_user_id") 
-values ('1', 'Strathcona Park - Lindsay Loop', 'system', 'system');
-insert into "shared".park ("external_id", "name", "create_user_id", "update_user_id") 
-values ('10', 'Inonoaklin Park', 'system', 'system');
-insert into "shared".park ("external_id", "name", "create_user_id", "update_user_id") 
-values ('1000', 'Hunwadi/Ahnuhati-Bald Conservancy', 'system', 'system');
-insert into "shared".park ("external_id", "name", "create_user_id", "update_user_id") 
-values ('73', 'sx̌ʷəx̌ʷnitkʷ Park', 'system', 'system');
