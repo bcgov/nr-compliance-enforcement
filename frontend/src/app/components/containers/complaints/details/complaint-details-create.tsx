9d014077db11d428215bb61b1b84f73fc53d24e5
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-07 17:05:10 -0700

    fix bug

--- frontend/src/app/components/containers/complaints/details/complaint-details-create.tsx
+++ frontend/src/app/components/containers/complaints/details/complaint-details-create.tsx
@@ -200,7 +200,6 @@ export const CreateComplaint: FC = () => {
         updatedBy: userid,
         complaintMethodReceivedCode: "",
         isPrivacyRequested: "U",
-        parkGuid: "",
       };
 
       applyComplaintData(model);
