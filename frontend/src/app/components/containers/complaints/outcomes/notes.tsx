9bd7d9844dc90d7791434fa0ed7f15c131fbdb8c
Mike Vesprini <mike@button.is>
2025-04-22 11:42:36 -0700

    Allow collaborators to add notes

--- frontend/src/app/components/containers/complaints/outcomes/notes.tsx
+++ frontend/src/app/components/containers/complaints/outcomes/notes.tsx
@@ -5,13 +5,17 @@ import { selectIsInEdit, selectNotes } from "@store/reducers/case-selectors";
 import { ComplaintParams } from "@components/containers/complaints/details/complaint-details-edit";
 import { Note } from "./notes/note";
 import { useAppSelector } from "@/app/hooks/hooks";
-import { selectComplaintViewMode } from "@/app/store/reducers/complaints";
+import { selectComplaintViewMode, selectComplaintCollaborators } from "@/app/store/reducers/complaints";
+import { personGuid } from "@/app/store/reducers/app";
 
 export const Notes: FC = () => {
   const { id = "", complaintType = "" } = useParams<ComplaintParams>();
   const isInEdit = useAppSelector(selectIsInEdit);
   const isReadOnly = useAppSelector(selectComplaintViewMode);
   const notes = useAppSelector(selectNotes);
+  const collaborators = useAppSelector(selectComplaintCollaborators);
+  const userPersonGuid = useAppSelector(personGuid);
+  const [userIsCollaborator, setUserIsCollaborator] = useState<boolean>(false);
 
   const [showAddNote, setShowAddNote] = useState(false);
 
@@ -20,6 +24,10 @@ export const Notes: FC = () => {
       setShowAddNote(false);
     }
   }, [isInEdit.notes]);
+
+  useEffect(() => {
+    setUserIsCollaborator(collaborators.some((c) => c.personGuid === userPersonGuid));
+  }, [collaborators, userPersonGuid]);
 
   return (
     <section
@@ -49,7 +57,7 @@ export const Notes: FC = () => {
           id="outcome-report-add-note"
           title="Add additional notes"
           onClick={(e) => setShowAddNote(true)}
-          disabled={isReadOnly}
+          disabled={isReadOnly && !userIsCollaborator}
         >
           <i className="bi bi-plus-circle"></i>
           <span>Add note</span>
