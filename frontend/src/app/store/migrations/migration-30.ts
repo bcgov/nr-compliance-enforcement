83a14c0c896010d6d96c3952bf904a0add4f4c17
Mike Vesprini <mike@button.is>
2025-04-25 21:51:56 -0700

    Add actioned by to code tables

---
+++ frontend/src/app/store/migrations/migration-30.ts
@@ -0,0 +1,12 @@
+export const addHwcrOutcomeActionedByCodes = {
+  30: (state: any) => {
+    // Also includes new wildlife outcome codes
+    return {
+      ...state,
+      codeTables: {
+        "wildlife-outcomes": [],
+        "hwcr-outcome-actioned-by-codes": [],
+      },
+    };
+  },
+};
---
+++ frontend/src/app/types/app/code-tables/hwcr-outcome-actioned-by.ts
@@ -0,0 +1,5 @@
+import { BaseCodeTable } from "./base-code-table";
+
+export interface HwcrOutcomeActionedBy extends BaseCodeTable {
+  actionedBy: string;
+}
