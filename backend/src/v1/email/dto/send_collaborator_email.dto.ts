96d1f2481f9e19336fe438691091d8c9c5751032
Mike Vesprini <mike@button.is>
2025-05-22 11:20:09 -0700

    New files needed for collab emails

---
+++ backend/src/v1/email/dto/send_collaborator_email.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { UUID } from "crypto";
+import { COMPLAINT_TYPE } from "../../../types/models/complaints/complaint-type";
+
+export class SendCollaboratorEmalDto {
+  @ApiProperty({
+    example: "ERS",
+    description: "The type of complaint that the collaborator is being added to.",
+  })
+  public complaintType: COMPLAINT_TYPE;
+
+  @ApiProperty({
+    example: "https://natcom.test.gov.bc.ca/complaint/HWCR/25-000062",
+    description: "The url of the complaint details page for the given complaint.",
+  })
+  complaintUrl: string;
+
+  @ApiProperty({
+    example: "903f87c8-76dd-427c-a1bb-4d179e443252",
+    description: "The person guid for the officer",
+  })
+  personGuid: UUID;
+}
