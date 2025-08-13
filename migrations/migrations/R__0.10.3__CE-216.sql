dcafbd5cc56bd17cdee8e5d6ca7197a606956fb2
afwilcox <alecwilcox@gmail.com>
2025-08-13 14:57:03 -0700

    fix: typo

--- migrations/migrations/R__0.10.3__CE-216.sql
+++ migrations/migrations/R__0.10.3__CE-216.sql
@@ -6,13 +6,13 @@ UPDATE complaint SET owned_by_agency_code_ref = 'EPO'
 FROM allegation_complaint
 WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier 
    AND allegation_complaint.violation_code = 'WASTE'
-   AND complaint.owened_by_agency_code_ref IS NULL;
+   AND complaint.owned_by_agency_code_ref IS NULL;
 
 UPDATE complaint SET owned_by_agency_code_ref = 'EPO'
 FROM allegation_complaint 
 WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier 
    AND allegation_complaint.violation_code = 'PESTICDE'
-   AND complaint.owened_by_agency_code_ref IS NULL;
+   AND complaint.owned_by_agency_code_ref IS NULL;
 
 UPDATE complaint SET owned_by_agency_code_ref = 'COS'
 FROM allegation_complaint 
