4063e25968fac7078838dee8971fa9d1e95db1f8
Ryan Rondeau <nayr974@users.noreply.github.com>
2025-08-14 10:34:40 -0700

    fix: CE-1845 (#1196)
    Co-authored-by: team-wolverine-nrm-repo-updater[bot] <203337993+team-wolverine-nrm-repo-updater[bot]@users.noreply.github.com>

--- frontend/src/app/store/reducers/officer.ts
+++ frontend/src/app/store/reducers/officer.ts
@@ -508,7 +508,7 @@ export const selectOfficersByZoneAgencyAndRole =
       if (zone) {
         return officers.filter((officer) => {
           const zoneCode = officer?.office_guid?.cos_geo_org_unit?.zone_code ?? null;
-          const agencyCode = officer?.office_guid?.agency_code?.agency_code ?? null;
+          const agencyCode = officer?.office_guid?.agency_code_ref ?? null;
           const fromAdminOffice = officer?.office_guid?.cos_geo_org_unit?.administrative_office_ind;
           const zoneAgencyMatch = zone === zoneCode && (agency === agencyCode || !agency);
           const roleMatch =
