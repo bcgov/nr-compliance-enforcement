f3ebb4c4071cca0ae5621b310119229919104e93
afwilcox <alecwilcox@gmail.com>
2025-08-14 23:45:05 -0700

    chore: Update dangerous re-runnable DB script (#1197)
    Co-authored-by: Ryan Rondeau <nayr974@users.noreply.github.com>
    Co-authored-by: team-wolverine-nrm-repo-updater[bot] <203337993+team-wolverine-nrm-repo-updater[bot]@users.noreply.github.com>

--- migrations/migrations/R__0.10.3__CE-216.sql
+++ migrations/migrations/R__0.10.3__CE-216.sql
@@ -2,21 +2,17 @@
 -- update complaint owner to EPO agency where allegation violation_code is WASTE or PESTICDE
 --
 
---
--- ATTENTION IF THIS FILE IS EVER TOUCHED ON A RELEASE FOR ANY REASON!!!!!!!!!!!!
---
--- If this file is touched causing the checksum to change flyway will execute it in the 
--- various environment, INCLUDING PRODUCTION.  This may not be desired, especially the first two 
--- statements.   Think twice before touching this file.  In retrospect this should have been a versioned script.
---
-
 UPDATE complaint SET owned_by_agency_code_ref = 'EPO'
 FROM allegation_complaint
-WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier AND allegation_complaint.violation_code = 'WASTE';
+WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier 
+   AND allegation_complaint.violation_code = 'WASTE'
+   AND complaint.owned_by_agency_code_ref IS NULL;
 
 UPDATE complaint SET owned_by_agency_code_ref = 'EPO'
 FROM allegation_complaint 
-WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier AND allegation_complaint.violation_code = 'PESTICDE';
+WHERE complaint.complaint_identifier = allegation_complaint.complaint_identifier 
+   AND allegation_complaint.violation_code = 'PESTICDE'
+   AND complaint.owned_by_agency_code_ref IS NULL;
 
 UPDATE complaint SET owned_by_agency_code_ref = 'COS'
 FROM allegation_complaint 
