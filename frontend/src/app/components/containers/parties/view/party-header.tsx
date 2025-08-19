b7b72d6d25447f8390cd433964e2cc9d7cf7987a
Dmitri Korin <dmitri.korin@gov.bc.ca>
2025-08-19 09:28:48 -0700

    Remove redundant fragment

--- frontend/src/app/components/containers/parties/view/party-header.tsx
+++ frontend/src/app/components/containers/parties/view/party-header.tsx
@@ -14,57 +14,55 @@ export const PartyHeader: FC<PartyHeaderProps> = ({ partyData }) => {
   const dateLogged = partyData?.partyCreatedDateTime ? new Date(partyData.partyCreatedDateTime).toString() : undefined;
 
   return (
-    <>
-      <div className="comp-details-header">
-        <div className="comp-container">
-          {/* <!-- breadcrumb start --> */}
-          <div className="comp-complaint-breadcrumb">
-            <nav aria-label="breadcrumb">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item comp-nav-item-name-inverted">
-                  <Link
-                    to=""
-                    onClick={() => window.alert("Navigating to the list of parties")}
-                  >
-                    Parties of Interest
-                  </Link>
-                </li>
-                <li
-                  className="breadcrumb-item"
-                  aria-current="page"
+    <div className="comp-details-header">
+      <div className="comp-container">
+        {/* <!-- breadcrumb start --> */}
+        <div className="comp-complaint-breadcrumb">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item comp-nav-item-name-inverted">
+                <Link
+                  to=""
+                  onClick={() => window.alert("Navigating to the list of parties")}
                 >
-                  {partyId}
-                </li>
-              </ol>
-            </nav>
-          </div>
-          {/* <!-- breadcrumb end --> */}
+                  Parties of Interest
+                </Link>
+              </li>
+              <li
+                className="breadcrumb-item"
+                aria-current="page"
+              >
+                {partyId}
+              </li>
+            </ol>
+          </nav>
+        </div>
+        {/* <!-- breadcrumb end --> */}
 
-          {/* <!-- party info start --> */}
-          <div className="comp-details-title-container">
-            <div className="comp-details-title-info">
-              <h1 className="comp-box-complaint-id">
-                <span>Party of Interest</span>&nbsp;&nbsp;#{partyId}
-              </h1>
-            </div>
-            <ActionMenu />
+        {/* <!-- party info start --> */}
+        <div className="comp-details-title-container">
+          <div className="comp-details-title-info">
+            <h1 className="comp-box-complaint-id">
+              <span>Party of Interest</span>&nbsp;&nbsp;#{partyId}
+            </h1>
           </div>
+          <ActionMenu />
+        </div>
 
-          {/* Party Type Details */}
-          <div
-            className="mt-1 max-width-48ch"
-            id="comp-nature-of-complaint"
-          >
-            <span>{partyType}</span>
-          </div>
-          <div
-            className="mt-1 max-width-48ch"
-            id="comp-nature-of-complaint"
-          >
-            <span>{`Created on: ${formatDate(dateLogged)} ${formatTime(dateLogged)}`}</span>
-          </div>
+        {/* Party Type Details */}
+        <div
+          className="mt-1 max-width-48ch"
+          id="comp-nature-of-complaint"
+        >
+          <span>{partyType}</span>
+        </div>
+        <div
+          className="mt-1 max-width-48ch"
+          id="comp-nature-of-complaint"
+        >
+          <span>{`Created on: ${formatDate(dateLogged)} ${formatTime(dateLogged)}`}</span>
         </div>
       </div>
-    </>
+    </div>
   );
 };
