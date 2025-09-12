0ec5e7b9b7e3c066370c0b9bc79c9cf17c671989
afwilcox <alecwilcox@gmail.com>
2025-09-12 11:09:06 -0700

    fix: missing test data, minor script refactor

---
+++ migrations-inspection/migrations/R__inspection_officer_xref.sql
@@ -0,0 +1,28 @@
+-- Add assignee to code table
+insert into inspection.officer_inspection_xref_code (
+  officer_inspection_xref_code,
+	short_description,
+  long_description,
+  display_order,
+  create_user_id,
+  create_utc_timestamp,
+  update_user_id,
+  update_utc_timestamp,
+  active_ind
+) values(
+  'ASSIGNEE',
+  'Officer assigned',
+  'The officer to whom the inspection is assigned.',
+  1,
+  user,
+  now(),
+  user,
+  now(),
+  true
+) ON CONFLICT (officer_inspection_xref_code) DO UPDATE SET
+    short_description = EXCLUDED.short_description,
+    long_description = EXCLUDED.long_description,
+    display_order = EXCLUDED.display_order,
+    active_ind = EXCLUDED.active_ind,
+    update_user_id = 'FLYWAY',
+    update_utc_timestamp = NOW();
--- migrations-inspection/test-only-migrations/R__data_inspections.sql
+++ migrations-inspection/test-only-migrations/R__data_inspections.sql
@@ -1,45 +1,44 @@
----------------------------------
--- Inserts code table values into the inspection_status_code table
--- or if the Code already exists, updates the data
----------------------------------
-INSERT INTO inspection.inspection_status_code (
-    inspection_status_code, short_description, long_description, display_order, active_ind, create_user_id, create_utc_timestamp
-) VALUES
-    ('OPEN', 'Open', 'Open', 10, TRUE, 'FLYWAY', NOW()),
-    ('CLOSED', 'Closed', 'Closed', 20, TRUE, 'FLYWAY', NOW())
-ON CONFLICT (inspection_status_code) DO UPDATE SET
-    short_description = EXCLUDED.short_description,
-    long_description = EXCLUDED.long_description,
-    display_order = EXCLUDED.display_order,
-    active_ind = EXCLUDED.active_ind,
-    update_user_id = 'FLYWAY',
-    update_utc_timestamp = NOW();
+-- Create test inspections
+insert into
+  inspection.inspection (
+    inspection_guid,
+    owned_by_agency_ref,
+    inspection_status,
+    inspection_opened_utc_timestamp,
+    create_user_id,
+    create_utc_timestamp
+  )
+values
+  (
+    '8116f74c-19d2-4903-9dfc-d5607c1fb900',
+    'COS',
+    'OPEN',
+    now (),
+    'system',
+    now ()
+  ),
+  (
+    '8116f74c-19d2-4903-9dfc-d5607c1fb901',
+    'COS',
+    'OPEN',
+    now (),
+    'system',
+    now ()
+  ),
+  (
+    '8116f74c-19d2-4903-9dfc-d5607c1fb902',
+    'COS',
+    'OPEN',
+    now (),
+    'system',
+    now ()
+  ),
+  (
+    '8116f74c-19d2-4903-9dfc-d5607c1fb903',
+    'COS',
+    'OPEN',
+    now (),
+    'system',
+    now ()
+  ) on conflict do nothing;
 
--- Add assignee to code table
-insert into inspection.officer_inspection_xref_code (
-  officer_inspection_xref_code,
-	short_description,
-  long_description,
-  display_order,
-  create_user_id,
-  create_utc_timestamp,
-  update_user_id,
-  update_utc_timestamp,
-  active_ind
-) values(
-  'ASSIGNEE',
-  'Officer assigned',
-  'The officer to whom the inspection is assigned.',
-  1,
-  user,
-  now(),
-  user,
-  now(),
-  true
-) ON CONFLICT (officer_inspection_xref_code) DO UPDATE SET
-    short_description = EXCLUDED.short_description,
-    long_description = EXCLUDED.long_description,
-    display_order = EXCLUDED.display_order,
-    active_ind = EXCLUDED.active_ind,
-    update_user_id = 'FLYWAY',
-    update_utc_timestamp = NOW();
