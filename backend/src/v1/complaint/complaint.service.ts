ee6967ddd216546f1de5efd6103be24aafb58fab
Scarlett <35635257+Scarlett-Truong@users.noreply.github.com>
2025-09-18 08:20:04 -0700

    fix: CE-1934 COS enforcement still shown when apply filters in Sector view (#1248)
    Co-authored-by: team-wolverine-nrm-repo-updater[bot] <203337993+team-wolverine-nrm-repo-updater[bot]@users.noreply.github.com>

--- backend/src/v1/complaint/complaint.service.ts
+++ backend/src/v1/complaint/complaint.service.ts
@@ -204,6 +204,10 @@ export class ComplaintService {
           .createQueryBuilder("allegation")
           .leftJoin("allegation.complaint_identifier", "complaint")
           .leftJoin("allegation.violation_code", "violation_code");
+
+        if (excludeCOSEnforcement) {
+          builder.andWhere("NOT (complaint.owned_by_agency_code_ref = 'COS')");
+        }
         break;
       case "GIR":
         builder = this._girComplaintRepository
@@ -279,6 +283,9 @@ export class ComplaintService {
             "violation_code.long_description",
             "violation_agency_xref.agency_code_ref",
           ]);
+        if (excludeCOSEnforcement) {
+          builder.andWhere("NOT (complaint.owned_by_agency_code_ref = 'COS')");
+        }
         break;
       case "GIR":
         builder = this._girComplaintRepository
@@ -1029,40 +1036,43 @@ export class ComplaintService {
   };
 
   canViewComplaint = async (id: string, req?: any): Promise<boolean> => {
-    let builder: SelectQueryBuilder<complaintAlias> | SelectQueryBuilder<Complaint>;
+    return true; // Temporarily allow COS Enforcement complaints to be viewed by all agencies
+    // Comment out the logic below until we have a requirement to restrict access again
 
-    try {
-      builder = this.complaintsRepository
-        .createQueryBuilder("complaint")
-        .leftJoin("complaint.complaint_type_code", "complaint_type")
-        .addSelect([
-          "complaint_type.complaint_type_code",
-          "complaint_type.short_description",
-          "complaint_type.long_description",
-        ]);
+    // let builder: SelectQueryBuilder<complaintAlias> | SelectQueryBuilder<Complaint>;
 
-      builder.where("complaint.complaint_identifier = :id", { id });
+    // try {
+    //   builder = this.complaintsRepository
+    //     .createQueryBuilder("complaint")
+    //     .leftJoin("complaint.complaint_type_code", "complaint_type")
+    //     .addSelect([
+    //       "complaint_type.complaint_type_code",
+    //       "complaint_type.short_description",
+    //       "complaint_type.long_description",
+    //     ]);
 
-      const res = await builder.getOne();
-      const { owned_by_agency_code_ref, complaint_type_code } = res;
+    //   builder.where("complaint.complaint_identifier = :id", { id });
 
-      if (complaint_type_code.complaint_type_code === "ERS") {
-        const hasCOSRole = hasRole(req, Role.COS);
-        const collaborators = await this._personService.getCollaborators(id);
-        const isCollab = collaborators.some(
-          (collab: any) => collab.authUserGuid.split("-").join("") === req.user.idir_user_guid.toLowerCase(),
-        );
-        const isCOSComplaint = owned_by_agency_code_ref === "COS";
-        if (isCOSComplaint) {
-          if (!hasCOSRole && !isCollab) {
-            return false;
-          }
-        }
-      }
-      return true;
-    } catch (e) {
-      this.logger.error(e);
-    }
+    //   const res = await builder.getOne();
+    //   const { owned_by_agency_code_ref, complaint_type_code } = res as Complaint;
+
+    //   if (complaint_type_code.complaint_type_code === "ERS") {
+    //     const hasCOSRole = hasRole(req, Role.COS);
+    //     const collaborators = await this._personService.getCollaborators(id);
+    //     const isCollab = collaborators.some(
+    //       (collab: any) => collab.authUserGuid.split("-").join("") === req.user.idir_user_guid.toLowerCase(),
+    //     );
+    //     const isCOSComplaint = owned_by_agency_code_ref === "COS";
+    //     if (isCOSComplaint) {
+    //       if (!hasCOSRole && !isCollab) {
+    //         return false;
+    //       }
+    //     }
+    //   }
+    //   return true;
+    // } catch (e) {
+    //   this.logger.error(e);
+    // }
   };
 
   findById = async (
