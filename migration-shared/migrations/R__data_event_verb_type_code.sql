00ddceb46aba7280a82c6b10d654f3fe3a4c289a
Ryan Rondeau <ryan_rondeau@live.ca>
2025-10-07 10:33:45 -0700

    Update R__data_event_verb_type_code.sql

--- migration-shared/migrations/R__data_event_verb_type_code.sql
+++ migration-shared/migrations/R__data_event_verb_type_code.sql
@@ -136,4 +136,37 @@ SET
   update_user_id = 'system',
   update_utc_timestamp = now ();
 
+INSERT INTO
+  shared.event_verb_type_code (
+    event_verb_type_code,
+    short_description,
+    long_description,
+    display_order,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+VALUES
+  (
+    'CREATED',
+    'Created',
+    'Created',
+    50,
+    true,
+    'system',
+    now (),
+    null,
+    null
+  ) ON CONFLICT (event_verb_type_code) DO
+UPDATE
+SET
+  short_description = EXCLUDED.short_description,
+  long_description = EXCLUDED.long_description,
+  display_order = EXCLUDED.display_order,
+  active_ind = EXCLUDED.active_ind,
+  update_user_id = 'system',
+  update_utc_timestamp = now ();
+
 ;
