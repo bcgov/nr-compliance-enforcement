ca05046a7e0a3c213c0d19c807a77d9b46b28bc6
Mike Vesprini <mike@button.is>
2025-10-09 13:11:47 -0700

    Add nats constants to backend-cm

---
+++ backend-cm/src/common/nats_constants.ts
@@ -0,0 +1,20 @@
+export const NATS_DURABLE_EVENTS = "nats_durable_events";
+
+export const STREAMS = {
+  EVENTS: "events",
+};
+
+export const STREAM_TOPICS = {
+  CASE_CREATED: "event.case.created",
+  CASE_CLOSED: "event.case.closed",
+  COMPLAINT_ADDED_TO_CASE: "event.complaint.added_to_case",
+  COMPLAINT_REMOVED_FROM_CASE: "event.complaint.removed_from_case",
+  INVESTIGATION_CREATED: "event.investigation.created",
+  INVESTIGATION_CLOSED: "event.investigation.closed",
+  INSPECTION_CREATED: "event.inspection.created",
+  INSPECTION_CLOSED: "event.inspection.closed",
+};
+
+export type StreamTopics = (typeof STREAM_TOPICS)[keyof typeof STREAM_TOPICS];
+
+export type EventVerbTypes = "OPENED" | "CLOSED" | "ADDED" | "REMOVED";
