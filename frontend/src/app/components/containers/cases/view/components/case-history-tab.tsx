5214dc9b6ac199bce50e8967473e1d11f9ab2ef7
Ryan Rondeau <ryan_rondeau@live.ca>
2025-10-10 10:40:06 -0700

    handle case

--- frontend/src/app/components/containers/cases/view/components/case-history-tab.tsx
+++ frontend/src/app/components/containers/cases/view/components/case-history-tab.tsx
@@ -96,7 +96,7 @@ export const CaseHistoryTab: FC<CaseHistoryTabProps> = ({ caseIdentifier }) => {
   useEffect(() => {
     if (events.length > 0 && allOfficers) {
       const userAuthGuids = getUserGuidsFromEvents(events);
-      const officers = allOfficers.filter((officer) => userAuthGuids.has(officer.auth_user_guid.split("-").join("")));
+      const officers = allOfficers.filter((officer) => userAuthGuids.has(officer.auth_user_guid.toUpperCase().split("-").join("")));
       setEventOfficers(officers);
     }
   }, [events, allOfficers]);
