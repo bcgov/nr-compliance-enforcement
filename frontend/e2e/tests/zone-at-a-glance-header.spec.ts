9ede04a5d338f3a7c91853c287a0356d307c47c6
Alec Wilcox <alecwilcox@gmail.com>
2025-10-17 12:36:26 -0700

    chore: migration zone-at-a-glance-header to pw

---
+++ frontend/e2e/tests/zone-at-a-glance-header.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+import { STORAGE_STATE_BY_ROLE } from "../utils/authConfig";
+import { waitForSpinner } from "../utils/helpers";
+
+test.describe("COMPENF-259 Zone at a Glance - View Complaint Stats", () => {
+  test.use({ storageState: STORAGE_STATE_BY_ROLE.COS });
+
+  test("it can navigate to zone at a glance", async ({ page }) => {
+    //-- navigate to application root
+    await page.goto("/");
+
+    //-- navigate to the zone at a glance
+    await page.locator("#icon-zone-at-a-glance-link").click();
+
+    //First ZAG test... might take a bit longer to spin up
+    await expect(page.locator(".comp-loader-overlay")).toBeVisible();
+    await expect(page.locator(".comp-loader-overlay")).not.toBeVisible();
+
+    //-- make sure we're on the zone at a glance page
+    await expect(
+      page
+        .locator(".comp-main-content")
+        .getByText(/Zone at a glance/)
+        .first(),
+    ).toBeVisible();
+
+    //-- navigate back to complaints
+    await page.locator("#icon-complaints-link").click();
+    await waitForSpinner(page);
+    await expect(
+      page
+        .locator(".comp-page-header")
+        .getByText(/Complaints/)
+        .first(),
+    ).toBeVisible();
+  });
+});
