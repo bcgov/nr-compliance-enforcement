b3c714b576ca14885fe74bb0dd73fc6dec6cb949
Ryan Rondeau <ryan_rondeau@live.ca>
2025-10-17 12:12:41 -0700

    Update graphql.ts

--- frontend/src/generated/graphql.ts
+++ frontend/src/generated/graphql.ts
@@ -1262,12 +1262,6 @@ export type QuerycheckInvestigationNameExistsArgs = {
 };
 
 
-export type QuerycaseFilesByActivityIdsArgs = {
-  activityIdentifiers: Array<Scalars['String']['input']>;
-  activityType: Scalars['String']['input'];
-};
-
-
 export type QueryconfigurationCodesArgs = {
   configurationCode?: InputMaybe<Scalars['String']['input']>;
 };
