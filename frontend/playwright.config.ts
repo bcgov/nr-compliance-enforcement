5b2d360ebd308c42cc689ce5270ca794b5d13291
Alec Wilcox <alecwilcox@gmail.com>
2025-10-24 12:06:31 -0700

    chore: adjust trace capture settings

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
