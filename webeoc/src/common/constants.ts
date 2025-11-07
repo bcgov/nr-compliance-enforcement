fd323079b85cb0ef27b6bc129c33baa512207b8f
Alec Wilcox <alecwilcox@gmail.com>
2025-11-07 10:21:11 -0800

    chore: rename environment variable

--- webeoc/src/common/constants.ts
+++ webeoc/src/common/constants.ts
@@ -1,10 +1,10 @@
 export const STAGING_API_ENDPOINT_CREATES = "staging-complaint/webeoc-complaint";
 export const STAGING_API_ENDPOINT_UPDATES = "staging-complaint/webeoc-complaint-update";
-export const NATS_DURABLE_COMPLAINTS = `${process.env.COMPLAINT_STREAM_NAME}`;
+export const NATS_DURABLE_COMPLAINTS = `${process.env.WEBEOC_STREAM_NAME}`;
 
 export const STREAMS = {
-  COMPLAINTS: `${process.env.COMPLAINT_STREAM_NAME}-complaints`,
-  ACTIONS_TAKEN: `${process.env.COMPLAINT_STREAM_NAME}-actions`,
+  COMPLAINTS: `${process.env.WEBEOC_STREAM_NAME}-complaints`,
+  ACTIONS_TAKEN: `${process.env.WEBEOC_STREAM_NAME}-actions`,
 };
 
 export const OPERATIONS = {
@@ -15,14 +15,14 @@ export const OPERATIONS = {
 };
 
 export const STREAM_TOPICS = {
-  COMPLAINTS: `${process.env.COMPLAINT_STREAM_NAME}.new_complaints`,
-  STAGING_COMPLAINTS: `${process.env.COMPLAINT_STREAM_NAME}.new_staging_complaints`,
-  COMPLAINT_UPDATE: `${process.env.COMPLAINT_STREAM_NAME}.updated_complaints`,
-  STAGING_COMPLAINT_UPDATE: `${process.env.COMPLAINT_STREAM_NAME}.new_staging_complaint_update`,
-  STAGE_ACTION_TAKEN: `${process.env.COMPLAINT_STREAM_NAME}.stage-action-taken`,
-  STAGE_UPDATE_ACTION_TAKEN: `${process.env.COMPLAINT_STREAM_NAME}.stage-update-action-taken`,
-  ACTION_TAKEN: `${process.env.COMPLAINT_STREAM_NAME}.action-taken`,
-  UPDATE_ACTION_TAKEN: `${process.env.COMPLAINT_STREAM_NAME}.update-action-taken`,
+  COMPLAINTS: `${process.env.WEBEOC_STREAM_NAME}.new_complaints`,
+  STAGING_COMPLAINTS: `${process.env.WEBEOC_STREAM_NAME}.new_staging_complaints`,
+  COMPLAINT_UPDATE: `${process.env.WEBEOC_STREAM_NAME}.updated_complaints`,
+  STAGING_COMPLAINT_UPDATE: `${process.env.WEBEOC_STREAM_NAME}.new_staging_complaint_update`,
+  STAGE_ACTION_TAKEN: `${process.env.WEBEOC_STREAM_NAME}.stage-action-taken`,
+  STAGE_UPDATE_ACTION_TAKEN: `${process.env.WEBEOC_STREAM_NAME}.stage-update-action-taken`,
+  ACTION_TAKEN: `${process.env.WEBEOC_STREAM_NAME}.action-taken`,
+  UPDATE_ACTION_TAKEN: `${process.env.WEBEOC_STREAM_NAME}.update-action-taken`,
 };
 
 export const STAGING_APIS = {
