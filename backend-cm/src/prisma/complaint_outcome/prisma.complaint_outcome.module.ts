1ae7a3124dcdc8cb15f7b4675e163198b345335a
Mike Vesprini <mike@button.is>
2025-11-25 16:11:57 -0800

    Use prisma extension for outcomes

--- backend-cm/src/prisma/complaint_outcome/prisma.complaint_outcome.module.ts
+++ backend-cm/src/prisma/complaint_outcome/prisma.complaint_outcome.module.ts
@@ -1,19 +1,16 @@
 import { Module } from "@nestjs/common";
 import { ComplaintOutcomePrismaService } from "./prisma.complaint_outcome.service";
 import { PrismaClient } from ".prisma/complaint_outcome";
+import createPgSessionExtension from "src/pg-session-extension/prisma-pg-session-extension";
 
 @Module({
   providers: [
     {
       provide: ComplaintOutcomePrismaService,
-      useValue: new PrismaClient(), // Initialize the Prisma client for case management
-      // To use the pg sessions extension for RLS, replace the useValue with:
-      /**
       useFactory: () => {
         const client = new PrismaClient();
         return client.$extends(createPgSessionExtension(client));
       },
-       */
     },
   ],
   exports: [ComplaintOutcomePrismaService],
