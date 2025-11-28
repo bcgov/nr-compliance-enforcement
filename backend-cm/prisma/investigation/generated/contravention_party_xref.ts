5a12cc79d0ed9b36ec10103c4a717724aea4c26d
Alec Wilcox <alecwilcox@gmail.com>
2025-11-28 14:53:45 -0800

    chore: commit generated files

--- backend-cm/prisma/investigation/generated/contravention_party_xref.ts
+++ backend-cm/prisma/investigation/generated/contravention_party_xref.ts
@@ -6,8 +6,8 @@ export class contravention_party_xref {
   @ApiProperty({ type: String })
   contravention_party_xref_guid: string;
 
-  @ApiPropertyOptional({ type: String })
-  contravention_guid?: string;
+  @ApiProperty({ type: String })
+  contravention_guid: string;
 
   @ApiPropertyOptional({ type: String })
   investigation_party_guid?: string;
@@ -27,8 +27,8 @@ export class contravention_party_xref {
   @ApiPropertyOptional({ type: Date })
   update_utc_timestamp?: Date;
 
-  @ApiPropertyOptional({ type: () => contravention })
-  contravention?: contravention;
+  @ApiProperty({ type: () => contravention })
+  contravention: contravention;
 
   @ApiPropertyOptional({ type: () => investigation_party })
   investigation_party?: investigation_party;
