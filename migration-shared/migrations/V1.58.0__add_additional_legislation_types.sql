459deb041f40a9c6f0545645d8441feef82cfd33
Ryan Rondeau <ryan_rondeau@live.ca>
2025-12-09 10:29:50 -0800

    combine scripts

--- migration-shared/migrations/V1.58.0__add_additional_legislation_types.sql
+++ migration-shared/migrations/V1.58.0__add_additional_legislation_types.sql
@@ -2,7 +2,6 @@
 -- Reference: http://www.bclaws.ca/standards/act.xsd
 -- Reference: http://www.bclaws.ca/standards/regulation.xsd
 -- Reference: http://www.bclaws.ca/standards/bylaw.xsd
-
 INSERT INTO
   legislation_type_code (
     legislation_type_code,
@@ -14,6 +13,24 @@ INSERT INTO
     create_utc_timestamp
   )
 VALUES
+  (
+    'PART',
+    'Part',
+    'A major division of an Act or Regulation, typically numbered Part 1, Part 2, etc.',
+    25,
+    TRUE,
+    'FLYWAY',
+    NOW ()
+  ),
+  (
+    'DEF',
+    'Definition',
+    'A defined term within an Act or Regulation, typically found in a definitions section.',
+    35,
+    TRUE,
+    'FLYWAY',
+    NOW ()
+  ),
   (
     'BYLAW',
     'Bylaw',
@@ -50,4 +67,3 @@ VALUES
     'FLYWAY',
     NOW ()
   ) ON CONFLICT (legislation_type_code) DO NOTHING;
-
