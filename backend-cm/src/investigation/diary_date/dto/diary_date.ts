b53667ba5daf3f4df97a1ba9ade7879faf337514
Ryan Rondeau <ryan_rondeau@live.ca>
2025-12-18 15:10:23 -0800

    Update diary_date.ts

--- backend-cm/src/investigation/diary_date/dto/diary_date.ts
+++ backend-cm/src/investigation/diary_date/dto/diary_date.ts
@@ -1,48 +1,7 @@
 import { createMap, forMember, mapFrom, Mapper } from "@automapper/core";
 import { Field, InputType } from "@nestjs/graphql";
 import { IsOptional } from "class-validator";
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-
-export class diary_date {
-  @ApiProperty({ type: String })
-  diary_date_guid: string;
-
-  @ApiProperty({ type: String })
-  investigation_guid: string;
-
-  @ApiProperty({ type: Date })
-  due_date: Date;
-
-  @ApiProperty({ type: String })
-  description: string;
-
-  @ApiProperty({ type: Date })
-  added_utc_timestamp: Date;
-
-  @ApiPropertyOptional({ type: String })
-  added_user_guid?: string;
-
-  @ApiPropertyOptional({ type: Date })
-  updated_utc_timestamp?: Date;
-
-  @ApiPropertyOptional({ type: String })
-  updated_user_guid?: string;
-
-  @ApiProperty({ type: Boolean })
-  active_ind: boolean = true;
-
-  @ApiProperty({ type: String })
-  create_user_id: string;
-
-  @ApiProperty({ type: Date })
-  create_utc_timestamp: Date;
-
-  @ApiPropertyOptional({ type: String })
-  update_user_id?: string;
-
-  @ApiPropertyOptional({ type: Date })
-  update_utc_timestamp?: Date;
-}
+import { diary_date } from "../../../../prisma/investigation/generated/diary_date";
 
 export class DiaryDate {
   diaryDateGuid: string;
