99f240ec5c5950ec0c3e85ff196453a7ee4b1728
Alec Wilcox <alecwilcox@gmail.com>
2026-04-08 15:49:14 -0700

    feat: first draft form

--- backend/src/external_api/investigation_data.ts
+++ backend/src/external_api/investigation_data.ts
@@ -1,16 +1,48 @@
-import { get, getAppUsers } from "src/external_api/shared_data";
+import { get } from "src/external_api/shared_data";
 
+// Used for Report generation
 export const getTask = async (token: string, taskId: string) => {
-  const query = `{task(taskId: "${taskId}") {
-    investigationIdentifier
-    taskTypeCode
-    assignedUserIdentifier
-    taskNumber
-    description
-    taskCategoryTypeCode
-    remarks
-    dueDate
-  }}`;
+  const query = `{
+    task(taskId: "${taskId}") {
+      investigationIdentifier
+      taskTypeCode
+      assignedUserIdentifier
+      taskNumber
+      description
+      taskCategoryTypeCode
+      remarks
+      dueDate
+    }
+    diaryDatesByTask(taskGuid: "${taskId}") {
+        dueDate
+        description
+    }
+    getExhibitsByTask(taskId: "${taskId}") {
+      exhibitNumber
+      description
+      dateCollected
+      collectedAppUserGuidRef
+    }
+    getActivityNotesByTask(taskGuid: "${taskId}") {
+      contentText
+      actionedDate
+      actionedTime
+      actionedAppUserGuidRef
+    }
+    appUsers {
+       appUserGuid
+       lastName
+       firstName
+    }
+    taskCategoryTypeCodes { 
+      taskCategoryTypeCode 
+      longDescription
+    }
+    taskTypeCodes { 
+      taskTypeCode 
+      longDescription
+    }
+  }`;
 
   const { data, errors } = await get(token, { query });
 
@@ -18,18 +50,49 @@ export const getTask = async (token: string, taskId: string) => {
     throw new Error(`GraphQL errors occurred while fetching task: ${JSON.stringify(errors)}`);
   }
 
-  // Data conversions
-  // TODO: Felt cute, might delete later
-  const task = data?.task;
-  if (task) {
-    const users = await getAppUsers(token);
-    const findUserName = (guid: string) => {
-      const user = users.find((u) => u.appUserGuid === guid);
-      return user ? `${user.lastName}, ${user.firstName}` : "Unknown officer";
-    };
+  const {
+    task,
+    diaryDatesByTask,
+    getExhibitsByTask,
+    getActivityNotesByTask,
+    appUsers,
+    taskCategoryTypeCodes,
+    taskTypeCodes,
+  } = data;
 
-    task.assignedUserIdentifier = findUserName(task.assignedUserIdentifier);
-  }
+  // Convert data into report readable format
 
-  return task;
+  // Users
+  const userMap = new Map(appUsers.map((u) => [u.appUserGuid, { firstName: u.firstName, lastName: u.lastName }]));
+  const _resolveUser = (guid: string) => userMap.get(guid) ?? { firstName: "Unknown", lastName: "User" };
+
+  // Task Categories
+  const categoryMap = new Map(
+    taskCategoryTypeCodes.map((tc) => [tc.taskCategoryTypeCode, { longDescription: tc.longDescription }]),
+  );
+  const _resolveTaskCategory = (categoryCode: string) =>
+    categoryMap.get(categoryCode) ?? { longDescription: "Unknown category" };
+
+  // Task types
+  const typeMap = new Map(taskTypeCodes.map((tt) => [tt.taskTypeCode, { longDescription: tt.longDescription }]));
+  const _resolveTaskType = (typeCode: string) => typeMap.get(typeCode) ?? { longDescription: "Unknown type" };
+
+  return {
+    ...task,
+    assignedUser: _resolveUser(task.assignedUserIdentifier),
+    taskType: {
+      category: _resolveTaskCategory(task.taskCategoryTypeCode),
+      type: _resolveTaskType(task.taskTypeCode),
+    },
+    exhibits: getExhibitsByTask.map((exhibit) => ({
+      ...exhibit,
+      collectedBy: _resolveUser(exhibit.collectedAppUserGuidRef),
+    })),
+    activityNotes: getActivityNotesByTask.map((note) => ({
+      ...note,
+      actionedBy: _resolveUser(note.actionedAppUserGuidRef),
+    })),
+    diaryDates: diaryDatesByTask,
+    generatedOn: new Date(),
+  };
 };
