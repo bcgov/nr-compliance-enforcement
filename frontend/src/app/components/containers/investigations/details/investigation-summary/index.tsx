ff1448027c05163a078006eb29fc6f4c33f20501
afwilcox <alecwilcox@gmail.com>
2026-05-20 08:50:09 -0700

    feat: Add expanded associated data component onto cases (#1594)
    Co-authored-by: Ryan Rondeau <nayr974@users.noreply.github.com>
    Co-authored-by: team-wolverine-nrm-repo-updater[bot] <203337993+team-wolverine-nrm-repo-updater[bot]@users.noreply.github.com>

--- frontend/src/app/components/containers/investigations/details/investigation-summary/index.tsx
+++ frontend/src/app/components/containers/investigations/details/investigation-summary/index.tsx
@@ -10,11 +10,11 @@ import { MapObjectType } from "@/app/types/maps/map-element";
 import { selectOfficerByAppUserGuid } from "@/app/store/reducers/officer";
 import DiaryDates from "@/app/components/containers/investigations/details/investigation-diary-dates";
 import { InvestigationItem } from "@/app/components/containers/investigations/details/investigation-summary/investigation-item";
-import CaseActivities from "@/app/components/containers/investigations/details/investigation-summary/case-activities";
 import { selectAgencyDropdown } from "@/app/store/reducers/code-table";
 import { useFormDirtyState } from "@/app/hooks/use-unsaved-changes-warning";
 import { useGeocodedCenter } from "@/app/hooks/use-geocoded-center";
 import { getMapZoom } from "@/app/common/geocoder";
+import { CaseActivities } from "@/app/components/containers/cases/case-activities/case-activities";
 import { useInvestigationReadOnly } from "../../hooks/use-investigation-read-only";
 
 interface InvestigationSummaryProps {
@@ -170,10 +170,10 @@ export const InvestigationSummary: FC<InvestigationSummaryProps> = ({
       <div className="comp-details-view">
         <div className="comp-details-content">
           {caseGuid && (
-            <CaseActivities
-              caseGuid={caseGuid}
-              caseName={caseName}
-            />
+            <>
+              <h2 className="mb-6 mb-sm-3">Associated data</h2>
+              <CaseActivities cases={[{ caseIdentifier: caseGuid, name: caseName }]} />
+            </>
           )}
           <div className="d-flex align-items-center justify-content-between mb-3">
             <h2 className="mb-0">Investigation summary</h2>
