8de9b55928a94bc6189d4a8797c5b1de98c21b79
Ryan Rondeau <ryan_rondeau@live.ca>
2026-06-02 12:45:13 -0700

    Update V1.70.0__add_mines_offices.sql

--- migration-shared/migrations/V1.70.0__add_mines_offices.sql
+++ migration-shared/migrations/V1.70.0__add_mines_offices.sql
@@ -16,25 +16,6 @@ VALUES
     ('OFFLOC', 'Office Location', NULL, 3, true, 'FLYWAY', NOW(), 'FLYWAY', NOW())
 ON CONFLICT DO NOTHING;
 
-INSERT INTO shared.geo_organization_unit_code (geo_organization_unit_code, short_description, long_description, effective_date, expiry_date, create_user_id, create_utc_timestamp, update_user_id, update_utc_timestamp, geo_org_unit_type_code, administrative_office_ind)
-VALUES
-    ('BLKCRKCR', 'Black Creek/Campbell River', 'Black Creek/Campbell River', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('CHLWK', 'Chilliwack', 'Chilliwack', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('NNIMO', 'Nanaimo', 'Nanaimo', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('SQMSHWHS', 'Squamish/Whistler', 'Squamish/Whistler', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('KMLPS', 'Kamloops', 'Kamloops', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('WLMSLK', 'Williams Lake', 'Williams Lake', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('DWSNCRK', 'Dawson Creek', 'Dawson Creek', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('PRCG', 'Prince George', 'Prince George', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('SMITHRS', 'Smithers', 'Smithers', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('TERRC', 'Terrace', 'Terrace', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('CSTLGAR', 'Castlegar', 'Castlegar', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('CRNBK', 'Cranbrook', 'Cranbrook', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('PNTCTN', 'Penticton', 'Penticton', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('RVLSTK', 'Revelstoke', 'Revelstoke', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false),
-    ('VRNON', 'Vernon', 'Vernon', NOW(), NULL, 'FLYWAY', NOW(), 'FLYWAY', NOW(), 'OFFLOC', false)
-ON CONFLICT DO NOTHING;
-
 INSERT INTO shared.office (office_guid, create_user_id, create_utc_timestamp, update_user_id, update_utc_timestamp, geo_organization_unit_code, agency_code_ref)
 VALUES
     -- Coast
