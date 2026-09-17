5c26c7ce0c8bfde98595b84f9af3d4b94a3c3b45
Alec Wilcox <alecwilcox@gmail.com>
2026-09-17 14:16:42 -0700

    fix: align column sizes and names

--- migration-complaint-outcome/migrations/V1.78.0__rename_species_code.sql
+++ migration-complaint-outcome/migrations/V1.78.0__rename_species_code.sql
@@ -1,4 +1,7 @@
 ALTER TABLE complaint_outcome.wildlife
   RENAME COLUMN species_code TO species_code_ref;
 
+ALTER TABLE complaint_outcome.wildlife
+  ALTER COLUMN species_code_ref TYPE character varying(16);
+
 COMMENT ON COLUMN complaint_outcome.wildlife.species_code_ref IS 'Unenforced foreign key to shared.species_code.species_code. A reference to the species of animal involved in the outcome of the human wildlife conflict complaint.';
--- migration-complaint/migrations/V1.78.0__move_species_to_shared.sql
+++ migration-complaint/migrations/V1.78.0__move_species_to_shared.sql
@@ -4,6 +4,9 @@ ALTER TABLE complaint.hwcr_complaint
 ALTER TABLE complaint.hwcr_complaint
   RENAME COLUMN species_code TO species_code_ref;
 
+ALTER TABLE complaint.hwcr_complaint
+  ALTER COLUMN species_code_ref TYPE character varying(16);
+
 COMMENT ON COLUMN complaint.hwcr_complaint.species_code_ref IS 'A reference to the species of animal involved in the human wildlife conflict complaint. The value is a code from the shared.species_code table. No foreign key exists as the tables reside in different schemas.';
 
 DROP TABLE complaint.species_code;
